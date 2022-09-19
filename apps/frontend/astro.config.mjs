6e22950b0b8bd328909480a89cdb35a12d42445f
Tony Dang <tony@tonydang.blog>
2022-09-19 11:54:57 -0700

    add mdx integration to frontend

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://tonydang.blog",
   server: {
     port: 3000,
   },
+  integrations: [mdx()],
 });
