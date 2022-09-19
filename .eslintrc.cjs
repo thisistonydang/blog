5eb710d3f99e6f332b5ce3cc7c70daa3f1b87f78
Tony Dang <tony@tonydang.blog>
2022-09-19 12:50:19 -0700

    add prettier and eslint to root devDeps

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+    "prettier",
+  ],
+  overrides: [{ files: ["*.astro"], parser: "astro-eslint-parser" }],
+};
