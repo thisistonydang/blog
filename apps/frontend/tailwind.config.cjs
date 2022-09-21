6d8d8f7a4bbee94b222bac989e4cbb8455f8c7a1
Tony Dang <tony@tonydang.blog>
2022-09-21 13:59:43 -0700

    tailwind.config.cjs: refactor shake animation

--- apps/frontend/tailwind.config.cjs
+++ apps/frontend/tailwind.config.cjs
@@ -30,15 +30,10 @@ module.exports = {
       keyframes: {
         "fade-in": { from: { opacity: 0 }, to: { opacity: 1 } },
         shake: {
-          "10%": { transform: "translate3d(-1px, 0, 0)" },
-          "20%": { transform: "translate3d(2px, 0, 0)" },
-          "30%": { transform: "translate3d(-4px, 0, 0)" },
-          "40%": { transform: "translate3d(4px, 0, 0)" },
-          "50%": { transform: "translate3d(-4px, 0, 0)" },
-          "60%": { transform: "translate3d(4px, 0, 0)" },
-          "70%": { transform: "translate3d(-4px, 0, 0)" },
-          "80%": { transform: "translate3d(2px, 0, 0)" },
-          "90%": { transform: "translate3d(-1px, 0, 0)" },
+          "10%, 90%": { transform: "translate3d(-1px, 0, 0)" },
+          "20%, 80%": { transform: "translate3d(2px, 0, 0)" },
+          "30%, 50%, 70%": { transform: "translate3d(-4px, 0, 0)" },
+          "40%, 60%": { transform: "translate3d(4px, 0, 0)" },
         },
         wave: {
           "0%": { transform: "rotate(0deg)" },
@@ -50,7 +45,7 @@ module.exports = {
       },
       animation: {
         "fade-in": "fade-in 500ms",
-        shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+        shake: "shake 0.8s",
         wave: "wave 2s linear infinite",
       },
     },
