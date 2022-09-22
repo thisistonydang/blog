32cc35b2fc769062491baac6bc400e8e005817bb
Tony Dang <tony@tonydang.blog>
2022-09-22 10:01:07 -0700

    init add Footer.astro

---
+++ apps/frontend/src/layouts/main/_components/Footer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import EasterEgg from "./EasterEgg.svelte";
+  import SubscriptionForm from "./SubscriptionForm.svelte";
+
+  let date = new Date().getFullYear();
+
+  onMount((): number => (date = new Date().getFullYear()));
+</script>
+
+<footer class="mt-24">
+  <SubscriptionForm />
+  <div class="mt-20 flex items-center">
+    <EasterEgg />
+    <p class="ml-2 text-xs opacity-90">
+      &copy; {date} Tony Dang<br />
+      Built with
+      <a href="https://astro.build">Astro</a>,
+      <a href="https://svelte.dev">Svelte</a>,
+      <a href="https://tailwindcss.com">Tailwind</a>
+      &amp; &#x2615;
+    </p>
+  </div>
+</footer>
