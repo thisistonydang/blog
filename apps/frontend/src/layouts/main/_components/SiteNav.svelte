5adba5d6f19241fd69e34adc4508c4e8db2e58ef
Tony Dang <tony@tonydang.blog>
2022-09-22 13:03:51 -0700

    init add SiteNav.svelte

---
+++ apps/frontend/src/layouts/main/_components/SiteNav.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { nav_pages } from "../_stores/nav_pages";
+  import NavLink from "./NavLink.svelte";
+
+  export let current_path: string;
+</script>
+
+<nav class="hidden sm:block ml-3">
+  <ul>
+    {#each nav_pages as page}
+      <li class="inline-block px-2">
+        <NavLink {current_path} {page} />
+      </li>
+    {/each}
+  </ul>
+</nav>
