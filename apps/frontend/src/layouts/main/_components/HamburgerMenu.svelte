6dc8a688c838a7d311490449cc58f0656f1ae016
Tony Dang <tony@tonydang.blog>
2022-09-22 13:13:18 -0700

    init add HamburgerMenu.svelte

---
+++ apps/frontend/src/layouts/main/_components/HamburgerMenu.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { hamburger_menu_is_open } from "../_stores/hamburger_menu_is_open";
+  import { nav_pages } from "../_stores/nav_pages";
+  import NavLink from "./NavLink.svelte";
+
+  export let current_path: string;
+</script>
+
+{#if $hamburger_menu_is_open}
+  <nav class="-mt-5 sm:hidden">
+    <ul class="ml-10 mb-7">
+      {#each nav_pages as page}
+        <li class="w-fit py-1.5">
+          <NavLink {current_path} {page} />
+        </li>
+      {/each}
+    </ul>
+  </nav>
+  <div class="mb-10 prose dark:prose-invert sm:hidden">
+    <hr />
+  </div>
+{/if}
