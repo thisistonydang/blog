35b9873cf1b0492e4a2990ce85e9ab2597593856
Tony Dang <tony@tonydang.blog>
2022-09-23 19:28:00 -0700

    init add Whoops.svelte

---
+++ apps/frontend/src/lib/components/Whoops.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let message = "";
+</script>
+
+<div class="text-center">
+  <div class="text-9xl my-24">(◑_◑)</div>
+  {#if message}
+    <p class="text-lg">{message}</p>
+  {/if}
+</div>
