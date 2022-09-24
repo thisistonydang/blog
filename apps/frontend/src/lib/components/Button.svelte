297004fe10346617cb3da18f4c3d0867f937ea95
Tony Dang <tony@tonydang.blog>
2022-09-23 20:48:14 -0700

    init add Button.svelte

---
+++ apps/frontend/src/lib/components/Button.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let width: string | undefined = undefined;
+  export let loading = false;
+  export let onclick: string | undefined = undefined;
+  export let handleClick: (() => void) | undefined = undefined;
+
+  let button: HTMLButtonElement;
+
+  // Set button width based on slotted text to prevent change in width when
+  // loading spinner shows.
+  onMount((): void => {
+    if (!width) width = `${button.offsetWidth}px`;
+  });
+</script>
+
+<button
+  bind:this={button}
+  disabled={loading}
+  {onclick}
+  on:click={handleClick}
+  style:width
+  class="
+    bg-heading
+    h-[41px]
+    px-3
+    text-center text-sm text-bg
+    hover:opacity-95
+  "
+>
+  {#if loading}
+    <svg
+      class="inline-block fill-bg animate-[spin_2s_linear_infinite]"
+      width="1.4rem"
+      height="1.4rem"
+      viewBox="0 0 512 512"
+    >
+      <path
+        d="
+          M304 48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5
+          48-48zm0 416c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48
+          48s48-21.5 48-48zM48 304c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48
+          21.5-48 48s21.5 48 48 48zm464-48c0-26.5-21.5-48-48-48s-48 21.5-48
+          48s21.5 48 48 48s48-21.5 48-48zM142.9 437c18.7-18.7 18.7-49.1
+          0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9s49.1 18.7 67.9
+          0zm0-294.2c18.7-18.7 18.7-49.1 0-67.9S93.7 56.2 75 75s-18.7 49.1 0
+          67.9s49.1 18.7 67.9 0zM369.1 437c18.7 18.7 49.1 18.7 67.9 0s18.7-49.1
+          0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9z
+        "
+      />
+    </svg>
+  {:else}
+    <slot />
+  {/if}
+</button>
