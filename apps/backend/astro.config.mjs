63661abfb5dfb654d0b9f5d77d2c7f457e69012b
Tony Dang <tony@tonydang.blog>
2022-09-26 16:07:13 -0700

    add cloudflare adapter

--- apps/backend/astro.config.mjs
+++ apps/backend/astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3001,
   },
+  output: "server",
+  adapter: cloudflare(),
 });
