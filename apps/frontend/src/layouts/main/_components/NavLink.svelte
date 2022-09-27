60674e8597c116618860ae8b5faa5bcf5a9e2d11
Tony Dang <tony@tonydang.blog>
2022-09-27 12:13:47 -0700

    init add NavLink.svelte

---
+++ apps/frontend/src/layouts/main/_components/NavLink.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let current_path: string;
+  export let page: { name: string; path: string };
+</script>
+
+<a
+  class="
+    text-heading opacity-90
+    after:block after:h-0.5 after:w-0
+    after:bg-accent
+    after:duration-200 after:hover:w-full
+  "
+  class:after:w-full={current_path === page.path}
+  href={page.path}
+>
+  {@html page.name}
+</a>
