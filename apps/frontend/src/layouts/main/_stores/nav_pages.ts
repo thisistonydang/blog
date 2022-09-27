e2e7677d89472c771834328c87e03403a5d6f82d
Tony Dang <tony@tonydang.blog>
2022-09-27 11:46:41 -0700

    init add nav_pages.ts

---
+++ apps/frontend/src/layouts/main/_stores/nav_pages.ts
@@ -0,0 +1,7 @@
+export const nav_pages = [
+  { name: "posts", path: "/posts" },
+  { name: "about", path: "/about" },
+  { name: "now", path: "/now" },
+  { name: "contact", path: "/contact" },
+  { name: "&#129483; buy me a coffee?", path: "/support" },
+];
