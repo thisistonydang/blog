739eae06da1e8186957983257a2db0c0336f20a4
Tony Dang <tony@tonydang.blog>
2022-09-28 16:29:53 -0700

    init add ScrollToTopLink.svelte

---
+++ apps/frontend/src/layouts/blog-post/_components/ScrollToTopLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let path: string;
+
+  const handleClick = (): void => {
+    document.documentElement.classList.add("scroll-smooth");
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    setTimeout((): void => {
+      document.documentElement.classList.remove("scroll-smooth");
+    }, 500);
+  };
+</script>
+
+<a class="no-underline" href={path} on:click|preventDefault={handleClick}>
+  &uarr; Scroll to top
+</a>
