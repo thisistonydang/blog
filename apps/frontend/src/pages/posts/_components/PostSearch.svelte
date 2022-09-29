6cb817b15c2e1afcddb143ca276ff5e4152b74a3
Tony Dang <tony@tonydang.blog>
2022-09-29 11:14:34 -0700

    init add PostSearch.svelte

---
+++ apps/frontend/src/pages/posts/_components/PostSearch.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import TextField from "@lib/components/TextField.svelte";
+  import Whoops from "@lib/components/Whoops.svelte";
+  import type { BlogPostFrontmatter } from "@lib/types/frontmatter";
+
+  export let posts: BlogPostFrontmatter[];
+
+  let search_string = "";
+  let filteredPosts = posts;
+
+  $: words = search_string.split(" ").filter((word): boolean => word !== "");
+
+  $: if (words.length) {
+    filteredPosts = posts.filter((post: BlogPostFrontmatter): boolean => {
+      let match = false;
+      words.forEach((word): void => {
+        if (
+          post.title.match(new RegExp(word, "i")) ||
+          post.description.match(new RegExp(word, "i"))
+        ) {
+          match = true;
+        }
+      });
+      return match;
+    });
+  } else {
+    filteredPosts = posts;
+  }
+</script>
+
+<TextField label="Search" type="search" bind:value={search_string} />
+
+<ul class="list-none p-0">
+  {#each filteredPosts as post (post.path)}
+    <li>
+      <time class="text-xs" datetime={post.pubDate}>
+        {post.pubDate.substring(0, 10)}
+      </time>
+      <br />
+      <a class="text-lg" href={post.path}>{post.title}</a>
+      <p class="mt-0">{post.description}</p>
+    </li>
+  {:else}
+    <Whoops message="Can't find any posts..." />
+  {/each}
+</ul>
