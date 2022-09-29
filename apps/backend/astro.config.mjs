2171a059c32fc82df21c6bcd7fc358b12bbcd6f3
Tony Dang <tony@tonydang.blog>
2022-09-29 10:19:33 -0700

    format update

--- apps/backend/astro.config.mjs
+++ apps/backend/astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3001,
-  },
+  server: { port: 3001 },
   output: "server",
   adapter: cloudflare(),
 });
