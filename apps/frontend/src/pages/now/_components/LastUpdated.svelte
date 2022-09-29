031532ca919be6aeff4c2195cefec884c2298d12
Tony Dang <tony@tonydang.blog>
2022-09-29 14:30:35 -0700

    init add LastUpdated.svelte

---
+++ apps/frontend/src/pages/now/_components/LastUpdated.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { intervalToDuration, formatDuration } from "date-fns";
+
+  export let milliseconds: number;
+
+  const last_updated_date = new Intl.DateTimeFormat(undefined, {
+    dateStyle: "long",
+    timeStyle: "medium",
+  }).format(new Date(milliseconds));
+
+  let time_since_updated: string;
+
+  const format_time_since_last_updated = (milliseconds: number): void => {
+    const start = new Date(milliseconds);
+    const duration = intervalToDuration({ start, end: new Date() });
+    console.log("duration:", duration);
+    const formattedDuration = formatDuration(duration, { delimiter: "," });
+    console.log("formattedDuration:", formattedDuration);
+    const durationArray = formattedDuration.split(",");
+    console.log("durationArray:", durationArray);
+    let time = "";
+    durationArray.forEach((i: string, index: number): void => {
+      if (durationArray.length > 1 && index + 1 === durationArray.length) {
+        time = `${time} and ${i}`;
+      } else if (time) {
+        time = `${time}, ${i}`;
+      } else {
+        time = i;
+      }
+    });
+    time_since_updated = time;
+  };
+
+  format_time_since_last_updated(milliseconds);
+  setInterval((): void => format_time_since_last_updated(milliseconds), 1000);
+</script>
+
+<b>Last Updated:</b> <time>{last_updated_date}</time><br />
+<small>(Or exactly <time>{time_since_updated}</time> ago)</small>
