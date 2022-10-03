13c4f12f45321efd894b002ebfce17193484b1ce
Tony Dang <tony@tonydang.blog>
2022-10-03 16:36:43 -0700

    add /support/donations to backend router

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "itty-router";
 
 import api_subscribe from "./routes/api/subscribe";
+import support_donations from "./routes/support/donations";
 
 const router = Router();
 router.post("/api/subscribe", api_subscribe);
+router.get("/support/donations", support_donations);
 
 export default { fetch: router.handle };
