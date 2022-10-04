0a20d9e9e4c58224ad32055446c353d98ad4baa3
Tony Dang <tony@tonydang.blog>
2022-10-04 11:26:34 -0700

    add /support/create-stripe-checkout to backend router

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "itty-router";
 
 import api_subscribe from "./routes/api/subscribe";
+import support_create_stripe_checkout from "./routes/support/create-stripe-checkout";
 import support_donations from "./routes/support/donations";
 
 const router = Router();
 router.post("/api/subscribe", api_subscribe);
+router.post("/support/create-stripe-checkout", support_create_stripe_checkout);
 router.get("/support/donations", support_donations);
 
 export default { fetch: router.handle };
