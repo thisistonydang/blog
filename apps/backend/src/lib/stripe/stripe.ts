eda5af65b07d1c00e8af03a1d3fdf8520c1dce26
Tony Dang <tony@tonydang.blog>
2022-10-06 13:43:13 -0700

    formatting refactor

--- apps/backend/src/lib/stripe/stripe.ts
+++ apps/backend/src/lib/stripe/stripe.ts
@@ -1,9 +1,8 @@
 import Stripe from "stripe";
 import type { Env } from "@lib/types/env";
 
-export const stripe = (env: Env): Stripe => {
-  return new Stripe(env.STRIPE_API_KEY, {
+export const stripe = (env: Env): Stripe =>
+  new Stripe(env.STRIPE_API_KEY, {
     apiVersion: "2022-08-01",
     httpClient: Stripe.createFetchHttpClient(),
   });
-};
