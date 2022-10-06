9c5d1165c2fb0ec64b57b0200f70790a0aa83af2
Tony Dang <tony@tonydang.blog>
2022-10-06 09:50:40 -0700

    add /support/webhook to backend router

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,10 +3,12 @@ import { Router } from "itty-router";
 import api_subscribe from "./routes/api/subscribe";
 import support_create_stripe_checkout from "./routes/support/create-stripe-checkout";
 import support_donations from "./routes/support/donations";
+import support_webhook from "./routes/support/webhook";
 
 const router = Router();
 router.post("/api/subscribe", api_subscribe);
 router.post("/support/create-stripe-checkout", support_create_stripe_checkout);
 router.get("/support/donations", support_donations);
+router.post("/support/webhook", support_webhook);
 
 export default { fetch: router.handle };
