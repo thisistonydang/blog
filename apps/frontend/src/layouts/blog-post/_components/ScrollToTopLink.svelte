3574eff98371db92adccc05c405f52d589a94879
Tony Dang <tony@tonydang.blog>
2022-10-09 15:17:52 -0700

    refactor arrow function to function

--- apps/frontend/src/layouts/blog-post/_components/ScrollToTopLink.svelte
+++ apps/frontend/src/layouts/blog-post/_components/ScrollToTopLink.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   export let path: string;
 
-  const handleClick = (): void => {
+  function handleClick(): void {
     document.documentElement.classList.add("scroll-smooth");
     document.body.scrollTop = document.documentElement.scrollTop = 0;
     setTimeout((): void => {
       document.documentElement.classList.remove("scroll-smooth");
     }, 500);
-  };
+  }
 </script>
 
 <a class="no-underline" href={path} on:click|preventDefault={handleClick}>
