c700cbd6d7bf6628b5852b55965df504b2557969
Tony Dang <tony@tonydang.blog>
2022-10-09 15:03:46 -0700

    refactor arrow function to function

--- apps/backend/src/lib/db/supabase.ts
+++ apps/backend/src/lib/db/supabase.ts
@@ -2,5 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import type { SupabaseClient } from "@supabase/supabase-js";
 import type { Env } from "@lib/types/env";
 
-export const supabase = (env: Env): SupabaseClient =>
-  createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_KEY);
+export function supabase(env: Env): SupabaseClient {
+  return createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_KEY);
+}
