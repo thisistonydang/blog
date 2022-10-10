51c6186d2e5673898296904d167cfafa51f60197
Tony Dang <tony@tonydang.blog>
2022-10-10 16:11:50 -0700

    rm old astro backend. using cloudflare workers instead

--- apps/backend-old/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-
-import cloudflare from "@astrojs/cloudflare";
-
-export default defineConfig({
-  server: { port: 3001 },
-  output: "server",
-  adapter: cloudflare(),
-});
--- apps/backend-old/src/lib/db/supabase-server-client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase_server_client = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_SERVICE_KEY
-);
