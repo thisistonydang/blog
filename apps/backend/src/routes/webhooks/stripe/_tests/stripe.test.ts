25813b1b45aacfde280080d1e7f8165cc48d87db
Tony Dang <tony@tonydang.blog>
2022-10-10 15:48:20 -0700

    init add routes/webhooks/stripe/_tests/stripe.test.ts

---
+++ apps/backend/src/routes/webhooks/stripe/_tests/stripe.test.ts
@@ -0,0 +1,106 @@
+import { describe, expect, it } from "vitest";
+
+import CryptoJS from "crypto-js";
+
+import { supabase } from "../../../../lib/db/supabase";
+import { env } from "../../../../lib/testing/env";
+import api_route from "../index";
+
+describe("/webhooks/stripe", () => {
+  [
+    {
+      // Already handled event
+      id: 123,
+      type: "checkout.session.completed",
+      name: "Tony",
+      signature_invalidator: "",
+      expected: new Response(null, { status: 204 }),
+    },
+    {
+      // Invalid event id
+      id: null,
+      type: "checkout.session.completed",
+      name: "Tony",
+      signature_invalidator: "",
+      expected: new Response("invalid event id", { status: 400 }),
+    },
+    {
+      // Invalid session object (customer name is null)
+      id: 456,
+      type: "checkout.session.completed",
+      name: null,
+      signature_invalidator: "",
+      expected: new Response("invalid session object", { status: 400 }),
+    },
+    {
+      // Not yet handled event that needs to be handled
+      id: 456,
+      type: "checkout.session.completed",
+      name: "Tony",
+      signature_invalidator: "",
+      expected: new Response(null, { status: 201 }),
+    },
+    {
+      // Not yet handled event, but does not need to be handled
+      id: 456,
+      type: "",
+      name: "Tony",
+      signature_invalidator: "",
+      expected: new Response(null, { status: 204 }),
+    },
+    {
+      // Event with invalid signature or request payload
+      id: 456,
+      type: "checkout.session.completed",
+      name: "Tony",
+      signature_invalidator: "blah",
+      expected: new Response("invalid signature or request", { status: 400 }),
+    },
+  ].forEach(({ id, type, name, signature_invalidator, expected }) => {
+    it("returns expected response", async () => {
+      // GIVEN mock stripe event
+      const stripe_event = {
+        id: 123,
+        type: "checkout.session.completed",
+        data: {
+          object: {
+            customer_details: { name: "Tony", email: "tony@tonydang.blog" },
+            amount_total: 500,
+            success_url: `${env.BLOG_URL}/support/successful-one-time-donation`,
+          },
+        },
+      };
+
+      // Modify event per test case. Type assertions added to avoid tsc errors,
+      // but value can be null for testing.
+      stripe_event.id = id as number;
+      stripe_event.type = type;
+      stripe_event.data.object.customer_details.name = name as string;
+
+      // AND GIVEN only one existing stripe_event with id = 123
+      await supabase(env)
+        .from("stripe_event")
+        .delete()
+        .neq("stripe_event_id", "blah");
+      await supabase(env).from("stripe_event").insert({ stripe_event_id: 123 });
+      const { data } = await supabase(env).from("stripe_event").select("*");
+      expect(data?.length).to.equal(1);
+
+      // WHEN A mock event request from Stripe is handled.
+      const v1 = CryptoJS.HmacSHA256(
+        `timestamp.${JSON.stringify(stripe_event)}${signature_invalidator}`,
+        env.STRIPE_ENDPOINT_SECRET
+      );
+      const stripe_signature = `t=timestamp,v1=${v1}`;
+      const request = new Request("https://tonydang.blog", {
+        method: "POST",
+        body: JSON.stringify(stripe_event),
+        headers: { "stripe-signature": stripe_signature },
+      });
+      const res = await api_route(request, env);
+
+      // THEN Expected response is returned.
+      expect(res).to.deep.equal(expected);
+    });
+  });
+});
