80b54985bb9662cb812f14c12465eedaa04f2b34
Tony Dang <tony@tonydang.blog>
2022-10-14 10:04:01 -0700

    init add relay-results/_lib/color

---
+++ apps/frontend/src/pages/relay-results/_lib/color/category-12.ts
@@ -0,0 +1,14 @@
+export const category_12 = [
+  "#256676", // teal
+  "#7c28a4", // purple
+  "#83b0d8", // light blue
+  "#26b09d", // turquoise
+  "#683d0d", // dark brown
+  "#fd5917", // red
+  "#fe85ad", // pink
+  "#5e9222", // olive
+  "#e38744", // brown
+  "#0b5313", // dark green
+  "#bde267", // green
+  "#5e5698", // blue
+];
