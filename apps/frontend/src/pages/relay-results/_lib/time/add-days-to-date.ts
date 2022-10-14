c52cb9deefdbb83260f3e52f3bf12b938754cd26
Tony Dang <tony@tonydang.blog>
2022-10-14 10:19:20 -0700

    better code comment

--- apps/frontend/src/pages/relay-results/_lib/time/add-days-to-date.ts
+++ apps/frontend/src/pages/relay-results/_lib/time/add-days-to-date.ts
@@ -1,5 +1,10 @@
 /**
  * Create a JS Date object and add specified number of days.
+ *
+ * @param dateParam - Either number of seconds since 00:00:00 UTC on 1 January
+ * 1970 OR a date string in a format recognized by the Date.parse() method.
+ *
+ * @param days - Number of days to add.
  */
 export function add_days_to_date(
   dateParam: number | string,
