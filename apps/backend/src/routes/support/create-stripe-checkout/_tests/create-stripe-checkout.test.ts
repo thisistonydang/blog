a3affd90819d8600d4e0fa5a3160d10b7de56bba
Tony Dang <tony@tonydang.blog>
2022-10-20 15:50:59 -0700

    format update

--- apps/backend/src/routes/support/create-stripe-checkout/_tests/create-stripe-checkout.test.ts
+++ apps/backend/src/routes/support/create-stripe-checkout/_tests/create-stripe-checkout.test.ts
@@ -4,22 +4,20 @@ import { env } from "@lib/testing/env";
 import api_route from "../index";
 
 describe("/support/create-stripe-checkout", () => {
-  [
-    { qty: "0", expected: "http://localhost:3000" },
-    { qty: "1", expected: "https://checkout.stripe.com" },
-  ].forEach(({ qty, expected }) => {
-    it("returns expected json response", async () => {
-      // GIVEN N/A.
+  it.each([
+    ["0", "http://localhost:3000"],
+    ["1", "https://checkout.stripe.com"],
+  ])("returns expected json response", async (qty, expected) => {
+    // GIVEN N/A.
 
-      // WHEN Request is made to api route with mocked request.formData.
-      const request = new Request("https://tonydang.blog");
-      const form_data_mock = async () => ({ get: () => qty });
-      request.formData = form_data_mock as unknown as () => Promise<FormData>;
-      const res = await api_route(request, env);
+    // WHEN Request is made to api route with mocked request.formData.
+    const request = new Request("https://tonydang.blog");
+    const form_data_mock = async () => ({ get: () => qty });
+    request.formData = form_data_mock as unknown as () => Promise<FormData>;
+    const res = await api_route(request, env);
 
-      // THEN Reponse json is as expected.
-      expect(res.status).to.equal(303);
-      expect(res.headers.get("location")).to.include(expected);
-    });
+    // THEN Reponse json is as expected.
+    expect(res.status).to.equal(303);
+    expect(res.headers.get("location")).to.include(expected);
   });
 });
