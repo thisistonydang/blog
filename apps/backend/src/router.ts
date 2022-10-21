dafd2c0558f37984b1059ebd4bb7de6e5ca3547f
Tony Dang <tony@tonydang.blog>
2022-10-21 14:49:14 -0700

    add /list/verify to router

--- apps/backend/src/router.ts
+++ apps/backend/src/router.ts
@@ -2,6 +2,7 @@ import { Router } from "itty-router";
 
 import api_subscribe from "./routes/api/subscribe";
 import list_unsubscribe from "./routes/list/unsubscribe";
+import list_verify from "./routes/list/verify";
 import support_create_stripe_checkout from "./routes/support/create-stripe-checkout";
 import support_donations from "./routes/support/donations";
 import webhooks_stripe from "./routes/webhooks/stripe";
@@ -9,6 +10,7 @@ import webhooks_stripe from "./routes/webhooks/stripe";
 const router = Router();
 router.post("/api/subscribe", api_subscribe);
 router.get("/list/unsubscribe", list_unsubscribe);
+router.get("/list/verify", list_verify);
 router.post("/support/create-stripe-checkout", support_create_stripe_checkout);
 router.get("/support/donations", support_donations);
 router.post("/webhooks/stripe", webhooks_stripe);
