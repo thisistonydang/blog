38de9b6e7280529ffe331b5c1eba6d2d7136ba98
Tony Dang <tony@tonydang.blog>
2022-10-21 10:15:44 -0700

    init add routes/list/unsubscribe/index.ts

---
+++ apps/backend/src/routes/list/unsubscribe/index.ts
@@ -0,0 +1,13 @@
+import { supabase } from "@lib/db/supabase";
+import type { Env } from "@lib/types/env";
+
+export default async function (request: Request, env: Env): Promise<Response> {
+  const { error } = await supabase(env)
+    .from("contact")
+    .update({ is_subscriber: false })
+    .match({ contact_id: new URL(request.url).searchParams.get("id") });
+
+  if (error) return Response.redirect(`${env.BLOG_URL}/whoops`, 303);
+
+  return Response.redirect(`${env.BLOG_URL}/list/unsubscribe/success`, 303);
+}
