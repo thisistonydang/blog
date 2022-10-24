af796058a7922c8cc5a4241e84cd94d5639e4853
Tony Dang <tony@tonydang.blog>
2022-10-24 16:54:10 -0700

    format update

--- apps/backend/src/lib/db/supabase.ts
+++ apps/backend/src/lib/db/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import type { SupabaseClient } from "@supabase/supabase-js";
+
 import type { Env } from "@lib/types/env";
 
 export function supabase(env: Env): SupabaseClient {
