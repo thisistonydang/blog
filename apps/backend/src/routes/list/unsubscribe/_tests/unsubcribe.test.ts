27ebda1568be05d83c5033c4b6d901f903081808
Tony Dang <tony@tonydang.blog>
2022-10-24 11:14:07 -0700

    when contact unsubscribe, ensure they are also verified

--- apps/backend/src/routes/list/unsubscribe/_tests/unsubcribe.test.ts
+++ apps/backend/src/routes/list/unsubscribe/_tests/unsubcribe.test.ts
@@ -1,20 +1,29 @@
 // @vitest-environment miniflare
 
-import { describe, expect, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
 import { supabase } from "@lib/db/supabase";
 import { env } from "@lib/testing/env";
 
 import api_route from "../index";
 
+beforeAll(async () => {
+  // SETUP Clear contact table.
+  await supabase(env).from("contact").delete().neq("email", "blah");
+});
+
+afterAll(async () => {
+  // TEARDOWN Clear contact table.
+  await supabase(env).from("contact").delete().neq("email", "blah");
+});
+
 describe("/list/unsubscribe", () => {
   it("unsubscribes a contact", async () => {
     // GIVEN A subscribed contact.
     const email = "tony@tonydang.blog";
     const contact_id = crypto.randomUUID();
-    await supabase(env).from("contact").delete().match({ email });
     await supabase(env).from("contact").insert({
-      contact_id: contact_id,
+      contact_id,
       name: "Tony Dang",
       preferred_name: "Tony",
       email,
@@ -30,7 +39,8 @@ describe("/list/unsubscribe", () => {
     const res = await api_route(request, env);
     rows = await supabase(env).from("contact").select("*").match({ email });
 
-    // THEN Contact is unsubscribed and redirected to confirmation page.
+    // THEN Contact is verified, unsubscribed, and redirected to confirmation page.
+    expect(rows.data?.[0].is_verified).to.be.true;
     expect(rows.data?.[0].is_subscriber).to.be.false;
     expect(res.status).to.equal(303);
     expect(res.headers.get("location")).to.include("/list/unsubscribe/success");
--- apps/backend/src/routes/list/unsubscribe/index.ts
+++ apps/backend/src/routes/list/unsubscribe/index.ts
@@ -4,7 +4,7 @@ import type { Env } from "@lib/types/env";
 export default async function (request: Request, env: Env): Promise<Response> {
   const { error } = await supabase(env)
     .from("contact")
-    .update({ is_subscriber: false })
+    .update({ is_verified: true, is_subscriber: false })
     .match({ contact_id: new URL(request.url).searchParams.get("id") });
 
   if (error) return Response.redirect(`${env.BLOG_URL}/whoops`, 303);
