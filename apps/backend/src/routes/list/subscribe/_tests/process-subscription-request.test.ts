6414a3fa0084dcc735e38489a4cd31ed19261bb5
Tony Dang <tony@tonydang.blog>
2022-10-25 14:54:00 -0700

    refactor

--- apps/backend/src/routes/list/subscribe/_tests/process-subscription-request.test.ts
+++ apps/backend/src/routes/list/subscribe/_tests/process-subscription-request.test.ts
@@ -1,65 +1,62 @@
 // @vitest-environment miniflare
 
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { afterAll, describe, expect, it } from "vitest";
 
 import { supabase } from "@lib/db/supabase";
 import { env } from "@lib/testing/env";
 
 import { process_subscription_request } from "../index";
 
-const SENDS_EMAIL = process.env.TEST_EMAIL;
-
-beforeAll(async () => {
-  // SETUP Clear contact table and add test data.
-  await supabase(env).from("contact").delete().neq("email", "blah");
-});
+const SKIP_EMAIL = process.env.SKIP_EMAIL;
+const unverified_email = "new.hope7149@fastmail.com";
+const new_contact_email = "merry.cat4054@fastmail.com";
 
 afterAll(async () => {
-  // TEARDOWN Clear contact table.
-  await supabase(env).from("contact").delete().neq("email", "blah");
+  await supabase(env)
+    .from("contact")
+    .delete()
+    .match({ email: unverified_email });
+  await supabase(env)
+    .from("contact")
+    .delete()
+    .match({ email: new_contact_email });
 });
 
 describe("process_subscription_request", () => {
-  it.skipIf(SENDS_EMAIL)(
+  it.skipIf(SKIP_EMAIL)(
     "updates contact info if existing unverified contact",
     async () => {
       // GIVEN An existing unverified contact.
-      const email = "white.moon8474@fastmail.com";
-      await supabase(env)
-        .from("contact")
-        .insert([
-          {
-            name: "is_NOT_verified",
-            preferred_name: "is_NOT_verified",
-            email,
-          },
-        ]);
+      await supabase(env).from("contact").insert({
+        name: "is_NOT_verified",
+        preferred_name: "is_NOT_verified",
+        email: unverified_email,
+      });
 
       // WHEN Subscription request is processed.
-      await process_subscription_request(env, "first_name last_name", email);
+      await process_subscription_request(env, "new hope", unverified_email);
       const { data } = await supabase(env)
         .from("contact")
         .select("*")
-        .match({ email });
+        .match({ email: unverified_email });
 
       // THEN Contact info is updated in db.
-      expect(data?.[0].name).to.equal("first_name last_name");
-      expect(data?.[0].preferred_name).to.equal("first_name");
+      expect(data?.[0].name).to.equal("new hope");
+      expect(data?.[0].preferred_name).to.equal("new");
     }
   );
-  it.skipIf(SENDS_EMAIL)("adds new contact to database", async () => {
+  it.skipIf(SKIP_EMAIL)("adds new contact to database", async () => {
     // GIVEN New contact.
-    const email = "new.hope7149@fastmail.com";
 
     // WHEN Subscription request is processed.
-    await process_subscription_request(env, "first_name last_name", email);
+    await process_subscription_request(env, "merry cat", new_contact_email);
     const { data } = await supabase(env)
       .from("contact")
       .select("*")
-      .match({ email });
+      .match({ email: new_contact_email });
 
     // THEN New contact is added to db.
-    expect(data?.[0].name).to.equal("first_name last_name");
-    expect(data?.[0].preferred_name).to.equal("first_name");
+    expect(data?.[0].name).to.equal("merry cat");
+    expect(data?.[0].preferred_name).to.equal("merry");
   });
 });
