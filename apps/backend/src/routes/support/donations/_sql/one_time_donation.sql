38fd1ebbcb8e2c16066eb1ee9a97f919effac8a1
Tony Dang <tony@tonydang.blog>
2022-10-25 15:32:22 -0700

    refactor

--- apps/backend/src/routes/support/donations/_sql/one_time_donation.sql
+++ apps/backend/src/routes/support/donations/_sql/one_time_donation.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS one_time_donation CASCADE;
 
 CREATE TABLE one_time_donation (
-    one_time_donation_id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    one_time_donation_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name citext NOT NULL,
     email citext NOT NULL,
     display_name citext NOT NULL,
