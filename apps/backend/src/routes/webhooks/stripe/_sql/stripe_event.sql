80655d9223cf7afd36af6053469db6375eb30f6c
Tony Dang <tony@tonydang.blog>
2022-10-25 15:33:23 -0700

    refactor

--- apps/backend/src/routes/webhooks/stripe/_sql/stripe_event.sql
+++ apps/backend/src/routes/webhooks/stripe/_sql/stripe_event.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS stripe_event CASCADE;
 
 CREATE TABLE stripe_event (
     stripe_event_id text PRIMARY KEY,
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE stripe_event ENABLE ROW LEVEL SECURITY;
