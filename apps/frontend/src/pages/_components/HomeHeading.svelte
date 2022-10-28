925d978814ae660c99246fabad8f09f2e3750937
Tony Dang <tony@tonydang.blog>
2022-10-28 12:33:08 -0700

    add css animate to home waving hand

--- apps/frontend/src/pages/_components/HomeHeading.svelte
+++ apps/frontend/src/pages/_components/HomeHeading.svelte
@@ -1,5 +1,11 @@
 <div class="text-heading text-3xl">Hi! I'm</div>
 <h1 class="text-5xl">
   Tony Dang
-  <span role="img" aria-label="waving hand">👋</span>
+  <span
+    role="img"
+    aria-label="waving hand"
+    class="inline-block animate-[wiggle_2.5s_linear_infinite]"
+  >
+    👋
+  </span>
 </h1>
