91cf993aa43f7c498adb11797f34c876ffc50c6c
Tony Dang <tony@tonydang.blog>
2022-10-28 08:27:07 -0700

    update send-verification-email.test.ts for aws-ses -> mailchannels

--- apps/backend/src/routes/list/subscribe/_tests/send-verification-email.test.ts
+++ apps/backend/src/routes/list/subscribe/_tests/send-verification-email.test.ts
@@ -6,21 +6,22 @@ import { env } from "@lib/testing/env";
 
 import { send_verification_email } from "../index";
 
-const SKIP_EMAIL = process.env.SKIP_EMAIL;
-
 describe("send_verification_email", () => {
-  it.skipIf(SKIP_EMAIL)("sends verification email", async () => {
-    // GIVEN N/A.
+  it("returns null body and 202 status when not in production", async () => {
+    // GIVEN Non-production environment.
+    expect(env.MODE).to.equal("test");
 
-    // WHEN Verification email is sent.
+    // WHEN send_verification_email is called.
     const res = await send_verification_email(
       env,
-      "white.moon8474@fastmail.com",
-      "test_send_verification_email",
-      "123"
+      "email",
+      "name",
+      "preferred_name",
+      "contact_id"
     );
 
-    // THEN Email is successfully sent.
-    expect(res.MessageId).to.be.a("string");
+    // THEN Null response with 202 status is returned.
+    expect(res.body).to.be.null;
+    expect(res.status).to.equal(202);
   });
 });
