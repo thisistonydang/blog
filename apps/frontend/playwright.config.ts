1d3cc6b2af209fcb9df78c6791ea71f02c2f2532
Tony Dang <tony@tonydang.blog>
2022-10-31 10:40:02 -0700

    set playwright to only match *.spec.ts files

--- apps/frontend/playwright.config.ts
+++ apps/frontend/playwright.config.ts
@@ -12,6 +12,7 @@ import { devices } from "@playwright/test";
  */
 const config: PlaywrightTestConfig = {
   testDir: "./src",
+  testMatch: "*.spec.ts",
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
