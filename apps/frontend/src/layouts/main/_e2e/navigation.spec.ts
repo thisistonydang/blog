b1f0dd610611f68215572f008bb0958a21fa724e
Tony Dang <tony@tonydang.blog>
2022-11-01 14:45:39 -0700

    init add navigation.spec.ts

---
+++ apps/frontend/src/layouts/main/_e2e/navigation.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("navigation", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test("can navigate to home page", async ({ page }) => {
+    await page.goto("/posts");
+    await page.locator("text=Td").click();
+    await expect(page).toHaveURL("/");
+    await expect(page).toHaveTitle("Tony Dang");
+  });
+
+  const nav_pages = [
+    { name: "Posts", path: "/posts" },
+    { name: "About", path: "/about" },
+    { name: "Now", path: "/now" },
+    { name: "Contact", path: "/contact" },
+    { name: "Buy Me a Coffee?", path: "/support" },
+  ];
+
+  for (const nav_page of nav_pages) {
+    test(`[desktop] can navigate to '${nav_page.name}' page`, async ({
+      page,
+      isMobile,
+    }) => {
+      test.skip(isMobile ?? false);
+
+      await page.locator(`nav >> text=${nav_page.name}`).click();
+      await expect(page).toHaveURL(nav_page.path);
+      await expect(page).toHaveTitle(`${nav_page.name} | Tony Dang`);
+    });
+
+    test(`[mobile] can navigate to '${nav_page.name}' page`, async ({
+      page,
+      isMobile,
+    }) => {
+      test.skip(!isMobile ?? false);
+
+      await page.locator('[aria-label="toggle hamburger menu"]').click();
+      await page
+        .locator(`nav >> text=${nav_page.name} >> visible=true`)
+        .click();
+      await expect(page).toHaveURL(nav_page.path);
+      await expect(page).toHaveTitle(`${nav_page.name} | Tony Dang`);
+    });
+  }
+});
