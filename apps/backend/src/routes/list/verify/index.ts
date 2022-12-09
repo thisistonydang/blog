97d91d45626bae50d80815514b7391e8f70b4208
Tony Dang <tony@tonydang.blog>
2022-12-09 11:11:14 -0800

    feat - send self email when new subscriber is verified

--- apps/backend/src/routes/list/verify/index.ts
+++ apps/backend/src/routes/list/verify/index.ts
@@ -1,5 +1,6 @@
 import { supabase } from "@lib/db/supabase";
 import { verify_jwt } from "@lib/jwt/verify-jwt";
+import { send_email } from "@lib/mailchannels/send_email";
 import type { Env } from "@lib/types/env";
 
 import type { Contact } from "../_types/contact";
@@ -23,6 +24,13 @@ export default async function (request: Request, env: Env): Promise<Response> {
         .update({ is_verified: true, is_subscriber: true })
         .match({ contact_id: payload.id });
       if (res.error) return Response.redirect(`${env.BLOG_URL}/whoops`, 303);
+      send_email(
+        env,
+        "tony@tonydang.blog",
+        "Tony",
+        "New Subscriber",
+        `Name: ${contact.name}<br>Email: ${contact.email}`
+      );
       return Response.redirect(`${env.BLOG_URL}/list/verify/success`, 303);
     }
   }
