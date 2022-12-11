ac93090152eadbd5e4aee5bbbea2e200708a1911
Tony Dang <tony@tonydang.blog>
2022-12-10 16:06:28 -0800

    update playwright config to allow retries - some tests failing due to timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,8 @@ const config: PlaywrightTestConfig = {
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : "50%",
+  retries: process.env.CI ? 2 : 1,
+  workers: process.env.CI ? 1 : "100%",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
   globalSetup: require.resolve("./global-setup"),
