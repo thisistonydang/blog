c460c94ff6cfa9e82f89717833bb99dbf767df10
Tony Dang <tony@tonydang.blog>
2022-12-13 10:22:49 -0800

    update deps

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
-  workers: process.env.CI ? 1 : "100%",
+  workers: process.env.CI ? 1 : "50%",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
   globalSetup: require.resolve("./global-setup"),
