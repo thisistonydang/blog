0ce124de1fae7638a2ffdee67d47993c98a5adcb
Tony Dang <tony@tonydang.blog>
2023-01-05 12:06:00 -0800

    init add shared d3 types

---
+++ apps/frontend/src/lib/types/d3.ts
@@ -0,0 +1,10 @@
+export interface Dimensions {
+  width: number;
+  height: number;
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+  bounded_height: number;
+  bounded_width: number;
+}
