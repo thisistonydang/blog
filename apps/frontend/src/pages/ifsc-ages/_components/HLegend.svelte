ef2de7a96bccc6a5b55b5670be5362531d772ceb
Tony Dang <tony@tonydang.blog>
2023-01-05 15:05:46 -0800

    init add HLegend.svelte

---
+++ apps/frontend/src/pages/ifsc-ages/_components/HLegend.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { ScaleOrdinal } from "d3";
+  import type { Dimensions } from "@lib/types/d3";
+
+  export let dms: Dimensions;
+  export let color_scale: ScaleOrdinal<string, string>;
+  export let spacing = 75;
+
+  const SQUARE_LENGTH = 10;
+</script>
+
+<g
+  transform={`translate(5, ${
+    dms.height - (dms.height - dms.bounded_height) / 2.5
+  })`}
+>
+  {#each color_scale.domain() as label, i}
+    <rect
+      class={`fill-[${color_scale(label)}]`}
+      width={SQUARE_LENGTH}
+      height={SQUARE_LENGTH}
+      transform={`translate(${0 + spacing * i}, 0)`}
+    />
+
+    <text
+      class="fill-heading text-xs"
+      transform={`translate(${20 + spacing * i}, ${SQUARE_LENGTH})`}
+    >
+      {label}
+    </text>
+  {/each}
+</g>
