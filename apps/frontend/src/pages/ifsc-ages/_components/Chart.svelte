4be9d363759408c2f6fe5705aa78cb9833b80f74
Tony Dang <tony@tonydang.blog>
2023-01-05 12:24:45 -0800

    init add Chart.svelte

---
+++ apps/frontend/src/pages/ifsc-ages/_components/Chart.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { Dimensions } from "@lib/types/d3";
+
+  export let dms: Dimensions;
+</script>
+
+<svg width={dms.width} height={dms.height}>
+  <g transform={`translate(${dms.left}, ${dms.top})`}>
+    <slot />
+  </g>
+</svg>
