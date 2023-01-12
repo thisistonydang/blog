75b03f019224cc30a0c15d4665d57d924f9b938c
Tony Dang <tony@tonydang.blog>
2023-01-12 14:08:18 -0800

    Chart.svelte: add a11y title

--- apps/frontend/src/lib/components/Chart.svelte
+++ apps/frontend/src/lib/components/Chart.svelte
@@ -2,9 +2,11 @@
   import type { Dimensions } from "@lib/types/d3";
 
   export let dms: Dimensions;
+  export let title: string;
 </script>
 
-<svg width={dms.width} height={dms.height}>
+<svg role="figure" width={dms.width} height={dms.height} on:focus={() => null}>
+  <title>{title}</title>
   <g transform={`translate(${dms.left}, ${dms.top})`}>
     <slot />
   </g>
