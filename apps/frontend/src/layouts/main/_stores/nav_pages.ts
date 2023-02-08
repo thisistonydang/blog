4ebbea3468ca9be1642311fd43add3e60bc88325
Tony Dang <tony@tonydang.blog>
2023-02-07 16:15:07 -0800

    rm all nav pages except for posts'

--- apps/frontend/src/layouts/main/_stores/nav_pages.ts
+++ apps/frontend/src/layouts/main/_stores/nav_pages.ts
@@ -1,11 +1,11 @@
 export const nav_pages = [
   { name: "posts", path: "/posts", prefetch: true },
-  { name: "about", path: "/about", prefetch: true },
-  { name: "now", path: "/now", prefetch: true },
-  { name: "contact", path: "/contact", prefetch: false },
-  {
-    name: '<span role="img" aria-label="coffee">☕</span> buy me a coffee?',
-    path: "/support",
-    prefetch: true,
-  },
+  // { name: "about", path: "/about", prefetch: true },
+  // { name: "now", path: "/now", prefetch: true },
+  // { name: "contact", path: "/contact", prefetch: false },
+  // {
+  //   name: '<span role="img" aria-label="coffee">☕</span> buy me a coffee?',
+  //   path: "/support",
+  //   prefetch: true,
+  // },
 ];
