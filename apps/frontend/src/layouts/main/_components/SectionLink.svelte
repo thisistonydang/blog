ab46a7e67cf3eab59ca0c22f7ea893d991bdc70e
Tony Dang <tony@tonydang.blog>
2023-02-08 15:29:27 -0800

    add SectionLink.svelte

---
+++ apps/frontend/src/layouts/main/_components/SectionLink.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let id: string;
+  export let current_section: string;
+</script>
+
+<li class="my-5">
+  <a href={`#${id}`} aria-label={`Go to ${id} section.`}>
+    <svg width="32" height="32">
+      <circle
+        cx="16"
+        cy="16"
+        r="15"
+        stroke-width="2"
+        class="stroke-surface fill-bg"
+        class:stroke-text={id === current_section}
+      />
+    </svg>
+  </a>
+</li>
