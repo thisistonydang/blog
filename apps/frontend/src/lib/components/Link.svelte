0f279e2263988c5c1f3c3de7481ee31a5e37416c
Tony Dang <tony@tonydang.blog>
2023-02-09 15:30:02 -0800

    init and Link.svelte

---
+++ apps/frontend/src/lib/components/Link.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let href: string;
+  export let prefetch = true;
+</script>
+
+<span class="not-prose">
+  <a
+    {href}
+    rel={prefetch ? "prefetch" : null}
+    class="
+      bg-heading text-bg border-heading
+      hover:bg-bg hover:text-heading
+      cursor-pointer border
+      p-[11px]
+      text-center text-sm
+    "
+  >
+    <slot />
+  </a>
+</span>
