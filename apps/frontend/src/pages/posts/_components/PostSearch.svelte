7927b2f953f6049200c02adca010b8f87a86d4be
Tony Dang <tony@tonydang.blog>
2023-02-10 15:46:31 -0800

    PostSearch.svelte: migrate to content collections

--- apps/frontend/src/pages/posts/_components/PostSearch.svelte
+++ apps/frontend/src/pages/posts/_components/PostSearch.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import TextField from "@lib/components/TextField.svelte";
   import Whoops from "@lib/components/Whoops.svelte";
-  import type { BlogPostFrontmatter } from "@lib/types/frontmatter";
+  import type { CollectionEntry } from "astro:content";
 
-  export let posts: BlogPostFrontmatter[];
+  export let posts: CollectionEntry<"posts">[];
 
   let search_string = "";
   let filteredPosts = posts;
@@ -11,12 +11,12 @@
   $: words = search_string.trim().replace(/\s+/g, " ").split(" ");
 
   $: if (words.length) {
-    filteredPosts = posts.filter((post: BlogPostFrontmatter): boolean => {
+    filteredPosts = posts.filter((post): boolean => {
       let match = false;
       words.forEach((word): void => {
         if (
-          post.title.match(new RegExp(word, "i")) ||
-          post.description.match(new RegExp(word, "i"))
+          post.data.title.match(new RegExp(word, "i")) ||
+          post.data.description.match(new RegExp(word, "i"))
         ) {
           match = true;
         }
@@ -26,19 +26,30 @@
   } else {
     filteredPosts = posts;
   }
+
+  function format_date(date: Date): string {
+    return new Intl.DateTimeFormat(undefined, {
+      dateStyle: "medium",
+    }).format(date);
+  }
 </script>
 
 <TextField label="Search" type="search" bind:value={search_string} />
 
 <ul class="list-none p-0">
-  {#each filteredPosts as post (post.path)}
+  {#each filteredPosts as post (post.data.path)}
     <li>
-      <time class="text-xs" datetime={post.pubDate}>
-        {post.pubDate.substring(0, 10)}
+      <time
+        class="text-xs"
+        datetime={post.data.pubDate.toISOString().split("T")[0]}
+      >
+        {format_date(post.data.pubDate)}
       </time>
       <br />
-      <a class="text-lg" href={post.path} rel="prefetch">{post.title}</a>
-      <p class="mt-0">{post.description}</p>
+      <a class="text-lg" href={post.data.path} rel="prefetch">
+        {post.data.title}
+      </a>
+      <p class="mt-0">{post.data.description}</p>
     </li>
   {:else}
     <Whoops message="Can't find any posts..." />
