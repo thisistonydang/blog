78664578ec6d36a3bf73ecc655295509dc5c717b
Tony Dang <tony@tonydang.blog>
2023-02-12 15:26:03 -0800

    update content schemas

--- apps/frontend/src/content/config.ts
+++ apps/frontend/src/content/config.ts
@@ -1,16 +1,21 @@
 import { z, defineCollection } from "astro:content";
 
+const page_schema = {
+  title: z.string().max(60),
+  description: z.string().max(160),
+  image: z.boolean().default(false),
+  alt: z.string().default(""),
+  noindex: z.boolean().default(false),
+  draft: z.boolean().default(true),
+};
+
+const post_schema = {
+  ...page_schema,
+  pubDate: z.date(),
+  tags: z.array(z.string()),
+};
+
 export const collections = {
-  posts: defineCollection({
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      image: z.boolean(),
-      alt: z.string(),
-      pubDate: z.date(),
-      tags: z.array(z.string()),
-      noindex: z.boolean(),
-      draft: z.boolean(),
-    }),
-  }),
+  pages: defineCollection({ schema: z.object(page_schema) }),
+  posts: defineCollection({ schema: z.object(post_schema) }),
 };
