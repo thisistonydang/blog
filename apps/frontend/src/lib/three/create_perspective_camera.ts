831c096f84fdbd75d60bb790e76df87306ec25ab
Tony Dang <tony@tonydang.blog>
2023-02-22 11:49:52 -0800

    use container sizes directly rather than using proxy sizes object

--- apps/frontend/src/lib/three/create_perspective_camera.ts
+++ apps/frontend/src/lib/three/create_perspective_camera.ts
@@ -3,13 +3,12 @@ import { PerspectiveCamera } from "three";
 /**
  * Create perspective camera and move camera out.
  */
-export function create_perspective_camera(sizes: {
-  width: number;
-  height: number;
-}): PerspectiveCamera {
+export function create_perspective_camera(
+  container: HTMLDivElement
+): PerspectiveCamera {
   const camera = new PerspectiveCamera(
     50,
-    sizes.width / sizes.height,
+    container.offsetWidth / container.offsetHeight,
     0.1,
     2000
   );
--- apps/frontend/src/lib/three/create_renderer.ts
+++ apps/frontend/src/lib/three/create_renderer.ts
@@ -3,12 +3,9 @@ import { WebGLRenderer } from "three";
 /**
  * Create renderer given container sizes.
  */
-export function create_renderer(sizes: {
-  width: number;
-  height: number;
-}): WebGLRenderer {
+export function create_renderer(container: HTMLDivElement): WebGLRenderer {
   const renderer = new WebGLRenderer({ alpha: true, antialias: true });
-  renderer.setSize(sizes.width, sizes.height);
+  renderer.setSize(container.offsetWidth, container.offsetHeight);
   renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
 
   return renderer;
