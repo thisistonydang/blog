16f9fca938f9103bf14ebbd7338650114ab14d68
Tony Dang <tony@tonydang.blog>
2023-02-22 11:59:32 -0800

    init add setup_three.ts

---
+++ apps/frontend/src/lib/three/setup_three.ts
@@ -0,0 +1,32 @@
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import { create_perspective_camera } from "./create_perspective_camera";
+import { create_renderer } from "./create_renderer";
+import { add_resize_listener } from "./add_resize_listener";
+
+/**
+ * Setup scene, canvas, and renderer given id of a parent div.
+ */
+export function setup_three(id: string): {
+  scene: Scene;
+  camera: PerspectiveCamera;
+  renderer: WebGLRenderer;
+} {
+  // Get parent container div by id.
+  const container = document.getElementById(id) as HTMLDivElement;
+
+  // Create scene.
+  const scene = new Scene();
+
+  // Add camera
+  const camera = create_perspective_camera(container);
+  scene.add(camera);
+
+  // Create renderer.
+  const renderer = create_renderer(container);
+  container.appendChild(renderer.domElement);
+
+  // Add resize listener.
+  add_resize_listener(container, camera, renderer);
+
+  return { scene, camera, renderer };
+}
