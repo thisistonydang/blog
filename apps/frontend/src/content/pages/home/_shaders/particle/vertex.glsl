fed2c260d6e6cfa682c755d27caeedbd4e080482
Tony Dang <tony@tonydang.blog>
2023-02-23 09:40:45 -0800

    init add particle vertex shader

---
+++ apps/frontend/src/content/pages/home/_shaders/particle/vertex.glsl
@@ -0,0 +1,46 @@
+uniform float u_elapsed_time;
+uniform float u_pixel_ratio;
+uniform float u_mix_percentage;
+
+attribute float a_scale;
+
+varying float v_mix_percentage;
+
+void main() {
+  /**
+ * Particle position
+ */
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+  // Oscillate particles randomly on all axes.
+  float amplitude = 0.05;
+  modelPosition.x += sin(u_elapsed_time + a_scale * 25.0) * amplitude;
+  modelPosition.y += sin(u_elapsed_time + a_scale * 50.0) * amplitude;
+  modelPosition.z += sin(u_elapsed_time + a_scale * 100.0) * amplitude;
+
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectionPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectionPosition;
+
+  /**
+ * Particle size
+ */
+
+  // Set base particle size
+  gl_PointSize = 125.0;
+
+  // Multiply by random scale to vary particle sizes
+  gl_PointSize = gl_PointSize * a_scale;
+
+  // Ensure size is uniform across devices
+  gl_PointSize = gl_PointSize * u_pixel_ratio;
+
+  // Add size attenuation
+  gl_PointSize = gl_PointSize * (1.0 / -viewPosition.z);
+
+  /**
+ * Set varyings
+ */
+  v_mix_percentage = u_mix_percentage;
+}
