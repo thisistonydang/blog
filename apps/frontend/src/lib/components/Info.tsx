4856655b9a2435b1b9756da17ed4b2586408f87e
Tony Dang <tony@tonydang.blog>
2023-02-28 16:32:39 -0800

    init add Info.tsx

---
+++ apps/frontend/src/lib/components/Info.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export default function Info({ children }: { children: React.ReactNode }) {
+  const [is_opened, set_is_opened] = useState(true);
+
+  // Hide info div if user presses on canvas.
+  useEffect(() => {
+    const hide_info = () => set_is_opened(false);
+
+    const canvas = document.querySelector("canvas");
+    canvas?.addEventListener("mousedown", hide_info);
+
+    return () => {
+      canvas?.removeEventListener("mousedown", hide_info);
+    };
+  }, []);
+
+  return (
+    <>
+      <aside>
+        {is_opened && (
+          <div
+            className="
+              bg-surface/80
+              fixed bottom-14 m-8 max-h-[50%] max-w-2xl
+              animate-[fly-up_0.25s] animate-[fade-in_0.25s]
+              overflow-y-auto rounded p-3 drop-shadow
+            "
+          >
+            {children}
+          </div>
+        )}
+        <button
+          className="
+            text-bg bg-text
+            fixed bottom-8 left-8 h-10 w-10
+            cursor-help rounded-full
+            font-serif text-2xl drop-shadow
+          "
+          aria-label={is_opened ? "Hide info." : "Show Info."}
+          onClick={() => set_is_opened(!is_opened)}
+        >
+          {is_opened ? <span className="text-3xl">&times;</span> : "i"}
+        </button>
+      </aside>
+    </>
+  );
+}
