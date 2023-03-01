359d4d8b7e40e6f9b7b453dab96de48f968a07d3
Tony Dang <tony@tonydang.blog>
2023-03-01 14:33:14 -0800

    init add particles Experience.tsx

---
+++ apps/frontend/src/content/projects/particles/_components/Experience.tsx
@@ -0,0 +1,11 @@
+import { OrbitControls } from "@react-three/drei";
+import Particles from "./Particles";
+
+export default function Experience() {
+  return (
+    <>
+      <Particles />
+      <OrbitControls />
+    </>
+  );
+}
