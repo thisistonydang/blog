2d15a9a4c7c3399425c35244f2be1f04447f4d10
Tony Dang <tony@tonydang.blog>
2023-02-28 16:40:09 -0800

    Info.tsx: disable button if not hydrated

--- apps/frontend/src/lib/components/Info.tsx
+++ apps/frontend/src/lib/components/Info.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 
 export default function Info({ children }: { children: React.ReactNode }) {
+  const [mounted, set_mounted] = useState(false);
   const [is_opened, set_is_opened] = useState(true);
 
-  // Hide info div if user presses on canvas.
   useEffect(() => {
+    set_mounted(true);
+
     const hide_info = () => set_is_opened(false);
 
     const canvas = document.querySelector("canvas");
@@ -31,12 +33,13 @@ export default function Info({ children }: { children: React.ReactNode }) {
           </div>
         )}
         <button
-          className="
+          className={`
             text-bg bg-text
             fixed bottom-8 left-8 h-10 w-10
-            cursor-help rounded-full
+            rounded-full
             font-serif text-2xl drop-shadow
-          "
+            ${mounted ? "cursor-help" : "cursor-not-allowed"}
+          `}
           aria-label={is_opened ? "Hide info." : "Show Info."}
           onClick={() => set_is_opened(!is_opened)}
         >
