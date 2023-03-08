fa645a2fc03700eed57ffbf0cc2ba58d6fe50ef3
Tony Dang <tony@tonydang.blog>
2023-03-08 15:04:47 -0800

    particles/_components/Experience.tsx: switch from OrbitControls -> TrackballControls

--- apps/frontend/src/content/projects/particles/_components/Experience.tsx
+++ apps/frontend/src/content/projects/particles/_components/Experience.tsx
@@ -1,11 +1,11 @@
-import { OrbitControls } from "@react-three/drei";
+import { TrackballControls } from "@react-three/drei";
 import Particles from "./Particles";
 
 export default function Experience() {
   return (
     <>
       <Particles />
-      <OrbitControls />
+      <TrackballControls />
     </>
   );
 }
