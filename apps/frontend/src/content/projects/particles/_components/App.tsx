fe9b9105f1eda72ebd1270e161796d829fd107b6
Tony Dang <tony@tonydang.blog>
2023-03-08 14:53:54 -0800

    add StrictMode and Controls to Particles

--- apps/frontend/src/content/projects/particles/_components/App.tsx
+++ apps/frontend/src/content/projects/particles/_components/App.tsx
@@ -1,18 +1,21 @@
+import { StrictMode } from "react";
 import { Canvas } from "@react-three/fiber";
+import Controls from "@lib/components/Controls";
 import Experience from "./Experience";
 
 export default function App() {
   return (
-    <>
+    <StrictMode>
       <main className="h-screen w-screen cursor-grab active:cursor-grabbing">
         <Canvas
           camera={{
-            position: [0, 0, 2],
+            position: [0, 0, 4],
           }}
         >
           <Experience />
         </Canvas>
       </main>
-    </>
+      <Controls />
+    </StrictMode>
   );
 }
