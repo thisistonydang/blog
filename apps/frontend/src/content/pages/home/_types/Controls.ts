d00ed04df7ca65998b9ce42fd5f245151967e737
Tony Dang <tony@tonydang.blog>
2023-03-08 14:08:37 -0800

    init add home/_types/Controls.ts

---
+++ apps/frontend/src/content/pages/home/_types/Controls.ts
@@ -0,0 +1,8 @@
+export interface Controls {
+  count: number;
+  size: number;
+  rotation: number;
+  oscillation: number;
+  light_color: { r: number; g: number; b: number };
+  dark_color: { r: number; g: number; b: number };
+}
