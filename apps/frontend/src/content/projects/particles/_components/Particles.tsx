a2d04987c619cda45966c44bfc4443accadb7e7a
Tony Dang <tony@tonydang.blog>
2023-03-08 17:19:09 -0800

    init add Particles.tsx

---
+++ apps/frontend/src/content/projects/particles/_components/Particles.tsx
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useControls } from "leva";
+import type { Points } from "three";
+
+import ParticlesGeometry from "./ParticlesGeometry";
+import ParticlesMaterial from "./ParticlesMaterial";
+
+export default function Particles() {
+  const particles = useRef<Points>(null);
+
+  const { rotation } = useControls({
+    rotation: { value: 0.05, min: -1, max: 1, step: 0.01 },
+  });
+
+  useFrame(({ clock }) => {
+    if (particles.current) {
+      particles.current.rotation.y = clock.getElapsedTime() * rotation;
+    }
+  });
+
+  return (
+    <points ref={particles}>
+      <ParticlesGeometry />
+      <ParticlesMaterial />
+    </points>
+  );
+}
