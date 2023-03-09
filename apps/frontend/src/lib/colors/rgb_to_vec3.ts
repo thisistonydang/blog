f36b0143df906d0014400adc7de88f365fd5f615
Tony Dang <tony@tonydang.blog>
2023-03-09 13:51:18 -0800

    rgb_to_vec3.ts: use global vec3 object for perf

--- apps/frontend/src/lib/colors/rgb_to_vec3.ts
+++ apps/frontend/src/lib/colors/rgb_to_vec3.ts
@@ -1,5 +1,12 @@
 import { Vector3 } from "three";
 
+// Global object for performance.
+const color = new Vector3();
+
 export function rgb_to_vec3({ r, g, b }: { r: number; g: number; b: number }) {
-  return new Vector3(r / 255, g / 255, b / 255);
+  color.x = r / 255;
+  color.y = g / 255;
+  color.z = b / 255;
+
+  return color;
 }
