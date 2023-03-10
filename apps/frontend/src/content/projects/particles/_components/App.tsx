cf4fdb61e225b64e66ae619a9cf2c660905dd91f
Tony Dang <tony@tonydang.blog>
2023-03-09 16:06:55 -0800

    particles/_components/App.tsx: add loading spinner before hydration

--- apps/frontend/src/content/projects/particles/_components/App.tsx
+++ apps/frontend/src/content/projects/particles/_components/App.tsx
@@ -1,23 +1,35 @@
 import { StrictMode } from "react";
 import { Canvas } from "@react-three/fiber";
 // import { Perf } from "r3f-perf";
+
 import Controls from "@lib/components/Controls";
+import Loading from "@lib/components/Loading";
+import { useMounted } from "@lib/hooks/useMounted";
+
 import Experience from "./Experience";
 
 export default function App() {
+  const mounted = useMounted();
+
   return (
     <StrictMode>
-      <main className="h-screen w-screen cursor-grab active:cursor-grabbing">
-        <Canvas
-          camera={{
-            position: [0, 0, 4],
-          }}
+      {mounted ? (
+        <main
+          className="
+              h-screen w-screen
+              animate-[fade-in_10s]
+              cursor-grab active:cursor-grabbing
+            "
         >
-          <Experience />
-          {/* <Perf position="bottom-right" /> */}
-        </Canvas>
-      </main>
-      <Controls />
+          <Canvas camera={{ position: [0, 0, 4] }}>
+            <Experience />
+            {/* <Perf position="bottom-right" /> */}
+          </Canvas>
+          <Controls />
+        </main>
+      ) : (
+        <Loading />
+      )}
     </StrictMode>
   );
 }
