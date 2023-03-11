2562e31fc6fd4a4c117b9752ef5184a358d29e11
Tony Dang <tony@tonydang.blog>
2023-03-11 15:25:14 -0800

    ParticlesGeometry.tsx: create typed array only once in global scope for perf

--- apps/frontend/src/content/projects/particles/_components/ParticlesGeometry.tsx
+++ apps/frontend/src/content/projects/particles/_components/ParticlesGeometry.tsx
@@ -1,45 +1,43 @@
-import { Fragment, useMemo } from "react";
 import { useControls } from "leva";
-import { BufferAttribute } from "three";
+
+const MAX_COUNT = 9999;
+const positions = new Float32Array(MAX_COUNT * 3);
+const scales = new Float32Array(MAX_COUNT * 1);
+
+for (let i = 0; i < MAX_COUNT; i++) {
+  // Set max_radius so that every 3rd particle is in the center nucleus.
+  const max_radius = i % 3 !== 0 ? 0.1 : 1;
+  const radius = Math.random() * max_radius;
+
+  const a = Math.random() - 0.5;
+  const b = Math.random() - 0.5;
+  const c = Math.random() - 0.5;
+  const d = Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+
+  positions[i * 3 + 0] = (a / d) * radius;
+  positions[i * 3 + 1] = (b / d) * radius;
+  positions[i * 3 + 2] = (c / d) * radius;
+
+  scales[i] = Math.random();
+}
 
 export default function ParticlesGeometry() {
   const { count } = useControls({
-    count: { value: 5000, min: 0, max: 9999, step: 1 },
+    count: { value: 5000, min: 0, max: MAX_COUNT, step: 1 },
   });
-
-  const { positions_attr, scales_attr } = useMemo(() => {
-    const positions = new Float32Array(count * 3);
-    const scales = new Float32Array(count * 1);
-
-    for (let i = 0; i < count; i++) {
-      // Set max_radius so that 75% of particles are in the center.
-      const max_radius = i < 0.75 * count ? 0.1 : 1;
-      const radius = Math.random() * max_radius;
-
-      const a = Math.random() - 0.5;
-      const b = Math.random() - 0.5;
-      const c = Math.random() - 0.5;
-      const d = Math.sqrt(a ** 2 + b ** 2 + c ** 2);
-
-      positions[i * 3 + 0] = (a / d) * radius;
-      positions[i * 3 + 1] = (b / d) * radius;
-      positions[i * 3 + 2] = (c / d) * radius;
-
-      scales[i] = Math.random();
-    }
-
-    const positions_attr = new BufferAttribute(positions, 3);
-    const scales_attr = new BufferAttribute(scales, 1);
-
-    return { positions_attr, scales_attr };
-  }, [count]);
 
   return (
     <bufferGeometry>
-      <Fragment key={count}>
-        <bufferAttribute attach="attributes-position" {...positions_attr} />
-        <bufferAttribute attach="attributes-a_scale" {...scales_attr} />
-      </Fragment>
+      <bufferAttribute
+        attach="attributes-position"
+        args={[positions, 3]}
+        count={count}
+      />
+      <bufferAttribute
+        attach="attributes-a_scale"
+        args={[scales, 1]}
+        count={count}
+      />
     </bufferGeometry>
   );
 }
