c76e9c185b71df0e35e094369683fd542ac76efc
Tony Dang <tony@tonydang.blog>
2023-03-13 14:02:21 -0700

    Info.tsx: mv title out from info window

--- apps/frontend/src/lib/components/Info.tsx
+++ apps/frontend/src/lib/components/Info.tsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import { useMounted } from "@lib/hooks/useMounted";
 
-export default function Info({ children }: { children: React.ReactNode }) {
+export default function Info({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
   const mounted = useMounted();
-  const [is_opened, set_is_opened] = useState(true);
+  const [is_opened, set_is_opened] = useState(false);
 
   useEffect(() => {
     const hide_info = () => set_is_opened(false);
@@ -28,20 +34,28 @@ export default function Info({ children }: { children: React.ReactNode }) {
           {children}
         </div>
       )}
-      <button
-        className={`
-          text-bg bg-text
-          fixed bottom-8 left-8 h-10 w-10
+      <div
+        className="
+          not-prose fixed bottom-8 left-8 flex
           animate-[fly-up_0.25s] animate-[fade-in_0.25s]
-          rounded-full font-serif
-          text-2xl drop-shadow
-          ${mounted ? "cursor-help" : "cursor-not-allowed"}
-        `}
-        aria-label={is_opened ? "Hide info." : "Show Info."}
-        onClick={() => set_is_opened(!is_opened)}
+          items-center gap-4
+        "
       >
-        {is_opened ? <span className="text-3xl">&times;</span> : "i"}
-      </button>
+        <button
+          className={`
+            text-bg bg-text
+            h-10 w-10
+            rounded-full font-serif
+            text-2xl drop-shadow
+            ${mounted ? "cursor-help" : "cursor-not-allowed"}
+          `}
+          aria-label={is_opened ? "Hide info." : "Show Info."}
+          onClick={() => set_is_opened(!is_opened)}
+        >
+          {is_opened ? <span className="text-3xl">&times;</span> : "i"}
+        </button>
+        <h1 className="text-heading text-3xl">{title}</h1>
+      </div>
     </aside>
   );
 }
