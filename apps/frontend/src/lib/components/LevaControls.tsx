2d8e61a814b5e5fbc57e7d280225bfa393a10b4f
Tony Dang <tony@tonydang.blog>
2023-03-13 14:52:16 -0700

    LevaControls: add Ping

--- apps/frontend/src/lib/components/LevaControls.tsx
+++ apps/frontend/src/lib/components/LevaControls.tsx
@@ -1,7 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Leva } from "leva";
+import Ping from "./Ping";
 
 export default function LevaControls() {
+  const [show_ping, set_show_ping] = useState(true);
+
   // Temp a11y fix. TODO: Make PR to Leva.
   useEffect(() => {
     const input = document.querySelector(
@@ -17,8 +20,15 @@ export default function LevaControls() {
         xs:right-[calc(15px+16px)] fixed right-4 top-[calc(16px+36px+40px-20px)]
         z-50 h-5 w-[280px]
       "
+      onMouseDown={() => set_show_ping(false)}
+      onTouchStart={() => set_show_ping(false)}
     >
       <Leva fill collapsed />
+      {show_ping && (
+        <div className="absolute -left-2 -top-2">
+          <Ping />
+        </div>
+      )}
     </div>
   );
 }
