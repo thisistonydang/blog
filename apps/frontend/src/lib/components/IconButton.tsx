f62f7d5199d4954d0c904c437d157d362a70668f
Tony Dang <tony@tonydang.blog>
2023-04-03 14:12:07 -0700

    IconButton.tsx: add toggleText and isPill props

--- apps/frontend/src/lib/components/IconButton.tsx
+++ apps/frontend/src/lib/components/IconButton.tsx
@@ -4,15 +4,19 @@ import Ping from "./Ping";
 export default function IconButton({
   ariaLabel,
   ariaLabelToggled,
+  toggledText,
   isToggled = false,
   hasPing = false,
+  isPill = false,
   onClick,
   children,
 }: {
   ariaLabel: string;
   ariaLabelToggled?: string;
+  toggledText?: React.ReactNode;
   isToggled?: boolean;
   hasPing?: boolean;
+  isPill?: boolean;
   onClick: () => void;
   children: React.ReactNode;
 }) {
@@ -22,16 +26,14 @@ export default function IconButton({
     <>
       <button
         className={`
-          text-bg bg-text
-          h-10 w-10
-          rounded-full font-serif
-          text-2xl drop-shadow
+          text-bg bg-text h-10 min-w-[40px] rounded-full drop-shadow
+          ${isPill ? "text-bold px-3 font-sans" : "font-serif text-2xl"}
           ${!mounted && "cursor-not-allowed"}
         `}
         aria-label={isToggled ? ariaLabelToggled : ariaLabel}
         onClick={onClick}
       >
-        {isToggled ? <>&times;</> : children}
+        {isToggled ? toggledText : children}
       </button>
       {hasPing && (
         <div className="absolute bottom-8 left-8">
