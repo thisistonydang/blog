552c787ee6c88f202f28ba78896c83ca570b6b94
Tony Dang <tony@tonydang.blog>
2023-04-05 17:53:24 -0700

    climbing-board/_components/AngleControls.tsx: init add

---
+++ apps/frontend/src/content/projects/climbing-board/_components/AngleControls.tsx
@@ -0,0 +1,63 @@
+import { useContext } from "react";
+
+import IconButton from "@lib/components/IconButton";
+
+import {
+  ALLOWED_ANGLES,
+  BoardAngleContext,
+  isBoardAngle,
+} from "../_context/BoardAngleContext";
+import { ControlsModeContext } from "../_context/ControlsModeContext";
+
+import { updateLocalStorage } from "../_hooks/useSyncLocalStorage";
+
+import ControlsDisplay from "./ControlsDisplay";
+import ControlsList from "./ControlsList";
+
+export default function AngleControls() {
+  const { boardAngle, setBoardAngle } = useContext(BoardAngleContext);
+  const { setControlsMode } = useContext(ControlsModeContext);
+
+  function updateBoardAngle(angle: number): void {
+    // Confirm angle is within allowed range.
+    const newAngle = isBoardAngle(angle) ? angle : 40;
+
+    // Update localStorage and app state with new angle.
+    updateLocalStorage(["angle", newAngle]);
+    setBoardAngle(newAngle);
+  }
+
+  return (
+    <ControlsList>
+      <li>
+        <ControlsDisplay width={64}>{boardAngle}&deg;</ControlsDisplay>
+      </li>
+      <li>
+        <IconButton
+          ariaLabel="Decrease board angle by 5 degrees."
+          disabled={boardAngle === ALLOWED_ANGLES[0]}
+          onClick={() => updateBoardAngle(boardAngle - 5)}
+        >
+          &#8722;
+        </IconButton>
+      </li>
+      <li>
+        <IconButton
+          ariaLabel="Increase board angle by 5 degrees."
+          disabled={boardAngle === ALLOWED_ANGLES.at(-1)}
+          onClick={() => updateBoardAngle(boardAngle + 5)}
+        >
+          &#43;
+        </IconButton>
+      </li>
+      <li>
+        <IconButton
+          ariaLabel="Hide angle controls."
+          onClick={() => setControlsMode("opened")}
+        >
+          &#x2714;
+        </IconButton>
+      </li>
+    </ControlsList>
+  );
+}
