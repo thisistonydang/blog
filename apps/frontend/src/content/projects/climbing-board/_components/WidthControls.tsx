192300a33159f5075b1dfd0f9ff903fdad4cec50
Tony Dang <tony@tonydang.blog>
2023-04-05 17:50:25 -0700

    climbing-board/_components/WidthControls.tsx: init add

---
+++ apps/frontend/src/content/projects/climbing-board/_components/WidthControls.tsx
@@ -0,0 +1,73 @@
+import { useContext } from "react";
+
+import IconButton from "@lib/components/IconButton";
+
+import {
+  ALLOWED_WIDTHS,
+  BoardWidthContext,
+  isBoardWidth,
+} from "../_context/BoardWidthContext";
+import { ControlsModeContext } from "../_context/ControlsModeContext";
+import { CurrentProblemContext } from "../_context/CurrentProblemContext";
+
+import allProblems from "../_data/problems.json";
+import { updateLocalStorage } from "../_hooks/useSyncLocalStorage";
+
+import ControlsDisplay from "./ControlsDisplay";
+import ControlsList from "./ControlsList";
+import { unSetProblem } from "./ContextProvider";
+
+export default function AngleControls() {
+  const { boardWidth, setBoardWidth } = useContext(BoardWidthContext);
+  const { setControlsMode } = useContext(ControlsModeContext);
+  const { setCurrentProblem } = useContext(CurrentProblemContext);
+
+  function updateBoardWidth(width: number): void {
+    // Confirm width is within allowed range.
+    const newWidth = isBoardWidth(width) ? width : 12;
+
+    // Find a problem at the new width.
+    const problem = allProblems.find((problem) => problem.width === newWidth);
+    const newProblem = problem ? problem : unSetProblem;
+
+    // Update localStorage and app state with new width and problem.
+    updateLocalStorage(["width", newWidth]);
+    updateLocalStorage(["problem", newProblem]);
+    setBoardWidth(newWidth);
+    setCurrentProblem(newProblem);
+  }
+
+  return (
+    <ControlsList>
+      <li>
+        <ControlsDisplay width={80}>{boardWidth} ft</ControlsDisplay>
+      </li>
+      <li>
+        <IconButton
+          ariaLabel="Decrease board width by 2 feet."
+          disabled={boardWidth === ALLOWED_WIDTHS[0]}
+          onClick={() => updateBoardWidth(boardWidth - 2)}
+        >
+          &#8722;
+        </IconButton>
+      </li>
+      <li>
+        <IconButton
+          ariaLabel="Increase board width by 2 feet."
+          disabled={boardWidth === ALLOWED_WIDTHS.at(-1)}
+          onClick={() => updateBoardWidth(boardWidth + 2)}
+        >
+          &#43;
+        </IconButton>
+      </li>
+      <li>
+        <IconButton
+          ariaLabel="Hide width controls."
+          onClick={() => setControlsMode("opened")}
+        >
+          &#x2714;
+        </IconButton>
+      </li>
+    </ControlsList>
+  );
+}
