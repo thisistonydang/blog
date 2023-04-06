44120c6aaef391ad50f00b2a02622215788cc8cc
Tony Dang <tony@tonydang.blog>
2023-04-06 13:03:11 -0700

    ControlsDialog.tsx: add hasPadding prop

--- apps/frontend/src/lib/components/ControlsDialog.tsx
+++ apps/frontend/src/lib/components/ControlsDialog.tsx
@@ -2,20 +2,23 @@ import Prose from "@lib/components/Prose";
 
 export default function ControlsDialog({
   isProse = false,
+  hasPadding = false,
   children,
 }: {
   isProse?: boolean;
+  hasPadding?: boolean;
   children: React.ReactNode;
 }) {
   return (
     <dialog
       open
-      className="
+      className={`
         bg-surface/90 fixed
         bottom-24 z-50 mx-8 max-h-[calc(100vh-96px-96px-96px)] max-w-2xl
         animate-[fly-up_0.25s] animate-[fade-in_0.25s]
-        overflow-y-auto rounded p-5 drop-shadow
-      "
+        overflow-y-auto rounded drop-shadow
+        ${hasPadding ? "p-5" : "p-0"}
+      `}
     >
       {isProse ? <Prose>{children}</Prose> : children}
     </dialog>
