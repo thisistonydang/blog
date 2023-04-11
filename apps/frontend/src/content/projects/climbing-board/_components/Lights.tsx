88e4c8de194c0d9e662ede42a058ee11a9544475
Tony Dang <tony@tonydang.blog>
2023-04-11 16:37:20 -0700

    climbing-board/_components/Lights.tsx: init add

---
+++ apps/frontend/src/content/projects/climbing-board/_components/Lights.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useRef } from "react";
+import { MathUtils } from "three";
+import {
+  MAIN_BOARD_HEIGHT,
+  MAX_BOARD_HEIGHT,
+} from "../_lib/constants/constants.js";
+
+import type { Object3D, SpotLight } from "three";
+
+// For Development
+// import { useHelper } from "@react-three/drei";
+// import { useControls } from "leva";
+// import { SpotLightHelper } from "three";
+
+export default function Lights() {
+  const spotLight = useRef<SpotLight>(null);
+  const target = useRef<Object3D>(null);
+
+  // Set spotLight target.
+  useEffect(() => {
+    if (!spotLight.current || !target.current) return;
+    spotLight.current.target = target.current;
+  }, []);
+
+  // useHelper(spotLight, SpotLightHelper);
+
+  // const { sIntensity, sY, sZ, angle, penumbra } = useControls("spotlight", {
+  //   sIntensity: { value: 7.5, min: 0, max: 50, step: 0.1 },
+  //   sY: { value: MAX_BOARD_HEIGHT / 2, min: -100, max: 100, step: 0.1 },
+  //   sZ: { value: 12, min: -100, max: 100, step: 0.1 },
+  //   angle: {
+  //     value: MathUtils.degToRad(75),
+  //     min: 0,
+  //     max: Math.PI / 2,
+  //     step: 0.1,
+  //   },
+  //   penumbra: { value: 0.5, min: 0, max: 1, step: 0.1 },
+  // });
+
+  // const { targetY, targetZ } = useControls("target", {
+  //   targetY: {
+  //     value: MAX_BOARD_HEIGHT - MAIN_BOARD_HEIGHT / 2,
+  //     min: -100,
+  //     max: 100,
+  //     step: 0.1,
+  //   },
+  //   targetZ: { value: -MAX_BOARD_HEIGHT / 2, min: -100, max: 100, step: 0.1 },
+  // });
+
+  return (
+    <>
+      <spotLight
+        ref={spotLight}
+        intensity={10}
+        position={[0, MAX_BOARD_HEIGHT / 2, 12]}
+        angle={MathUtils.degToRad(75)}
+        penumbra={0.5}
+      />
+      <object3D
+        ref={target}
+        position={[
+          0,
+          MAX_BOARD_HEIGHT - MAIN_BOARD_HEIGHT / 2,
+          -MAX_BOARD_HEIGHT / 2,
+        ]}
+      />
+    </>
+  );
+}
