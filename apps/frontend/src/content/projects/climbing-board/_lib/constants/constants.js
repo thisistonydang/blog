fc0800258f1082f44f72d7fe5cd241aa437cd31c
Tony Dang <tony@tonydang.blog>
2023-04-12 10:59:13 -0700

    climbing-board/_lib/constants/constants.js: change main board height and add separate row counts for main and kickboard

--- apps/frontend/src/content/projects/climbing-board/_lib/constants/constants.js
+++ apps/frontend/src/content/projects/climbing-board/_lib/constants/constants.js
@@ -2,15 +2,19 @@
 export const DEFAULT_CAMERA_FOV = 50;
 
 // Board
-export const MAX_BOARD_HEIGHT = 15; // 15 ft max for safety
-export const MAIN_BOARD_HEIGHT = 13.25;
-export const BOARD_THICKNESS = 2 / 12;
+export const BOARD_THICKNESS = 2 / 12; // Arbitrary thickness for aesthetics
+export const HOLD_SPACING = 0.65625; // Moonboard hold spacing
 
-// Main board
-export const HOLD_SPACING = 0.65625; // Moonboard hold spacing.
-export const MAX_ROWS = Math.floor(MAIN_BOARD_HEIGHT / HOLD_SPACING - 1);
+export const MAX_BOARD_HEIGHT = 15; // 15 ft max for safety
+export const MAIN_BOARD_HEIGHT = 12; // Standard 12ft height
+export const KICK_BOARD_HEIGHT = MAX_BOARD_HEIGHT - MAIN_BOARD_HEIGHT;
+
+export const MAIN_BOARD_ROWS = Math.floor(MAIN_BOARD_HEIGHT / HOLD_SPACING - 1);
+export const KICK_BOARD_ROWS = 2;
+
 export const alphabet = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"]; // Max columns
 export const NUM_GRIP_TYPES = 4;
 
 // Crash pad
+export const CRASH_PAD_DEPTH = MAX_BOARD_HEIGHT;
 export const BOARD_TO_PAD_GAP = 1 / 12;
