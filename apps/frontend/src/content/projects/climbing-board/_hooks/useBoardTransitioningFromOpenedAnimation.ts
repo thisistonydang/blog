4da662a1691f7f919d9cc0ffc3f4657c7ad1578d
Tony Dang <tony@tonydang.blog>
2023-04-12 17:01:38 -0700

    climbing-board/_hooks/useBoardTransitioningFromOpenedAnimation.ts: init add

---
+++ apps/frontend/src/content/projects/climbing-board/_hooks/useBoardTransitioningFromOpenedAnimation.ts
@@ -0,0 +1,38 @@
+import { useFrame } from "@react-three/fiber";
+import { useContext } from "react";
+import { MathUtils } from "three";
+
+import { ControlsModeContext } from "../_context/ControlsModeContext";
+
+import type { RefObject } from "react";
+import type { Group } from "three";
+
+export function useBoardTransitioningFromOpenedAnimation(
+  mainBoard: RefObject<Group>
+) {
+  const { controlsMode } = useContext(ControlsModeContext);
+
+  useFrame(({ invalidate }) => {
+    if (!mainBoard.current) return;
+
+    const FINAL_BOARD_ANGLE = 0;
+    const INTERPOLATION_FACTOR = 0.1;
+
+    if (
+      controlsMode === "transitioning_to_browse" ||
+      controlsMode === "transitioning_to_edit"
+    ) {
+      mainBoard.current.rotation.x = MathUtils.lerp(
+        mainBoard.current.rotation.x,
+        FINAL_BOARD_ANGLE,
+        INTERPOLATION_FACTOR
+      );
+
+      invalidate();
+    }
+
+    if (controlsMode === "browse" || controlsMode === "edit") {
+      mainBoard.current.rotation.x = FINAL_BOARD_ANGLE;
+    }
+  });
+}
