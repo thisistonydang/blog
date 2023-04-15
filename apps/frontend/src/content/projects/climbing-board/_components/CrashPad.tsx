e90ff012e262532adb41addfa84fc6061e0bb54f
Tony Dang <tony@tonydang.blog>
2023-04-14 17:09:13 -0700

    climbing-board/_components/CrashPad.tsx: change material to matcap

--- apps/frontend/src/content/projects/climbing-board/_components/CrashPad.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/CrashPad.tsx
@@ -1,4 +1,4 @@
-import { RoundedBox } from "@react-three/drei";
+import { RoundedBox, useTexture } from "@react-three/drei";
 import { useContext } from "react";
 
 import { BoardWidthContext } from "../_context/BoardWidthContext";
@@ -10,14 +10,15 @@ import {
 export default function CrashPad() {
   const { boardWidth } = useContext(BoardWidthContext);
   const CRASH_PAD_HEIGHT = MAX_BOARD_HEIGHT / 12; // 1" per feet of max height
+  const matcap = useTexture("/matcaps/64/1B1B1B_999999_575757_747474-64px.png");
 
   return (
     <RoundedBox
       args={[boardWidth + 8, CRASH_PAD_HEIGHT, CRASH_PAD_DEPTH]}
-      radius={0.6}
+      radius={0.2}
       position={[0, CRASH_PAD_HEIGHT / 2, 0]}
     >
-      <meshStandardMaterial color="#000000" />
+      <meshMatcapMaterial matcap={matcap} />
     </RoundedBox>
   );
 }
