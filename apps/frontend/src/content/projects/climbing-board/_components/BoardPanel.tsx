56774056d4b3df91f6255571cf2561042ca50a80
Tony Dang <tony@tonydang.blog>
2023-04-14 17:10:40 -0700

    climbing-board/_components/BoardPanel.tsx: switch boardMaterial with matcap

--- apps/frontend/src/content/projects/climbing-board/_components/BoardPanel.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/BoardPanel.tsx
@@ -1,23 +1,25 @@
+import { useTexture } from "@react-three/drei";
 import { useContext } from "react";
 
 import { BoardWidthContext } from "../_context/BoardWidthContext";
 
 import { BOARD_THICKNESS } from "../_lib/constants/constants.js";
 import { BOX_GEOMETRY_SCALE, boxGeometry } from "../_lib/geometries";
-import { boardMaterial } from "../_lib/materials";
 
 export default function BoardPanel({ height }: { height: number }) {
   const { boardWidth } = useContext(BoardWidthContext);
+  const matcap = useTexture("/matcaps/64/3B3B3B_C7C7C7_878787_A4A4A4-64px.png");
 
   return (
     <mesh
       geometry={boxGeometry}
-      material={boardMaterial}
       scale={[
         boardWidth * BOX_GEOMETRY_SCALE,
         height * BOX_GEOMETRY_SCALE,
         BOARD_THICKNESS * BOX_GEOMETRY_SCALE,
       ]}
-    ></mesh>
+    >
+      <meshMatcapMaterial matcap={matcap} />
+    </mesh>
   );
 }
