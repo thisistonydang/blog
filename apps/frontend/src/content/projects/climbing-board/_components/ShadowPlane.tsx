dadd270802efc1ae64ed5a604a95c2ed93ac8515
Tony Dang <tony@tonydang.blog>
2023-04-17 16:39:53 -0700

    climbing-board/_components/ShadowPlane.tsx: init add

---
+++ apps/frontend/src/content/projects/climbing-board/_components/ShadowPlane.tsx
@@ -0,0 +1,49 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+import { MathUtils } from "three";
+
+import type { ShadowMaterial } from "three";
+
+export default function ShadowPlane({
+  introAnimationCompleted,
+}: {
+  introAnimationCompleted: boolean;
+}) {
+  const material = useRef<ShadowMaterial>(null);
+
+  useFrame(({ invalidate }) => {
+    if (!material.current) return;
+
+    const FINAL_OPACITY = 1;
+    const INTERPOLATION_FACTOR = 0.025;
+
+    if (introAnimationCompleted && material.current.opacity !== FINAL_OPACITY) {
+      material.current.opacity = MathUtils.lerp(
+        material.current.opacity,
+        FINAL_OPACITY,
+        INTERPOLATION_FACTOR
+      );
+
+      const TOLERANCE = 0.001;
+
+      if (Math.abs(material.current.opacity - FINAL_OPACITY) < TOLERANCE) {
+        material.current.opacity = FINAL_OPACITY;
+      }
+
+      invalidate();
+    }
+  });
+
+  return (
+    <mesh
+      rotation={[-Math.PI * 0.5, 0, 0]}
+      position={[0, -1, 0]}
+      scale={[30, 30, 30]}
+      receiveShadow
+    >
+      <planeGeometry />
+      <shadowMaterial ref={material} transparent={true} opacity={0} />
+    </mesh>
+  );
+}
