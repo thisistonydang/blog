0b99d460f0e0fc9ad5b5576b5dcbc9cd9ae91365
Tony Dang <tony@tonydang.blog>
2023-04-17 16:42:05 -0700

    climbing-board/_components/CrashPad.tsx: use CRASH_PAD_HEIGHT const from constants rather than defining it in file

--- apps/frontend/src/content/projects/climbing-board/_components/CrashPad.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/CrashPad.tsx
@@ -4,12 +4,11 @@ import { useContext } from "react";
 import { BoardWidthContext } from "../_context/BoardWidthContext";
 import {
   CRASH_PAD_DEPTH,
-  MAX_BOARD_HEIGHT,
+  CRASH_PAD_HEIGHT,
 } from "../_lib/constants/constants.js";
 
 export default function CrashPad() {
   const { boardWidth } = useContext(BoardWidthContext);
-  const CRASH_PAD_HEIGHT = MAX_BOARD_HEIGHT / 12; // 1" per feet of max height
   const matcap = useTexture("/matcaps/64/1B1B1B_999999_575757_747474-64px.png");
 
   return (
