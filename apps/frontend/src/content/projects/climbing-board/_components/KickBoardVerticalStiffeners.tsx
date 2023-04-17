ac4434896c4d5d119af02504dc6dfc78c810de92
Tony Dang <tony@tonydang.blog>
2023-04-17 10:12:07 -0700

    climbing-board/_components/KickBoardVerticalStiffeners.tsx: init add

---
+++ apps/frontend/src/content/projects/climbing-board/_components/KickBoardVerticalStiffeners.tsx
@@ -0,0 +1,55 @@
+import { useTexture } from "@react-three/drei";
+import { useContext, useLayoutEffect, useRef } from "react";
+import { Matrix4 } from "three";
+
+import {
+  BOARD_THICKNESS,
+  HORIZONTAL_STIFFENER_SIZE,
+  KICK_BOARD_HEIGHT,
+} from "../_lib/constants/constants.js";
+import { BoardWidthContext } from "../_context/BoardWidthContext";
+import { boxGeometry } from "../_lib/geometries/boxGeometry";
+
+import type { InstancedMesh } from "three";
+
+const matrix4 = new Matrix4();
+
+export default function KickBoardVerticalStiffeners() {
+  const { boardWidth } = useContext(BoardWidthContext);
+  const instancedMesh = useRef<InstancedMesh>(null);
+  const matcap = useTexture("/matcaps/64/3B3B3B_C7C7C7_878787_A4A4A4-64px.png");
+  const STIFFENERS_COUNT = 2;
+
+  useLayoutEffect(() => {
+    if (!instancedMesh.current) return;
+
+    for (let i = 0; i < STIFFENERS_COUNT; i++) {
+      const positionX =
+        i === 0
+          ? boardWidth / 2 - BOARD_THICKNESS / 2
+          : -boardWidth / 2 + BOARD_THICKNESS / 2;
+
+      matrix4.makeScale(
+        BOARD_THICKNESS,
+        KICK_BOARD_HEIGHT,
+        HORIZONTAL_STIFFENER_SIZE
+      );
+      matrix4.setPosition(
+        positionX,
+        0,
+        -HORIZONTAL_STIFFENER_SIZE / 2 - BOARD_THICKNESS / 2
+      );
+      instancedMesh.current.setMatrixAt(i, matrix4);
+      instancedMesh.current.instanceMatrix.needsUpdate = true;
+    }
+  }, [boardWidth]);
+
+  return (
+    <instancedMesh
+      ref={instancedMesh}
+      args={[boxGeometry, undefined, STIFFENERS_COUNT]}
+    >
+      <meshMatcapMaterial matcap={matcap} />
+    </instancedMesh>
+  );
+}
