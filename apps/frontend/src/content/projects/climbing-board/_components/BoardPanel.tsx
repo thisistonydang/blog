59f0fe11466172daae7eb94607603b15f3715994
Tony Dang <tony@tonydang.blog>
2023-04-17 09:39:31 -0700

    climbing-board/_components/BoardPanel.tsx: update boxGeometry scale

--- apps/frontend/src/content/projects/climbing-board/_components/BoardPanel.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/BoardPanel.tsx
@@ -4,21 +4,14 @@ import { useContext } from "react";
 import { BoardWidthContext } from "../_context/BoardWidthContext";
 
 import { BOARD_THICKNESS } from "../_lib/constants/constants.js";
-import { BOX_GEOMETRY_SCALE, boxGeometry } from "../_lib/geometries";
+import { boxGeometry } from "../_lib/geometries/boxGeometry";
 
 export default function BoardPanel({ height }: { height: number }) {
   const { boardWidth } = useContext(BoardWidthContext);
   const matcap = useTexture("/matcaps/64/3B3B3B_C7C7C7_878787_A4A4A4-64px.png");
 
   return (
-    <mesh
-      geometry={boxGeometry}
-      scale={[
-        boardWidth * BOX_GEOMETRY_SCALE,
-        height * BOX_GEOMETRY_SCALE,
-        BOARD_THICKNESS * BOX_GEOMETRY_SCALE,
-      ]}
-    >
+    <mesh geometry={boxGeometry} scale={[boardWidth, height, BOARD_THICKNESS]}>
       <meshMatcapMaterial matcap={matcap} />
     </mesh>
   );
