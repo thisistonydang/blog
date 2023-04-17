6d644cc8b942c78b571bd76f7c2aeb1471b5be6f
Tony Dang <tony@tonydang.blog>
2023-04-17 09:49:36 -0700

    climbing-board/_components/MainBoardVerticalStiffeners.tsx: set shape based on HORIZONTAL_STIFFENER_SIZE

--- apps/frontend/src/content/projects/climbing-board/_components/MainBoardVerticalStiffeners.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/MainBoardVerticalStiffeners.tsx
@@ -1,33 +1,36 @@
 import { useTexture } from "@react-three/drei";
 import { useContext, useLayoutEffect, useRef } from "react";
-import { Euler, Matrix4, Shape, Vector3 } from "three";
+import { Matrix4, Shape } from "three";
 
 import { BoardWidthContext } from "../_context/BoardWidthContext";
 import {
   BOARD_THICKNESS,
+  HORIZONTAL_STIFFENER_SIZE,
   MAIN_BOARD_HEIGHT,
 } from "../_lib/constants/constants.js";
 
 import type { InstancedMesh } from "three";
 
-const euler = new Euler();
-const vector3 = new Vector3();
 const matrix4 = new Matrix4();
 
 // Create shape for extrudeGeometry
 const shape = new Shape();
 const stiffenerWidth = 1;
-const smallChamfer = 11 / 12;
 const bigChamfer = 4;
 shape.moveTo(0, 0);
 shape.lineTo(0, MAIN_BOARD_HEIGHT);
-shape.lineTo(stiffenerWidth - smallChamfer, MAIN_BOARD_HEIGHT);
+shape.lineTo(HORIZONTAL_STIFFENER_SIZE, MAIN_BOARD_HEIGHT);
+shape.lineTo(
+  HORIZONTAL_STIFFENER_SIZE,
+  MAIN_BOARD_HEIGHT - HORIZONTAL_STIFFENER_SIZE
+);
 shape.lineTo(stiffenerWidth, MAIN_BOARD_HEIGHT - bigChamfer);
 shape.lineTo(stiffenerWidth, bigChamfer);
-shape.lineTo(stiffenerWidth - smallChamfer, 0);
+shape.lineTo(HORIZONTAL_STIFFENER_SIZE, HORIZONTAL_STIFFENER_SIZE);
+shape.lineTo(HORIZONTAL_STIFFENER_SIZE, 0);
 shape.lineTo(0, 0);
 
-export default function Stiffeners() {
+export default function MainBoardVerticalStiffeners() {
   const { boardWidth } = useContext(BoardWidthContext);
   const instancedMesh = useRef<InstancedMesh>(null);
   const matcap = useTexture("/matcaps/64/3B3B3B_C7C7C7_878787_A4A4A4-64px.png");
@@ -40,11 +43,8 @@ export default function Stiffeners() {
       const positionX =
         i === 0 ? boardWidth / 2 - BOARD_THICKNESS : -boardWidth / 2;
 
-      // Set rotation and position
-      euler.set(0, Math.PI * 0.5, 0);
-      vector3.set(positionX, 0, -BOARD_THICKNESS / 2);
-      matrix4.makeRotationFromEuler(euler);
-      matrix4.setPosition(vector3);
+      matrix4.makeRotationY(Math.PI * 0.5);
+      matrix4.setPosition(positionX, 0, -BOARD_THICKNESS / 2);
       instancedMesh.current.setMatrixAt(i, matrix4);
       instancedMesh.current.instanceMatrix.needsUpdate = true;
     }
