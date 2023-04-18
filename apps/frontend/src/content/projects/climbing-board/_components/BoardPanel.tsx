565324f2feb481afe44731c054dbee3ad6724a6c
Tony Dang <tony@tonydang.blog>
2023-04-18 13:05:30 -0700

    climbing-board/_components/BoardPanel.tsx: use shared boardMaterial

--- apps/frontend/src/content/projects/climbing-board/_components/BoardPanel.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/BoardPanel.tsx
@@ -5,14 +5,18 @@ import { BoardWidthContext } from "../_context/BoardWidthContext";
 
 import { BOARD_THICKNESS } from "../_lib/constants/constants.js";
 import { boxGeometry } from "../_lib/geometries/boxGeometry";
+import { boardMaterial } from "../_lib/materials/boardMaterial";
 
 export default function BoardPanel({ height }: { height: number }) {
   const { boardWidth } = useContext(BoardWidthContext);
   const matcap = useTexture("/matcaps/64/3B3B3B_C7C7C7_878787_A4A4A4-64px.png");
 
   return (
-    <mesh geometry={boxGeometry} scale={[boardWidth, height, BOARD_THICKNESS]}>
-      <meshMatcapMaterial matcap={matcap} />
-    </mesh>
+    <mesh
+      geometry={boxGeometry}
+      material={boardMaterial}
+      material-matcap={matcap}
+      scale={[boardWidth, height, BOARD_THICKNESS]}
+    ></mesh>
   );
 }
