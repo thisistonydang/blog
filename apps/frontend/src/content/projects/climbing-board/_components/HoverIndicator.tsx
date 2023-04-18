dc79da882bf690f8b052a0a08183f9d1fd272798
Tony Dang <tony@tonydang.blog>
2023-04-18 15:36:38 -0700

    climbing-board/_components/HoverIndicator.tsx: extrat material into shared material

--- apps/frontend/src/content/projects/climbing-board/_components/HoverIndicator.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/HoverIndicator.tsx
@@ -1,12 +1,16 @@
+import { MeshBasicMaterial } from "three";
+
 import { bright } from "@lib/colors/paul-tol";
 import { ringGeometry } from "../_lib/geometries/ringGeometry";
 
 import type { ThreeElements } from "@react-three/fiber";
 
+const material = new MeshBasicMaterial({
+  color: `#${bright.grey}`,
+  transparent: true,
+  opacity: 0.5,
+});
+
 export default function HoverIndicator(props: ThreeElements["mesh"]) {
-  return (
-    <mesh {...props} geometry={ringGeometry}>
-      <meshBasicMaterial color={`#${bright.grey}`} transparent opacity={0} />
-    </mesh>
-  );
+  return <mesh {...props} geometry={ringGeometry} material={material}></mesh>;
 }
