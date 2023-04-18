038d8011e0d8248c8313f4e1b8397acd46db13b3
Tony Dang <tony@tonydang.blog>
2023-04-17 20:08:47 -0700

    climbing-board/_lib/geometries/ringGeometry.ts: init add

---
+++ apps/frontend/src/content/projects/climbing-board/_lib/geometries/ringGeometry.ts
@@ -0,0 +1,9 @@
+/**
+ * Shared ringGeometry
+ *
+ * Used for hover indicator and middle position type marker.
+ */
+
+import { RingGeometry } from "three";
+
+export const ringGeometry = new RingGeometry(0.3, 0.4, 32);
