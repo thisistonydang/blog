c539c4b98ecb281e2815b0bb8766fb0b4257de91
Tony Dang <tony@tonydang.blog>
2023-04-17 20:10:28 -0700

    climbing-board/_components/HoverIndicator.tsx: use shared ring geometry

--- apps/frontend/src/content/projects/climbing-board/_components/HoverIndicator.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/HoverIndicator.tsx
@@ -1,11 +1,11 @@
 import { bright } from "@lib/colors/paul-tol";
+import { ringGeometry } from "../_lib/geometries/ringGeometry";
 
 import type { ThreeElements } from "@react-three/fiber";
 
 export default function HoverIndicator(props: ThreeElements["mesh"]) {
   return (
-    <mesh {...props}>
-      <ringGeometry args={[0.3, 0.4, 32]} />
+    <mesh {...props} geometry={ringGeometry}>
       <meshBasicMaterial color={`#${bright.grey}`} transparent opacity={0} />
     </mesh>
   );
