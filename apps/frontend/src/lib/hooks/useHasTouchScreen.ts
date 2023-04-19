483345df3e580093ed6ecb9622a19005f9589b7c
Tony Dang <tony@tonydang.blog>
2023-04-19 14:23:23 -0700

    hooks/useHasTouchScreen.ts: update typing

--- apps/frontend/src/lib/hooks/useHasTouchScreen.ts
+++ apps/frontend/src/lib/hooks/useHasTouchScreen.ts
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 
+interface NavigatorExtended extends Navigator {
+  msMaxTouchPoints?: number;
+  useragent?: string;
+}
+
 /**
  * Returns whether user device has a touch screen.
  *
@@ -9,10 +14,15 @@ export function useHasTouchScreen() {
   const [hasTouchScreen, setHasTouchScreen] = useState(false);
 
   useEffect(() => {
+    const navigatorExtended: NavigatorExtended = navigator;
+
     if ("maxTouchPoints" in navigator) {
       setHasTouchScreen(navigator.maxTouchPoints > 0);
-    } else if ("msMaxTouchPoints" in navigator) {
-      setHasTouchScreen(navigator.msMaxTouchPoints > 0);
+    } else if (
+      "msMaxTouchPoints" in navigator &&
+      navigatorExtended.msMaxTouchPoints
+    ) {
+      setHasTouchScreen(navigatorExtended.msMaxTouchPoints > 0);
     } else {
       const mQ = matchMedia?.("(pointer:coarse)");
       if (mQ?.media === "(pointer:coarse)") {
@@ -21,7 +31,7 @@ export function useHasTouchScreen() {
         setHasTouchScreen(true); // deprecated, but good fallback
       } else {
         // Only as a last resort, fall back to user agent sniffing
-        const UA = navigator.userAgent;
+        const UA = navigatorExtended.userAgent;
         setHasTouchScreen(
           /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
             /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
