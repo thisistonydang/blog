65e4f33df8677f2ad4bb132dc97bf677d96ff5ad
Tony Dang <tony@tonydang.blog>
2023-04-20 13:58:14 -0700

    climbing-board/_components/Interface.tsx: add in FirstVisitModal

--- apps/frontend/src/content/projects/climbing-board/_components/Interface.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/Interface.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 
 import IconButton from "@lib/components/IconButton";
 import Prose from "@lib/components/Prose";
@@ -7,6 +7,7 @@ import { ControlsModeContext } from "../_context/ControlsModeContext";
 import AngleControls from "./AngleControls";
 import BrowseControls from "./BrowseControls";
 import EditControls from "./EditControls";
+import FirstVisitModal from "./FirstVisitModal";
 import InfoControls from "./InfoControls";
 import OpenedControls from "./OpenedControls";
 import WidthControls from "./WidthControls";
@@ -15,46 +16,48 @@ export default function Interface() {
   const { controlsMode, setControlsMode } = useContext(ControlsModeContext);
   const [showInfoPing, setShowInfoPing] = useState(true);
 
-  // Open controls on mount.
-  useEffect(() => setControlsMode("opened"), [setControlsMode]);
-
   return (
-    <nav>
-      {["closed", "opened"].includes(controlsMode) && (
-        <div
-          className="
+    <>
+      <FirstVisitModal />
+      <nav>
+        {["closed", "opened"].includes(controlsMode) && (
+          <div
+            className="
             fixed bottom-8 left-8 flex
             animate-[fly-right_0.25s] animate-[fade-in_0.25s]
             items-center gap-4
           "
-        >
-          <IconButton
-            ariaLabel="Show controls."
-            ariaLabelToggled="Hide controls."
-            toggledText={<>&times;</>}
-            isToggled={controlsMode === "opened"}
-            onClick={() => {
-              setControlsMode(controlsMode === "closed" ? "opened" : "closed");
-            }}
           >
-            &#x2B;
-          </IconButton>
-          <Prose>
-            <h1 className="xxs:text-4xl text-2xl">Climbing Board</h1>
-          </Prose>
-        </div>
-      )}
-      {controlsMode === "opened" && (
-        <OpenedControls
-          showInfoPing={showInfoPing}
-          setShowInfoPing={setShowInfoPing}
-        />
-      )}
-      {controlsMode === "info" && <InfoControls />}
-      {controlsMode === "width" && <WidthControls />}
-      {controlsMode === "angle" && <AngleControls />}
-      {controlsMode === "browse" && <BrowseControls />}
-      {controlsMode === "edit" && <EditControls />}
-    </nav>
+            <IconButton
+              ariaLabel="Show controls."
+              ariaLabelToggled="Hide controls."
+              toggledText={<>&times;</>}
+              isToggled={controlsMode === "opened"}
+              onClick={() => {
+                setControlsMode(
+                  controlsMode === "closed" ? "opened" : "closed"
+                );
+              }}
+            >
+              &#x2B;
+            </IconButton>
+            <Prose>
+              <h1 className="xxs:text-4xl text-2xl">Climbing Board</h1>
+            </Prose>
+          </div>
+        )}
+        {controlsMode === "opened" && (
+          <OpenedControls
+            showInfoPing={showInfoPing}
+            setShowInfoPing={setShowInfoPing}
+          />
+        )}
+        {controlsMode === "info" && <InfoControls />}
+        {controlsMode === "width" && <WidthControls />}
+        {controlsMode === "angle" && <AngleControls />}
+        {controlsMode === "browse" && <BrowseControls />}
+        {controlsMode === "edit" && <EditControls />}
+      </nav>
+    </>
   );
 }
