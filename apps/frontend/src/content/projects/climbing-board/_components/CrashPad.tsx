08690ff237d7780527e37d77537ca08fba49a75e
Tony Dang <tony@tonydang.blog>
2023-04-21 09:05:59 -0700

    climbing-board/_components/CrashPad.tsx: use native threejs RoundedBox to prevent console error with drei component

--- apps/frontend/src/content/projects/climbing-board/_components/CrashPad.tsx
+++ apps/frontend/src/content/projects/climbing-board/_components/CrashPad.tsx
@@ -1,5 +1,7 @@
-import { RoundedBox, useTexture } from "@react-three/drei";
+import { useTexture } from "@react-three/drei";
+import { BufferGeometryNode, extend } from "@react-three/fiber";
 import { useContext } from "react";
+import { RoundedBoxGeometry } from "three/examples/jsm/geometries/RoundedBoxGeometry";
 
 import { BoardWidthContext } from "../_context/BoardWidthContext";
 import {
@@ -9,22 +11,36 @@ import {
 } from "../_lib/constants/constants.js";
 import { padAndNutMaterial } from "../_lib/materials/padAndNutMaterial";
 
+extend({ RoundedBoxGeometry });
+
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    roundedBoxGeometry: BufferGeometryNode<
+      RoundedBoxGeometry,
+      typeof RoundedBoxGeometry
+    >;
+  }
+}
+
 export default function CrashPad() {
   const { boardWidth } = useContext(BoardWidthContext);
   const matcap = useTexture("/matcaps/64/1B1B1B_999999_575757_747474-64px.png");
 
   return (
-    <RoundedBox
-      args={[
-        boardWidth + 2 * CRASH_PAD_PADDING,
-        CRASH_PAD_HEIGHT,
-        CRASH_PAD_DEPTH,
-      ]}
-      radius={0.2}
+    <mesh
       material={padAndNutMaterial}
       material-matcap={matcap}
       position={[0, CRASH_PAD_HEIGHT / 2, 0]}
       castShadow
-    ></RoundedBox>
+    >
+      <roundedBoxGeometry
+        args={[
+          boardWidth + 2 * CRASH_PAD_PADDING,
+          CRASH_PAD_HEIGHT,
+          CRASH_PAD_DEPTH,
+          1,
+        ]}
+      />
+    </mesh>
   );
 }
