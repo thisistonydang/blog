9515d2404dce3ded267d2e2c3870414475925810
Tony Dang <tony@tonydang.blog>
2023-04-24 13:40:59 -0700

    three/World.ts: add animation loop

--- apps/frontend/src/lib/three/World.ts
+++ apps/frontend/src/lib/three/World.ts
@@ -2,6 +2,7 @@ import { createBasicCube } from "./components/basic-cube";
 import { createPerspectiveCamera } from "./components/perspective-camera";
 import { createScene } from "./components/scene";
 
+import { Loop } from "./systems/Loop";
 import { Resizer } from "./systems/Resizer";
 import { createRenderer } from "./systems/renderer";
 
@@ -10,21 +11,34 @@ import type { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 let scene: Scene;
 let camera: PerspectiveCamera;
 let renderer: WebGLRenderer;
+let loop: Loop;
 
 export class World {
   constructor(container: HTMLDivElement) {
     scene = createScene();
     camera = createPerspectiveCamera();
     renderer = createRenderer();
+    loop = new Loop(scene, camera, renderer);
     container.append(renderer.domElement);
 
     const cube = createBasicCube();
+
+    loop.updatables.push(cube);
+
     scene.add(cube);
 
-    const resizer = new Resizer(container, camera, renderer);
+    new Resizer(container, camera, renderer);
   }
 
   render() {
     renderer.render(scene, camera);
+  }
+
+  start() {
+    loop.start();
+  }
+
+  stop() {
+    loop.stop();
   }
 }
