29c704495a463e1e055b2df1ab6150ff4f125c95
Tony Dang <tony@tonydang.blog>
2023-04-24 15:01:13 -0700

    three/systems/oribit-controls.ts: init add

---
+++ apps/frontend/src/lib/three/systems/oribit-controls.ts
@@ -0,0 +1,18 @@
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+import type { Camera } from "three";
+import type { UpdatableOrbitControls } from "./Loop";
+
+export function createOrbitControls(
+  camera: Camera,
+  canvas: HTMLCanvasElement
+): UpdatableOrbitControls {
+  const controls = new OrbitControls(camera, canvas) as UpdatableOrbitControls;
+  controls.enableDamping = true;
+
+  controls.tick = (): void => {
+    controls.update();
+  };
+
+  return controls;
+}
