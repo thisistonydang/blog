94888106c00b742fb650c1112b5cdb90b3117912
Tony Dang <tony@tonydang.blog>
2023-04-24 12:05:51 -0700

    three/systems/renderer.ts: use physcially correct lights

--- apps/frontend/src/lib/three/systems/renderer.ts
+++ apps/frontend/src/lib/three/systems/renderer.ts
@@ -3,5 +3,8 @@ import { WebGLRenderer } from "three";
 export function createRenderer(): WebGLRenderer {
   const renderer = new WebGLRenderer({ alpha: true, antialias: true });
 
+  // Turn on the physically correct lighting model
+  renderer.useLegacyLights = false;
+
   return renderer;
 }
