50d414bb8b909553d333250451f8cd54b85fc5d3
Tony Dang <tony@tonydang.blog>
2023-04-24 11:43:59 -0700

    Resizer.ts: add resize listener

--- apps/frontend/src/lib/three/systems/Resizer.ts
+++ apps/frontend/src/lib/three/systems/Resizer.ts
@@ -1,22 +1,36 @@
 import type { PerspectiveCamera, WebGLRenderer } from "three";
 
+function setSize(
+  container: HTMLDivElement,
+  camera: PerspectiveCamera,
+  renderer: WebGLRenderer
+): void {
+  // Set the camera's aspect ratio.
+  camera.aspect = container.clientWidth / container.clientHeight;
+
+  // Update the camera's frustum.
+  camera.updateProjectionMatrix();
+
+  // Update the size of the renderer.
+  renderer.setSize(container.clientWidth, container.clientHeight);
+
+  // Set the pixel ratio. Note: pixel ratio may change when moving across
+  // different monitors.
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+}
+
 export class Resizer {
   constructor(
     container: HTMLDivElement,
     camera: PerspectiveCamera,
     renderer: WebGLRenderer
   ) {
-    // Set the camera's aspect ratio.
-    camera.aspect = container.clientWidth / container.clientHeight;
+    // Set initial size on load.
+    setSize(container, camera, renderer);
 
-    // Update the camera's frustum.
-    camera.updateProjectionMatrix();
-
-    // Update the size of the renderer.
-    renderer.setSize(container.clientWidth, container.clientHeight);
-
-    // Set the pixel ratio. Note: pixel ratio may change when moving across
-    // different monitors.
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    // Set the size again if a resize occurs.
+    window.addEventListener("resize", () => {
+      setSize(container, camera, renderer);
+    });
   }
 }
