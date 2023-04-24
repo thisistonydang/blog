4fdb79d1ac2cedcece5bef29554859de60c51337
Tony Dang <tony@tonydang.blog>
2023-04-24 14:09:33 -0700

    three/components/basic-cube.ts: change material to matcap and add animation behavior

--- apps/frontend/src/lib/three/components/basic-cube.ts
+++ apps/frontend/src/lib/three/components/basic-cube.ts
@@ -1,9 +1,20 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial } from "three";
+import { BoxGeometry, MathUtils, Mesh, MeshMatcapMaterial } from "three";
 
-export function createBasicCube(): Mesh {
+import type { UpdatableMesh } from "../systems/Loop";
+
+export function createBasicCube(): UpdatableMesh {
   const geometry = new BoxGeometry();
-  const material = new MeshBasicMaterial({ color: 0x0000ff });
-  const cube = new Mesh(geometry, material);
+  const material = new MeshMatcapMaterial();
+  const cube = new Mesh(geometry, material) as unknown as UpdatableMesh;
+
+  const radiansPerSecond = MathUtils.degToRad(30);
+
+  cube.tick = (delta: number): void => {
+    // Increase the cube's rotation each frame
+    cube.rotation.z += radiansPerSecond * delta;
+    cube.rotation.x += radiansPerSecond * delta;
+    cube.rotation.y += radiansPerSecond * delta;
+  };
 
   return cube;
 }
