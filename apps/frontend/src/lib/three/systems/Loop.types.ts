d67ea86e244ddf452fa522499be3823c5921acac
Tony Dang <tony@tonydang.blog>
2023-04-26 13:42:32 -0700

    three/systems/Loop.types.ts: init add

---
+++ apps/frontend/src/lib/three/systems/Loop.types.ts
@@ -0,0 +1,27 @@
+import type { InstancedMesh, Mesh } from "three";
+import type { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import type { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
+
+type Tick = (delta: number) => void;
+
+export interface UpdatableInstancedMesh extends InstancedMesh {
+  tick: Tick;
+}
+
+export interface UpdatableMesh extends Mesh {
+  tick: Tick;
+}
+
+export interface UpdatableOrbitControls extends OrbitControls {
+  tick: Tick;
+}
+
+export interface UpdatableTrackballControls extends TrackballControls {
+  tick: Tick;
+}
+
+export type UpdatableObject =
+  | UpdatableInstancedMesh
+  | UpdatableMesh
+  | UpdatableOrbitControls
+  | UpdatableTrackballControls;
