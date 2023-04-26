e03e90e442677932c6571679b34c263ecb91ceea
Tony Dang <tony@tonydang.blog>
2023-04-26 11:03:22 -0700

    hooks/hasTouchScreen.ts: add typing

--- apps/frontend/src/lib/hooks/hasTouchScreen.ts
+++ apps/frontend/src/lib/hooks/hasTouchScreen.ts
@@ -1,10 +1,24 @@
+interface NavigatorExtended extends Navigator {
+  msMaxTouchPoints?: number;
+  useragent?: string;
+}
+
+/**
+ * Returns whether user device has a touch screen.
+ *
+ * From MDN: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#mobile_device_detection
+ */
 export function hasTouchScreen(): boolean {
+  const navigatorExtended: NavigatorExtended = navigator;
   let hasTouchScreen = false;
 
   if ("maxTouchPoints" in navigator) {
     hasTouchScreen = navigator.maxTouchPoints > 0;
-  } else if ("msMaxTouchPoints" in navigator) {
-    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  } else if (
+    "msMaxTouchPoints" in navigator &&
+    navigatorExtended.msMaxTouchPoints
+  ) {
+    hasTouchScreen = navigatorExtended.msMaxTouchPoints > 0;
   } else {
     const mQ = matchMedia?.("(pointer:coarse)");
     if (mQ?.media === "(pointer:coarse)") {
@@ -13,7 +27,7 @@ export function hasTouchScreen(): boolean {
       hasTouchScreen = true; // deprecated, but good fallback
     } else {
       // Only as a last resort, fall back to user agent sniffing
-      const UA = navigator.userAgent;
+      const UA = navigatorExtended.userAgent;
       hasTouchScreen =
         /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
         /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
