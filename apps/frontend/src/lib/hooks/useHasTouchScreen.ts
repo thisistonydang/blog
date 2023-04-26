23494b5e5fb166e260f1d7dfe82ac3097acf0fba
Tony Dang <tony@tonydang.blog>
2023-04-26 11:17:14 -0700

    hooks/useHasTouchScreen.ts: extract logic into non-react hook

--- apps/frontend/src/lib/hooks/useHasTouchScreen.ts
+++ apps/frontend/src/lib/hooks/useHasTouchScreen.ts
@@ -1,44 +1,15 @@
 import { useEffect, useState } from "react";
-
-interface NavigatorExtended extends Navigator {
-  msMaxTouchPoints?: number;
-  useragent?: string;
-}
+import { hasTouchScreen } from "./hasTouchScreen";
 
 /**
  * Returns whether user device has a touch screen.
- *
- * From MDN: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#mobile_device_detection
  */
 export function useHasTouchScreen() {
-  const [hasTouchScreen, setHasTouchScreen] = useState(false);
+  const [touch, setTouch] = useState(false);
 
   useEffect(() => {
-    const navigatorExtended: NavigatorExtended = navigator;
-
-    if ("maxTouchPoints" in navigator) {
-      setHasTouchScreen(navigator.maxTouchPoints > 0);
-    } else if (
-      "msMaxTouchPoints" in navigator &&
-      navigatorExtended.msMaxTouchPoints
-    ) {
-      setHasTouchScreen(navigatorExtended.msMaxTouchPoints > 0);
-    } else {
-      const mQ = matchMedia?.("(pointer:coarse)");
-      if (mQ?.media === "(pointer:coarse)") {
-        setHasTouchScreen(!!mQ.matches);
-      } else if ("orientation" in window) {
-        setHasTouchScreen(true); // deprecated, but good fallback
-      } else {
-        // Only as a last resort, fall back to user agent sniffing
-        const UA = navigatorExtended.userAgent;
-        setHasTouchScreen(
-          /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-        );
-      }
-    }
+    setTouch(hasTouchScreen());
   }, []);
 
-  return hasTouchScreen;
+  return touch;
 }
