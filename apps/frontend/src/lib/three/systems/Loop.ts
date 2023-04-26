6ca07fe3c05b6dc30e67b250b935e9bb5f93bcf4
Tony Dang <tony@tonydang.blog>
2023-04-26 16:47:57 -0700

    three/systems/Loop.ts: rename updatables -> tickables for consistency

--- apps/frontend/src/lib/three/systems/Loop.ts
+++ apps/frontend/src/lib/three/systems/Loop.ts
@@ -2,7 +2,7 @@ import { Clock } from "three";
 import { Statistics } from "./Statistics";
 
 import type { Camera, Scene, WebGLRenderer } from "three";
-import type { UpdatableObject } from "./Loop.types";
+import type { TickableObject } from "./Loop.types";
 
 const clock = new Clock();
 
@@ -10,28 +10,28 @@ export class Loop {
   scene: Scene;
   camera: Camera;
   renderer: WebGLRenderer;
-  updatables: UpdatableObject[];
+  tickables: TickableObject[];
   statistics: Statistics | null;
 
   constructor(
     scene: Scene,
     camera: Camera,
     renderer: WebGLRenderer,
-    updatables: UpdatableObject[],
-    stats = false
+    tickables: TickableObject[],
+    showStats = false
   ) {
     this.scene = scene;
     this.camera = camera;
     this.renderer = renderer;
-    this.updatables = updatables;
-    this.statistics = stats ? new Statistics(this.renderer) : null;
+    this.tickables = tickables;
+    this.statistics = showStats ? new Statistics(this.renderer) : null;
   }
 
   start() {
     this.renderer.setAnimationLoop(() => {
       this.statistics?.begin();
 
-      // Tell every animated object to tick forward one frame
+      // Tell every tickable object to tick forward one frame
       this.tick();
 
       // Render a frame
@@ -49,8 +49,10 @@ export class Loop {
   tick() {
     const delta = clock.getDelta();
 
-    for (const object of this.updatables) {
-      object.tick(delta);
-    }
+    this.tickables.forEach((tickableObject) => {
+      if ("tick" in tickableObject) {
+        tickableObject.tick(delta);
+      }
+    });
   }
 }
