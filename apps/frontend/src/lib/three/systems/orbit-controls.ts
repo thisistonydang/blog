2d6bedc296422d3c93db012f1d37d4fa62173acb
Tony Dang <tony@tonydang.blog>
2023-04-27 12:06:31 -0700

    refactor typing

--- apps/frontend/src/lib/three/systems/orbit-controls.ts
+++ apps/frontend/src/lib/three/systems/orbit-controls.ts
@@ -1,13 +1,13 @@
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 import type { Camera } from "three";
-import type { UpdatableOrbitControls } from "./Loop.types";
+import type { Patched } from "../types/Patched";
 
 export function createOrbitControls(
   camera: Camera,
   canvas: HTMLCanvasElement
-): UpdatableOrbitControls {
-  const controls = new OrbitControls(camera, canvas) as UpdatableOrbitControls;
+): OrbitControls & Patched {
+  const controls: OrbitControls & Patched = new OrbitControls(camera, canvas);
   controls.enableDamping = true;
 
   controls.tick = (): void => {
--- apps/frontend/src/lib/three/systems/trackball-controls.ts
+++ apps/frontend/src/lib/three/systems/trackball-controls.ts
@@ -1,16 +1,16 @@
 import { TrackballControls } from "three/examples/jsm/controls/TrackballControls.js";
 
 import type { Camera } from "three";
-import type { UpdatableTrackballControls } from "./Loop.types";
+import type { Patched } from "../types/Patched";
 
 export function createTrackballControls(
   camera: Camera,
   canvas: HTMLCanvasElement
-): UpdatableTrackballControls {
-  const controls = new TrackballControls(
+): TrackballControls & Patched {
+  const controls: TrackballControls & Patched = new TrackballControls(
     camera,
     canvas
-  ) as UpdatableTrackballControls;
+  );
 
   controls.tick = (): void => {
     controls.update();
