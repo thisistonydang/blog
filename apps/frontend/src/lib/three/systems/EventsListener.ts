b786c3ecd3f39148bf2fc59efbd74530ef979302
Tony Dang <tony@tonydang.blog>
2023-04-27 13:19:26 -0700

    three/systems/EventsListener.ts: init add

---
+++ apps/frontend/src/lib/three/systems/EventsListener.ts
@@ -0,0 +1,38 @@
+import { Raycaster, Vector2 } from "three";
+import { clickListener } from "./click-listener";
+import { hoverListener } from "./hover-listener";
+
+import type { Camera, Intersection, Object3D } from "three";
+
+export class EventsListener {
+  raycaster = new Raycaster();
+  mouse = new Vector2(2, 2); // Set initial x and y to be > 1 so ray doesn't hit canvas on start up.
+  camera: Camera;
+  objectsToTest: Object3D[];
+
+  constructor(
+    canvas: HTMLCanvasElement,
+    camera: Camera,
+    objectsToTest: Object3D[]
+  ) {
+    this.camera = camera;
+    this.objectsToTest = objectsToTest;
+
+    // Track mouse position on canvas
+    canvas.addEventListener("mousemove", (e) => {
+      this.mouse.x = (e.offsetX / canvas.clientWidth) * 2 - 1;
+      this.mouse.y = -(e.offsetY / canvas.clientHeight) * 2 + 1;
+    });
+
+    // Listen for and handle mouse events
+    clickListener(this, canvas);
+    hoverListener(this, canvas);
+  }
+
+  castRay(): Intersection[] {
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    const intersects = this.raycaster.intersectObjects(this.objectsToTest);
+
+    return intersects;
+  }
+}
