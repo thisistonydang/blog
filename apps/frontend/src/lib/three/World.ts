bad5ec7230a950a7af2da57939b1b0bb83b210ad
Tony Dang <tony@tonydang.blog>
2023-04-28 10:37:51 -0700

    refactor World to be extensible by App

--- apps/frontend/src/lib/three/World.ts
+++ apps/frontend/src/lib/three/World.ts
@@ -1,72 +1,70 @@
-import { createCube } from "./components/cube";
-import { createPerspectiveCamera } from "./components/perspective-camera";
-import { createScene } from "./components/scene";
+import { createPerspectiveCamera } from "./components-core/perspective-camera";
+import { createRenderer } from "./components-core/renderer";
+import { createScene } from "./components-core/scene";
 
 import { EventsListener } from "./systems/EventsListener";
 import { Gui } from "./systems/Gui";
 import { Loop } from "./systems/Loop";
 import { Resizer } from "./systems/Resizer";
-import { createRenderer } from "./systems/renderer";
-import { createTrackballControls } from "./systems/trackball-controls";
-
-import { createAxesHelper } from "./helpers/axes-helper";
-import { createGridHelper } from "./helpers/grid-helper";
 
 import type { Object3D, PerspectiveCamera, Scene, WebGLRenderer } from "three";
 
-let scene: Scene;
-let camera: PerspectiveCamera;
-let renderer: WebGLRenderer;
-let eventsListener: EventsListener;
-let loop: Loop;
-let gui: Gui;
-
 export class World {
-  constructor(container: HTMLDivElement) {
-    scene = createScene();
-    camera = createPerspectiveCamera();
-    renderer = createRenderer();
-    container.append(renderer.domElement);
-    new Resizer(container, camera, renderer);
-    eventsListener = new EventsListener(renderer.domElement, camera);
-    loop = new Loop(scene, camera, renderer, true);
-    gui = new Gui(true);
-    createTrackballControls(camera, renderer.domElement, loop);
+  scene: Scene;
+  camera: PerspectiveCamera;
+  renderer: WebGLRenderer;
+  eventsListener: EventsListener;
+  loop: Loop;
+  gui: Gui;
 
-    const cube = createCube();
+  constructor(container: HTMLDivElement, showStats = false, showGui = false) {
+    // Create core components
+    this.scene = createScene();
+    this.camera = createPerspectiveCamera();
+    this.renderer = createRenderer();
+    container.append(this.renderer.domElement);
 
-    this.add([createAxesHelper(), createGridHelper(), cube]);
-    gui.init();
+    // Initialize systems
+    new Resizer(container, this.camera, this.renderer);
+    this.eventsListener = new EventsListener(
+      this.renderer.domElement,
+      this.camera
+    );
+    this.loop = new Loop(this.scene, this.camera, this.renderer, showStats);
+    this.gui = new Gui(showGui);
   }
 
+  /**
+   * Add objects to scene and relevant systems
+   */
   add(objects: Object3D[]): void {
     for (const object of objects) {
-      scene.add(object);
+      this.scene.add(object);
       if (
         "onClick" in object ||
         "onPointerEnter" in object ||
         "onPointerLeave" in object
       ) {
-        eventsListener.objectsToTest.push(object);
+        this.eventsListener.objectsToTest.push(object);
       }
       if ("tick" in object) {
-        loop.tickables.push(object);
+        this.loop.tickables.push(object);
       }
       if ("updateGui" in object) {
-        gui.tweakables.push(object);
+        this.gui.tweakables.push(object);
       }
     }
   }
 
   render(): void {
-    renderer.render(scene, camera);
+    this.renderer.render(this.scene, this.camera);
   }
 
   start(): void {
-    loop.start();
+    this.loop.start();
   }
 
   stop(): void {
-    loop.stop();
+    this.loop.stop();
   }
 }
