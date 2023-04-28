150b7acacd7039b5ac4acc8dc6765c5acc83547f
Tony Dang <tony@tonydang.blog>
2023-04-28 12:47:51 -0700

    Gui.ts: add helper method to create folders

--- apps/frontend/src/lib/three/systems/Gui.ts
+++ apps/frontend/src/lib/three/systems/Gui.ts
@@ -5,13 +5,33 @@ import type { Object3D } from "three";
 
 export class Gui {
   gui = new GUI();
+  togglesFolder: GUI;
+  folders: { [key: string]: boolean } = {};
   tweakables: Object3D[] = [];
+
+  constructor() {
+    this.togglesFolder = this.gui.addFolder("folder toggles");
+  }
 
   init(): void {
     this.tweakables.forEach((object) => {
       if (isPatched(object) && "updateGui" in object) {
-        object.updateGui(this.gui);
+        object.updateGui(this.createFolder);
       }
     });
   }
+
+  createFolder = (name: string, showFolder: boolean): GUI => {
+    // Create folder
+    const folder = this.gui.addFolder(name);
+    folder.show(showFolder);
+
+    // Create folder toggle
+    this.folders[name] = showFolder;
+    this.togglesFolder
+      .add(this.folders, name)
+      .onChange(() => folder.show(folder._hidden));
+
+    return folder;
+  };
 }
