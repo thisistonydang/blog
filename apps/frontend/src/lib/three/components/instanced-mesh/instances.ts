43f7c52169d40991c755d310f5e7b26ebbd2e253
Tony Dang <tony@tonydang.blog>
2023-04-28 17:31:57 -0700

    three/components/instanced-mesh/instances.ts: init add

---
+++ apps/frontend/src/lib/three/components/instanced-mesh/instances.ts
@@ -0,0 +1,14 @@
+export interface Instance {
+  id: string;
+  position: { x: number; y: number };
+  rotation?: number; // in degrees
+  scale?: { x: number; y: number; z: number };
+  color?: number;
+}
+
+export const instances: Instance[] = [
+  { id: "A", position: { x: -1.5, y: -1.5 } },
+  { id: "B", position: { x: -1.5, y: 1.5 } },
+  { id: "C", position: { x: 1.5, y: 1.5 } },
+  { id: "D", position: { x: 1.5, y: -1.5 } },
+];
