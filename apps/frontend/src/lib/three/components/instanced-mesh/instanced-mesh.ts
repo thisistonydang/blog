650d09c1596ffab6d92fc044e5f39b1d62eb6a87
Tony Dang <tony@tonydang.blog>
2023-04-29 14:41:17 -0700

    three/components/instanced-mesh/instanced-mesh.ts: init add

---
+++ apps/frontend/src/lib/three/components/instanced-mesh/instanced-mesh.ts
@@ -0,0 +1,52 @@
+import gsap from "gsap";
+import { BoxGeometry, InstancedMesh, MeshMatcapMaterial } from "three";
+
+import { addOnClickHandler } from "./add-on-click-handler";
+import { addOnHoverHandler } from "./add-on-hover-handler";
+import { addTick } from "./add-tick";
+import { INSTANCE_IDS_KEY } from "./get-instance";
+import { instances } from "./instances";
+import { updateInstanceColors } from "./update-instance-colors";
+import { updateInstanceMatrices } from "./update-instance-matrices";
+
+import type { Patched } from "../../types/Patched";
+
+export function createInstancedMesh(): InstancedMesh & Patched {
+  // Tweakable controls
+  const c = {
+    visible: true,
+    spin: () => {
+      gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
+    },
+  };
+
+  // Create mesh
+  const geometry = new BoxGeometry();
+  const material = new MeshMatcapMaterial();
+  const mesh: InstancedMesh & Patched = new InstancedMesh(
+    geometry,
+    material,
+    4
+  );
+  mesh.visible = c.visible;
+  mesh.userData[INSTANCE_IDS_KEY] = instances.map(({ id }) => id);
+  updateInstanceMatrices(mesh, instances);
+  updateInstanceColors(mesh, instances);
+
+  // Add event handlers
+  addOnClickHandler(mesh);
+  addOnHoverHandler(mesh);
+
+  // Add tick function
+  addTick(mesh);
+
+  // Add tweaks
+  mesh.updateGui = (createFolder) => {
+    const folder = createFolder("instanced mesh");
+
+    folder.add(c, "visible").onChange((v: boolean) => (mesh.visible = v));
+    folder.add(c, "spin");
+  };
+
+  return mesh;
+}
