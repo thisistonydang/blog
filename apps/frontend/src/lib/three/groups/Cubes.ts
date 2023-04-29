8a853710641bfe9fc72ac2006e26e637501343ca
Tony Dang <tony@tonydang.blog>
2023-04-29 16:31:04 -0700

    three/groups/Cubes.ts: init add

---
+++ apps/frontend/src/lib/three/groups/Cubes.ts
@@ -0,0 +1,43 @@
+import { MathUtils } from "three";
+
+import { createInstancedMesh } from "../components/instanced-mesh/instanced-mesh";
+import { createCube } from "../components/cube";
+import { BaseGroup } from "../systems/BaseGroup";
+
+import type { Gui } from "../systems/Gui";
+import type { World } from "../World";
+
+export class Cubes extends BaseGroup {
+  // Tweakable controls
+  c = {
+    visible: true,
+    rotate: false,
+  };
+
+  // Create group
+  constructor(world: World) {
+    super();
+
+    this.addObjects(world, [
+      createCube({ position: [-0.8, 0, 0] }),
+      createCube({ position: [0.8, 0, 0] }),
+      createInstancedMesh(),
+    ]);
+    this.visible = this.c.visible;
+  }
+
+  // Add tick function
+  tick(delta: number) {
+    if (this.c.rotate) {
+      this.rotation.y += MathUtils.degToRad(30) * delta;
+    }
+  }
+
+  // Add tweaks
+  updateGui(createFolder: Gui["createFolder"]) {
+    const folder = createFolder("cubes");
+
+    folder.add(this.c, "visible").onChange((v: boolean) => (this.visible = v));
+    folder.add(this.c, "rotate");
+  }
+}
