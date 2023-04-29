9af0b784c4177ca806026c514bf5b4e2ec850b86
Tony Dang <tony@tonydang.blog>
2023-04-29 14:14:40 -0700

    components/instanced-mesh/update-instance-matrices.ts: init add

---
+++ apps/frontend/src/lib/three/components/instanced-mesh/update-instance-matrices.ts
@@ -0,0 +1,48 @@
+import { Euler, MathUtils, Matrix4, Quaternion, Vector3 } from "three";
+
+import type { InstancedMesh } from "three";
+import type { Instance } from "./instances";
+
+const positionVector = new Vector3();
+const euler = new Euler();
+const quaternion = new Quaternion();
+const scaleVector = new Vector3();
+const matrix = new Matrix4();
+
+export function updateInstanceMatrices(
+  mesh: InstancedMesh,
+  instances: Instance[]
+): void {
+  instances.forEach(({ position, rotation, scale }, index) => {
+    // Set position
+    positionVector.set(position.x, position.y, 0);
+
+    // Set rotation
+    if (rotation) {
+      euler.set(
+        MathUtils.degToRad(rotation),
+        MathUtils.degToRad(rotation),
+        MathUtils.degToRad(rotation)
+      );
+    } else {
+      euler.set(
+        MathUtils.degToRad(0),
+        MathUtils.degToRad(0),
+        MathUtils.degToRad(0)
+      );
+    }
+    quaternion.setFromEuler(euler);
+
+    // Set scale
+    if (scale) {
+      scaleVector.set(scale.x, scale.y, scale.z);
+    } else {
+      scaleVector.set(1, 1, 1);
+    }
+
+    // Update instance matrix
+    matrix.compose(positionVector, quaternion, scaleVector);
+    mesh.setMatrixAt(index, matrix);
+    mesh.instanceMatrix.needsUpdate = true;
+  });
+}
