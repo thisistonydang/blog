1e2bcab7fc826ef49ffc68bc557ed6a174b1fc18
Tony Dang <tony@tonydang.blog>
2023-04-29 14:19:33 -0700

    three/components/instanced-mesh/add-on-click-handler.ts: init add

---
+++ apps/frontend/src/lib/three/components/instanced-mesh/add-on-click-handler.ts
@@ -0,0 +1,21 @@
+import { getInstance } from "./get-instance";
+import { instances } from "./instances";
+import { updateInstanceMatrices } from "./update-instance-matrices";
+
+import type { InstancedMesh } from "three";
+import type { Patched } from "@lib/three/types/Patched";
+
+export function addOnClickHandler(mesh: InstancedMesh & Patched): void {
+  mesh.onClick = (e) => {
+    const instance = getInstance(e);
+    if (!instance) return;
+
+    if (!instance.scale || (instance.scale && instance.scale.x === 1)) {
+      instance.scale = { x: 2, y: 2, z: 2 };
+    } else {
+      instance.scale = { x: 1, y: 1, z: 1 };
+    }
+
+    updateInstanceMatrices(mesh, instances);
+  };
+}
