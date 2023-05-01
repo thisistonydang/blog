a1e7dd9a66bb46fc98f6573915b466c49cee1487
Tony Dang <tony@tonydang.blog>
2023-05-01 09:58:34 -0700

    add tweaks for stats panel

--- apps/frontend/src/lib/three/systems/Statistics.ts
+++ apps/frontend/src/lib/three/systems/Statistics.ts
@@ -1,52 +1,93 @@
 import Stats from "stats.js";
 import type { WebGLRenderer } from "three";
-
-// Create custom stats panels
-const panelNames = [
-  "geometries",
-  "textures",
-  "programs",
-  "calls",
-  "lines",
-  "points",
-  "triangles",
-];
-const customPanels: { [key: string]: Stats.Panel } = {};
-panelNames.forEach((name) => {
-  customPanels[name] = new Stats.Panel(name, "#ff8", "#221");
-});
+import type { Gui } from "./Gui";
 
 export class Statistics {
+  // Tweakable controls
+  c = {
+    showStats: true,
+    statsPanel: 0,
+  };
+
+  allStatsPanels = [
+    "fps",
+    "ms",
+    "mb",
+    "geometries",
+    "textures",
+    "programs",
+    "calls",
+    "lines",
+    "points",
+    "triangles",
+  ];
+
+  customPanels: { [key: string]: Stats.Panel } = {};
+  stats = new Stats();
   renderer: WebGLRenderer;
-  stats: Stats;
 
   constructor(renderer: WebGLRenderer) {
     this.renderer = renderer;
-    this.stats = new Stats();
+
+    // Create custom stats panels
+    this.allStatsPanels.slice(3).forEach((panelName) => {
+      this.customPanels[panelName] = new Stats.Panel(panelName, "#ff8", "#221");
+    });
 
     // Add custom panels to stats panel
-    Object.values(customPanels).forEach((panel) => this.stats.addPanel(panel));
+    Object.values(this.customPanels).forEach((panel) =>
+      this.stats.addPanel(panel)
+    );
 
     // Add stats panel to DOM
-    this.stats.showPanel(0);
+    this.stats.dom.style.display = this.c.showStats ? "block" : "none";
+    this.stats.showPanel(this.c.statsPanel);
     document.body.appendChild(this.stats.dom);
   }
 
+  /**
+   * Begin stats collection
+   */
   begin(): void {
     this.stats.begin();
   }
 
+  /**
+   * End stats collection
+   */
   end(): void {
     this.stats.end();
   }
 
+  /**
+   * Update custom panels with renderer stats
+   */
   updateCustomPanels(): void {
-    customPanels.geometries?.update(this.renderer.info.memory.geometries, 50);
-    customPanels.textures?.update(this.renderer.info.memory.textures, 10);
-    customPanels.programs?.update(this.renderer.info.programs?.length ?? 0, 50);
-    customPanels.calls?.update(this.renderer.info.render.calls, 50);
-    customPanels.lines?.update(this.renderer.info.render.lines, 50);
-    customPanels.points?.update(this.renderer.info.render.points, 50);
-    customPanels.triangles?.update(this.renderer.info.render.triangles, 999999);
+    const rendererInfo = this.renderer.info;
+    this.customPanels.geometries?.update(rendererInfo.memory.geometries, 50);
+    this.customPanels.textures?.update(rendererInfo.memory.textures, 10);
+    this.customPanels.programs?.update(rendererInfo.programs?.length ?? 0, 50);
+    this.customPanels.calls?.update(rendererInfo.render.calls, 50);
+    this.customPanels.lines?.update(rendererInfo.render.lines, 50);
+    this.customPanels.points?.update(rendererInfo.render.points, 50);
+    this.customPanels.triangles?.update(rendererInfo.render.triangles, 999999);
+  }
+
+  updateGui({ devFolder }: Gui): void {
+    // Add display toggle for stats panel
+    devFolder.add(this.c, "showStats").onChange((showStats: boolean) => {
+      this.stats.dom.style.display = showStats ? "block" : "none";
+    });
+
+    // Create panel options
+    const options: { [key: string]: number } = {};
+    this.allStatsPanels.forEach((name, index) => {
+      options[name] = index;
+    });
+
+    // Add panel options to gui
+    devFolder.add(this.c, "statsPanel", options).onChange((panel: number) => {
+      this.stats.showPanel(panel);
+    });
   }
 }
