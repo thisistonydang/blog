2861b6ee9e29c3e4f2ebbf635b91bcf48432623b
Tony Dang <tony@tonydang.blog>
2023-05-01 12:39:28 -0700

    reafactor

--- apps/frontend/src/lib/three/systems/Events.ts
+++ apps/frontend/src/lib/three/systems/Events.ts
@@ -2,16 +2,19 @@ import { Raycaster, Vector2 } from "three";
 import { clickListener } from "./click-listener";
 import { hoverListener } from "./hover-listener";
 
-import type { Camera, Intersection, Object3D } from "three";
+import type { Intersection, Object3D } from "three";
+import type { World } from "../World";
 
 export class Events {
   raycaster = new Raycaster();
   mouse = new Vector2(2, 2); // Set initial x and y to be > 1 so ray doesn't hit canvas on start up.
-  camera: Camera;
+  world: World;
   objectsToTest: Object3D[] = [];
 
-  constructor(canvas: HTMLCanvasElement, camera: Camera) {
-    this.camera = camera;
+  constructor(world: World) {
+    this.world = world;
+
+    const canvas = world.renderer.domElement;
 
     // Track mouse position on canvas
     canvas.addEventListener("mousemove", (e) => {
@@ -20,14 +23,14 @@ export class Events {
     });
 
     // Listen for and handle mouse events
-    clickListener(this, canvas);
-    hoverListener(this, canvas);
+    clickListener(this);
+    hoverListener(this);
   }
 
-  castRay(): Intersection[] {
-    this.raycaster.setFromCamera(this.mouse, this.camera);
+  castRay = (): Intersection[] => {
+    this.raycaster.setFromCamera(this.mouse, this.world.camera);
     const intersects = this.raycaster.intersectObjects(this.objectsToTest);
 
     return intersects;
-  }
+  };
 }
