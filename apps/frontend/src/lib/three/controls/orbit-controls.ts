1c863d8b539a6331ef47aacf362c70153aef1a7f
Tony Dang <tony@tonydang.blog>
2023-05-01 12:24:33 -0700

    three/controls/orbit-controls.ts: add on change listener to request render when rendering on demand

--- apps/frontend/src/lib/three/controls/orbit-controls.ts
+++ apps/frontend/src/lib/three/controls/orbit-controls.ts
@@ -1,22 +1,33 @@
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-import type { Camera } from "three";
-import type { Loop } from "../systems/Loop";
 import type { Patched } from "../types/Patched";
+import type { World } from "../World";
 
-export function createOrbitControls(
-  camera: Camera,
-  canvas: HTMLCanvasElement,
-  loop: Loop
-): OrbitControls & Patched {
-  const controls: OrbitControls & Patched = new OrbitControls(camera, canvas);
+export function createOrbitControls({
+  camera,
+  renderer,
+  loop,
+}: World): OrbitControls & Patched {
+  // Create controls
+  const controls: OrbitControls & Patched = new OrbitControls(
+    camera,
+    renderer.domElement
+  );
   controls.enableDamping = true;
 
+  // Update controls each tick to allow damping
   controls.tick = (): void => {
     controls.update();
   };
-
   loop.tickables.push(controls);
+
+  // When rendering on demand, request a render on change events if a render has
+  // not already been requested.
+  controls.addEventListener("change", () => {
+    if (loop.frameloop === "demand") {
+      loop.requestRender();
+    }
+  });
 
   return controls;
 }
