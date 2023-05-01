75c41e72f8dfd5cc3cb903c8adf04fb5652b1f44
Tony Dang <tony@tonydang.blog>
2023-05-01 11:30:55 -0700

    three/systems/Resizer.ts: request render on window resize if rendering on demand

--- apps/frontend/src/lib/three/systems/Resizer.ts
+++ apps/frontend/src/lib/three/systems/Resizer.ts
@@ -1,36 +1,33 @@
-import type { PerspectiveCamera, WebGLRenderer } from "three";
-
-function setSize(
-  container: HTMLDivElement,
-  camera: PerspectiveCamera,
-  renderer: WebGLRenderer
-): void {
-  // Set the camera's aspect ratio.
-  camera.aspect = container.clientWidth / container.clientHeight;
-
-  // Update the camera's frustum.
-  camera.updateProjectionMatrix();
-
-  // Update the size of the renderer.
-  renderer.setSize(container.clientWidth, container.clientHeight);
-
-  // Set the pixel ratio. Note: pixel ratio may change when moving across
-  // different monitors.
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-}
+import type { World } from "../World";
 
 export class Resizer {
-  constructor(
-    container: HTMLDivElement,
-    camera: PerspectiveCamera,
-    renderer: WebGLRenderer
-  ) {
+  constructor(world: World, container: HTMLDivElement) {
     // Set initial size on load.
-    setSize(container, camera, renderer);
+    this.setSize(world, container);
 
-    // Set the size again if a resize occurs.
+    // Set size again if a resize event occurs.
     window.addEventListener("resize", () => {
-      setSize(container, camera, renderer);
+      this.setSize(world, container);
+
+      // If rendering on demand, request a render.
+      if (world.loop.frameloop === "demand") {
+        world.requestRender();
+      }
     });
+  }
+
+  setSize({ camera, renderer }: World, container: HTMLDivElement): void {
+    // Set the camera's aspect ratio.
+    camera.aspect = container.clientWidth / container.clientHeight;
+
+    // Update the camera's frustum.
+    camera.updateProjectionMatrix();
+
+    // Update the size of the renderer.
+    renderer.setSize(container.clientWidth, container.clientHeight);
+
+    // Set the pixel ratio. Note: pixel ratio may change when moving across
+    // different monitors.
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   }
 }
