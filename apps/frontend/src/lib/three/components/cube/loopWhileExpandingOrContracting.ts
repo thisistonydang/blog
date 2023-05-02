3cbdc1ef6a3330d33025f65c1b8cd3de3f4bbe79
Tony Dang <tony@tonydang.blog>
2023-05-02 14:03:26 -0700

    init add

---
+++ apps/frontend/src/lib/three/components/cube/loopWhileExpandingOrContracting.ts
@@ -0,0 +1,22 @@
+import { MathUtils } from "three";
+import { approxEq } from "@lib/math/approxEq";
+
+import type { Mesh } from "three";
+import type { World } from "../../World";
+import type { Patched } from "../../types/Patched";
+import type { Controls } from "./index";
+
+export function loopWhileExpandingOrContracting(
+  world: World,
+  c: Controls,
+  mesh: Mesh & Patched
+) {
+  world.loop.runWhile(
+    () => c.isExpanding && !approxEq(mesh.scale.x, 3),
+    () => (mesh.scale.x = MathUtils.lerp(mesh.scale.x, 3, 0.1))
+  );
+  world.loop.runWhile(
+    () => !c.isExpanding && !approxEq(mesh.scale.x, 1),
+    () => (mesh.scale.x = MathUtils.lerp(mesh.scale.x, 1, 0.1))
+  );
+}
---
+++ apps/frontend/src/lib/three/components/cube/onClick.ts
@@ -0,0 +1,21 @@
+import { loopWhileExpandingOrContracting } from "./loopWhileExpandingOrContracting";
+
+import type { Mesh } from "three";
+import type { Patched } from "../../types/Patched";
+import type { World } from "../../World";
+import type { Controls } from "./index";
+
+export function onClick({
+  world,
+  c,
+  mesh,
+}: {
+  world: World;
+  c: Controls;
+  mesh: Mesh & Patched;
+}): void {
+  mesh.onClick = () => {
+    c.isExpanding = !c.isExpanding;
+    loopWhileExpandingOrContracting(world, c, mesh);
+  };
+}
