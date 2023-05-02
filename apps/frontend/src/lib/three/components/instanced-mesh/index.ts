8b1abc7f259072c996b874d35b97488e7eee36f0
Tony Dang <tony@tonydang.blog>
2023-05-02 12:38:17 -0700

    refactor

---
+++ apps/frontend/src/lib/three/components/instanced-mesh/index.ts
@@ -0,0 +1,63 @@
+import gsap from "gsap";
+import { InstancedMesh, MeshMatcapMaterial } from "three";
+import { approxEq } from "@lib/math/approxEq";
+
+import { boxGeometry } from "../../geometries/box";
+
+import { instances } from "./instances";
+import { onClick } from "./onClick";
+import { onHover } from "./onHover";
+import { tick } from "./tick";
+import { updateGui } from "./updateGui";
+import { updateInstanceColors } from "./updateInstanceColors";
+import { updateInstanceMatrices } from "./updateInstanceMatrices";
+
+import type { Patched } from "../../types/Patched";
+import type { World } from "../../World";
+
+export interface Controls {
+  visible: boolean;
+  spin: () => void;
+}
+
+export const INSTANCE_IDS_KEY = "instance_ids";
+
+export function createInstancedMesh({
+  world,
+}: {
+  world: World;
+}): InstancedMesh & Patched {
+  // Controls
+  const c: Controls = {
+    visible: true,
+    spin: () => {
+      const end = mesh.rotation.y + Math.PI * 2;
+      gsap.to(mesh.rotation, { duration: 1, y: end });
+      world.loop.runWhile(() => !approxEq(mesh.rotation.y, end));
+    },
+  };
+
+  // Create mesh
+  const material = new MeshMatcapMaterial();
+  const mesh: InstancedMesh & Patched = new InstancedMesh(
+    boxGeometry,
+    material,
+    4
+  );
+  mesh.visible = c.visible;
+  mesh.userData[INSTANCE_IDS_KEY] = instances.map(({ id }) => id);
+  updateInstanceMatrices(mesh, instances);
+  updateInstanceColors(mesh, instances);
+
+  // Add event handlers
+  onClick({ mesh });
+  onHover({ mesh });
+
+  // Add tick function
+  tick({ mesh });
+
+  // Add tweaks
+  updateGui({ c, mesh });
+
+  return mesh;
+}
