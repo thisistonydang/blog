cf0043a59465b52746e1cdca6e2aacf5a61bb2d9
Tony Dang <tony@tonydang.blog>
2023-05-02 14:04:57 -0700

    init add

---
+++ apps/frontend/src/lib/three/components/cube/onHover.ts
@@ -0,0 +1,23 @@
+import type { Mesh, MeshMatcapMaterial } from "three";
+import type { Patched } from "../../types/Patched";
+import type { Controls } from "./index";
+
+export function onHover({
+  c,
+  material,
+  mesh,
+}: {
+  c: Controls;
+  material: MeshMatcapMaterial;
+  mesh: Mesh & Patched;
+}): void {
+  mesh.onPointerEnter = () => {
+    material.color.set(0xff0000);
+    document.body.style.cursor = "pointer";
+  };
+
+  mesh.onPointerLeave = () => {
+    document.body.style.cursor = "default";
+    material.color.set(c.color);
+  };
+}
