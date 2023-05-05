a7f5a5a5f538dd9d1f58178735fe5d8d65964cf5
Tony Dang <tony@tonydang.blog>
2023-05-05 11:42:04 -0700

    add camera tweaks

--- apps/frontend/src/lib/three/controls/orbit-controls.ts
+++ apps/frontend/src/lib/three/controls/orbit-controls.ts
@@ -3,23 +3,30 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import type { Patched } from "../types/Patched";
 import type { World } from "../World";
 
+interface Controls {
+  enableDamping: boolean;
+}
+
 export function createOrbitControls({
   camera,
   renderer,
   loop,
 }: World): OrbitControls & Patched {
+  const c: Controls = {
+    enableDamping: true,
+  };
+
   // Create controls
   const controls: OrbitControls & Patched = new OrbitControls(
     camera,
     renderer.domElement
   );
-  controls.enableDamping = true;
+  controls.enableDamping = c.enableDamping;
 
   // Update controls each tick to allow damping
   controls.tick = () => {
     controls.update();
   };
-  loop.tickables.push(controls);
 
   // When rendering on demand, request a render on change events if a render has
   // not already been requested.
@@ -28,6 +35,13 @@ export function createOrbitControls({
       loop.requestRender();
     }
   });
+
+  controls.updateGui = ({ createFolder }) => {
+    const folder = createFolder("camera");
+    folder.add(c, "enableDamping").onChange((enableDamping: boolean) => {
+      controls.enableDamping = enableDamping;
+    });
+  };
 
   return controls;
 }
