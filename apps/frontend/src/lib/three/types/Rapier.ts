ad6a1b59c4f4095187e1e1d5d38c25632e6c700e
Tony Dang <tony@tonydang.blog>
2023-05-05 10:51:14 -0700

    init add

---
+++ apps/frontend/src/lib/three/types/Rapier.ts
@@ -0,0 +1,5 @@
+import type RAPIER_2D from "@dimforge/rapier2d-compat";
+import type RAPIER_3D from "@dimforge/rapier3d-compat";
+
+export type Rapier2D = typeof RAPIER_2D;
+export type Rapier3D = typeof RAPIER_3D;
