bdb319d36dbc16844c207eb71d466eef7d3222c8
Tony Dang <tony@tonydang.blog>
2023-05-05 11:58:45 -0700

    World.ts: add physics, auto init physics and gui after adding objects, add controls as tweakable object

--- apps/frontend/src/lib/three/World.ts
+++ apps/frontend/src/lib/three/World.ts
@@ -1,13 +1,23 @@
+import { Object3D } from "three";
+
 import { createPerspectiveCamera } from "./components-core/perspective-camera";
 import { createRenderer } from "./components-core/renderer";
 import { createScene } from "./components-core/scene";
 
 import { Gui } from "./systems/Gui";
 import { Loop } from "./systems/Loop";
+import { Physics2D } from "./systems/Physics2D";
+import { Physics3D } from "./systems/Physics3D";
 import { Pointer } from "./systems/Pointer";
 import { Resizer } from "./systems/Resizer";
 
-import type { Object3D, PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import type {
+  EventDispatcher,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+import type { Rapier2D, Rapier3D } from "./types/Rapier";
 
 export class World {
   scene: Scene;
@@ -15,46 +25,72 @@ export class World {
   renderer: WebGLRenderer;
   pointer: Pointer;
   loop: Loop;
+  physics: Physics2D | Physics3D | null = null;
   gui: Gui | null = null;
 
-  constructor(container: HTMLDivElement) {
+  constructor(container: HTMLDivElement, RAPIER?: Rapier2D | Rapier3D) {
     // Create core components
     this.scene = createScene();
     this.camera = createPerspectiveCamera();
     this.renderer = createRenderer();
     container.append(this.renderer.domElement);
 
-    // Initialize systems
+    // Create systems
     new Resizer(this, container);
     this.pointer = new Pointer(this);
     this.loop = new Loop(this);
+
+    if (RAPIER !== undefined) {
+      if ("Vector2" in RAPIER) {
+        this.physics = new Physics2D(this, RAPIER);
+      } else if ("Vector3" in RAPIER) {
+        this.physics = new Physics3D(this, RAPIER);
+      }
+
+      if (this.physics) {
+        this.loop.tickables.push(this.physics);
+      }
+    }
+
     this.gui = new Gui(this);
   }
 
-  /**
-   * Add objects to root scene and relevant systems
-   */
-  addObjects(objects: Object3D[]): void {
+  addObjects(objects: (EventDispatcher | Object3D)[]): void {
+    // Add objects to root scene and relevant systems
     objects.forEach((object) => {
-      this.scene.add(object);
+      if (object instanceof Object3D) {
+        this.scene.add(object);
+      }
       this.updateSystems(object);
     });
+
+    // Initialize physics and GUI after all objects have been added
+    this.physics?.init();
+    this.gui?.init();
   }
 
   /**
    * Add an object to relevant systems
    */
-  updateSystems(object: Object3D): void {
-    if (
-      "onClick" in object ||
-      "onPointerEnter" in object ||
-      "onPointerLeave" in object
-    ) {
-      this.pointer.objectsToTest.push(object);
+  updateSystems(object: EventDispatcher | Object3D): void {
+    if (object instanceof Object3D) {
+      if ("addPhysics" in object) {
+        this.physics?.objects.push(object);
+      }
+
+      if (
+        "onClick" in object ||
+        "onPointerEnter" in object ||
+        "onPointerLeave" in object
+      ) {
+        this.pointer.objectsToTest.push(object);
+      }
     }
+
     if ("tick" in object) {
       this.loop.tickables.push(object);
     }
+
     if ("updateGui" in object) {
       this.gui?.tweakables.push(object);
     }
