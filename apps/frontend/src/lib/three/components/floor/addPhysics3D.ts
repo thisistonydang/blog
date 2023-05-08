c533911bbd8484f7d0ca2144d148e9239f973f47
Tony Dang <tony@tonydang.blog>
2023-05-08 16:28:13 -0700

    init add

---
+++ apps/frontend/src/lib/three/components/floor/addPhysics3D.ts
@@ -0,0 +1,20 @@
+import { addMesh } from "@lib/three/systems/Physics/3D/addMesh";
+import { cuboidColliderDesc } from "@lib/three/systems/Physics/3D/cuboidColliderDesc";
+
+import type { BoxGeometry, Mesh } from "three";
+import type { Patched } from "../../types/Patched";
+
+export function addPhysics3D({
+  mesh,
+}: {
+  mesh: Mesh<BoxGeometry> & Patched;
+}): void {
+  mesh.addPhysics3D = ({ RAPIER, physicsWorld }) => {
+    addMesh({
+      physicsWorld,
+      mesh,
+      rigidBodyDesc: RAPIER.RigidBodyDesc.fixed(),
+      colliderDesc: cuboidColliderDesc(RAPIER, mesh),
+    });
+  };
+}
