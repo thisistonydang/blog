3ea58bb1a7a839907e9f65e8dee09b5511798632
Tony Dang <tony@tonydang.blog>
2023-05-08 16:16:33 -0700

    init add

---
+++ apps/frontend/src/lib/three/systems/Physics/3D/addInstancedMesh.ts
@@ -0,0 +1,65 @@
+import { Euler, Quaternion } from "three";
+
+import {
+  cuboidColliderDesc,
+  hasCuboidGeometry,
+} from "../3D/cuboidColliderDesc";
+
+import type { World as PhysicsWorld } from "@dimforge/rapier3d-compat";
+import type { InstancedMesh } from "three";
+
+import type {
+  PhysicsBody,
+  PhysicsInstance,
+  Rapier3D,
+} from "../../../types/Rapier3D";
+
+const euler = new Euler();
+const quaternion = new Quaternion();
+
+export function addInstancedMesh({
+  RAPIER,
+  physicsWorld,
+  instancedMesh,
+  instances,
+}: {
+  RAPIER: Rapier3D;
+  physicsWorld: PhysicsWorld;
+  instancedMesh: InstancedMesh;
+  instances: PhysicsInstance[];
+}): void {
+  const physicsBodies: PhysicsBody[] = [];
+
+  instances.forEach(
+    ({ position, rotation, scale, rigidBodyDesc, restitution }) => {
+      // Describe rigid body
+      rigidBodyDesc
+        .setTranslation(position.x, position.y, position.z)
+        .setRotation(
+          quaternion.setFromEuler(euler.set(rotation.x, rotation.y, rotation.z))
+        );
+
+      // Create rigid body
+      const rigidBody = physicsWorld.createRigidBody(rigidBodyDesc);
+
+      // Describe collider
+      let colliderDesc;
+
+      if (hasCuboidGeometry(instancedMesh)) {
+        colliderDesc = cuboidColliderDesc(RAPIER, instancedMesh, scale);
+      }
+
+      if (!colliderDesc) throw new Error("Failed to create a colliderDesc.");
+
+      restitution && colliderDesc.setRestitution(restitution);
+
+      // Create collider
+      const collider = physicsWorld.createCollider(colliderDesc, rigidBody);
+
+      physicsBodies.push({ rigidBody, collider });
+    }
+  );
+
+  // Store rigidBody and collider in instancedMesh userData
+  instancedMesh.userData.physicsBodies = physicsBodies;
+}
