6c261217c61da6246263d24e45cde777ce82e313
Tony Dang <tony@tonydang.blog>
2023-05-08 16:27:16 -0700

    init add

---
+++ apps/frontend/src/lib/three/components/floor/floor.ts
@@ -0,0 +1,62 @@
+import gsap from "gsap";
+import { BoxGeometry, Mesh, MeshMatcapMaterial } from "three";
+
+import { approxEq } from "@lib/math/approxEq";
+import { boxGeometry } from "../../geometries/box";
+
+import { updateGui } from "./updateGui";
+import { addPhysics3D } from "./addPhysics3D";
+
+import type { Patched } from "../../types/Patched";
+import type { World } from "../../World";
+
+export interface Controls {
+  color: number;
+  positionX: number;
+  positionY: number;
+  positionZ: number;
+  width: number;
+  height: number;
+  depth: number;
+  visible: boolean;
+  spin: () => void;
+}
+
+export function floor({
+  world,
+  position = [0, 0, 0],
+}: {
+  world: World;
+  position?: [number, number, number];
+}): Mesh<BoxGeometry> & Patched {
+  const c: Controls = {
+    color: 0xffffff,
+    positionX: position[0],
+    positionY: position[1],
+    positionZ: position[2],
+    width: 50,
+    height: 1,
+    depth: 50,
+    visible: true,
+    spin: () => {
+      const end = mesh.rotation.y + Math.PI * 2;
+      gsap.to(mesh.rotation, { duration: 1, y: end });
+      world.loop.runWhile(() => !approxEq(mesh.rotation.y, end));
+    },
+  };
+
+  // Create mesh
+  const material = new MeshMatcapMaterial({ color: c.color });
+  const mesh: Mesh<BoxGeometry> & Patched = new Mesh(boxGeometry, material);
+  mesh.position.set(c.positionX, c.positionY, c.positionZ);
+  mesh.scale.set(c.width, c.height, c.depth);
+  mesh.visible = c.visible;
+
+  // Add physics
+  addPhysics3D({ mesh });
+
+  // Add tweaks
+  updateGui({ c, material, mesh });
+
+  return mesh;
+}
---
+++ apps/frontend/src/lib/three/components/floor/updateGui.ts
@@ -0,0 +1,46 @@
+import type { Mesh, MeshMatcapMaterial } from "three";
+import type { Patched } from "../../types/Patched";
+import type { Controls } from "./floor";
+
+export function updateGui({
+  c,
+  material,
+  mesh,
+}: {
+  c: Controls;
+  material: MeshMatcapMaterial;
+  mesh: Mesh & Patched;
+}): void {
+  mesh.updateGui = ({ createFolder }) => {
+    const folder = createFolder("floor");
+
+    // Color
+    folder.addColor(c, "color").onChange((v: string) => material.color.set(v));
+
+    // Position
+    folder
+      .add(c, "positionX", -10, 10, 0.1)
+      .onChange((v: number) => (mesh.position.x = v));
+    folder
+      .add(c, "positionY", -10, 10, 0.1)
+      .onChange((v: number) => (mesh.position.y = v));
+    folder
+      .add(c, "positionZ", -10, 10, 0.1)
+      .onChange((v: number) => (mesh.position.z = v));
+
+    // Scale
+    folder
+      .add(c, "width", 1, 100, 1)
+      .onChange((v: number) => (mesh.scale.x = v));
+    folder
+      .add(c, "height", 1, 100, 1)
+      .onChange((v: number) => (mesh.scale.y = v));
+    folder
+      .add(c, "depth", 1, 100, 1)
+      .onChange((v: number) => (mesh.scale.z = v));
+
+    // Misc
+    folder.add(c, "visible").onChange((v: boolean) => (mesh.visible = v));
+    folder.add(c, "spin");
+  };
+}
