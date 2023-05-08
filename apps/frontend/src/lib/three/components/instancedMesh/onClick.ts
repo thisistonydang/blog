c02ab42e11af4b400b2af0c174e0e8284c38801e
Tony Dang <tony@tonydang.blog>
2023-05-08 09:15:18 -0700

    refactor

--- apps/frontend/src/lib/three/components/instancedMesh/onClick.ts
+++ apps/frontend/src/lib/three/components/instancedMesh/onClick.ts
@@ -10,7 +10,7 @@ export function onClick({ mesh }: { mesh: InstancedMesh & Patched }): void {
     const instance = getInstance(intersection);
     if (!instance) return;
 
-    if (!instance.scale || (instance.scale && instance.scale.x === 1)) {
+    if (instance.scale.x === 1) {
       instance.scale = { x: 2, y: 2, z: 2 };
     } else {
       instance.scale = { x: 1, y: 1, z: 1 };
