c293ebb04e4a072c9909cee68ecdd5f5c1a61705
Tony Dang <tony@tonydang.blog>
2023-05-08 09:37:28 -0700

    refactor

--- apps/frontend/src/lib/three/components/instancedMesh/instances.ts
+++ apps/frontend/src/lib/three/components/instancedMesh/instances.ts
@@ -1,10 +1,4 @@
-export interface Instance {
-  id: string;
-  position: { x: number; y: number; z: number };
-  rotation: { x: number; y: number; z: number };
-  scale: { x: number; y: number; z: number };
-  color: number;
-}
+import type { Instance } from "@lib/three/types/Instance";
 
 const rawInstances = [
   { id: "A", position: { x: -1.5, y: -1.5 } },
