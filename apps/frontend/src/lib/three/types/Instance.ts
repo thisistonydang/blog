29b56b2e1a09c1fb9c21191d5a67f5e85837df25
Tony Dang <tony@tonydang.blog>
2023-05-08 09:33:35 -0700

    refactor

---
+++ apps/frontend/src/lib/three/types/Instance.ts
@@ -0,0 +1,7 @@
+export interface Instance {
+  id: string;
+  position: { x: number; y: number; z: number };
+  rotation: { x: number; y: number; z: number };
+  scale: { x: number; y: number; z: number };
+  color: number;
+}
