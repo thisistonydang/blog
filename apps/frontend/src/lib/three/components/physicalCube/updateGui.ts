865d113b7f074f0798da44e593e9dfa5a61af595
Tony Dang <tony@tonydang.blog>
2023-05-08 16:32:47 -0700

    init add

---
+++ apps/frontend/src/lib/three/components/physicalCube/updateGui.ts
@@ -0,0 +1,46 @@
+import type { Mesh, MeshMatcapMaterial } from "three";
+import type { Patched } from "../../types/Patched";
+import type { Controls } from "./physicalCube";
+
+export function updateGui({
+  c,
+  material,
+  mesh,
+}: {
+  c: Controls;
+  material: MeshMatcapMaterial;
+  mesh: Mesh & Patched;
+}): void {
+  mesh.updateGui = ({ createFolder }) => {
+    const folder = createFolder("physical cube");
+
+    // Color
+    folder.addColor(c, "color").onChange((v: string) => material.color.set(v));
+
+    // Position
+    folder
+      .add(c, "positionX", -10, 10, 0.1)
+      .onChange((v: number) => (mesh.position.x = v));
+    folder
+      .add(c, "positionY", -10, 10, 0.1)
+      .onChange((v: number) => (mesh.position.y = v));
+    folder
+      .add(c, "positionZ", -10, 10, 0.1)
+      .onChange((v: number) => (mesh.position.z = v));
+
+    // Scale
+    folder
+      .add(c, "width", 1, 10, 1)
+      .onChange((v: number) => (mesh.scale.x = v));
+    folder
+      .add(c, "height", 1, 10, 1)
+      .onChange((v: number) => (mesh.scale.y = v));
+    folder
+      .add(c, "depth", 1, 10, 1)
+      .onChange((v: number) => (mesh.scale.z = v));
+
+    // Misc
+    folder.add(c, "visible").onChange((v: boolean) => (mesh.visible = v));
+    folder.add(c, "spin");
+  };
+}
