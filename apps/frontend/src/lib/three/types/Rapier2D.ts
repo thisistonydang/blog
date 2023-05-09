c8554760c7c99f7f1529f290f577ea886e91c899
Tony Dang <tony@tonydang.blog>
2023-05-09 13:39:23 -0700

    update typing for Rapier2D to be consistent with Rapier3D

--- apps/frontend/src/lib/three/types/Rapier2D.ts
+++ apps/frontend/src/lib/three/types/Rapier2D.ts
@@ -1,9 +1,24 @@
 import type RAPIER from "@dimforge/rapier2d-compat";
-import type { Collider, RigidBody } from "@dimforge/rapier2d-compat";
+
+import type {
+  Collider,
+  ColliderDesc,
+  RigidBody,
+  RigidBodyDesc,
+} from "@dimforge/rapier2d-compat";
+
+import type { Instance } from "./Instance";
 
 export type Rapier2D = typeof RAPIER;
 
+export interface PhysicsInstance extends Instance {
+  rigidBodyDesc: RigidBodyDesc;
+  colliderDesc?: ColliderDesc;
+  restitution?: number;
+}
+
 export interface PhysicsBody {
+  id: string;
   rigidBody: RigidBody;
   collider: Collider;
 }
