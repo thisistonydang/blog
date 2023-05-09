379e1ea4765b7ceaafa3dfa9438963c26aff1512
Tony Dang <tony@tonydang.blog>
2023-05-08 17:28:16 -0700

    init add

---
+++ apps/frontend/src/lib/three/components/physcialCubes/instances.ts
@@ -0,0 +1,24 @@
+import RAPIER from "@dimforge/rapier3d-compat";
+
+import type { PhysicsInstance } from "@lib/three/types/Rapier3D";
+
+export const rawInstances = [
+  { id: "A", position: { x: -1.5, y: -1.5 } },
+  { id: "B", position: { x: -1.5, y: 1.5 } },
+  { id: "C", position: { x: 1.5, y: 1.5 } },
+  { id: "D", position: { x: 1.5, y: -1.5 } },
+];
+
+export const instances: PhysicsInstance[] = rawInstances.map(
+  ({ id, position }) => {
+    return {
+      id,
+      position: { ...position, z: 0 },
+      rotation: { x: 0, y: 0, z: 0 },
+      scale: { x: 0.25, y: 1, z: 1 },
+      color: 0x00ffff,
+      rigidBodyDesc: RAPIER.RigidBodyDesc.dynamic(),
+      restitution: 1,
+    };
+  }
+);
