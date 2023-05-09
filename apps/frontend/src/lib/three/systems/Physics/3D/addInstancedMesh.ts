5cb4a3e142c778c4da35dad49670e9358741e57f
Tony Dang <tony@tonydang.blog>
2023-05-09 12:20:22 -0700

    update addInstanceMesh to only add to movableObjects if not all instances are fixed

--- apps/frontend/src/lib/three/systems/Physics/3D/addInstancedMesh.ts
+++ apps/frontend/src/lib/three/systems/Physics/3D/addInstancedMesh.ts
@@ -1,23 +1,23 @@
 import { Euler, Quaternion } from "three";
 
-import type { World as PhysicsWorld } from "@dimforge/rapier3d-compat";
 import type { InstancedMesh } from "three";
-
 import type { PhysicsBody, PhysicsInstance } from "../../../types/Rapier3D";
+import type { Physics3D } from "../Physics3D";
 
 const euler = new Euler();
 const quaternion = new Quaternion();
 
 export function addInstancedMesh({
-  physicsWorld,
+  physics,
   instancedMesh,
   instances,
 }: {
-  physicsWorld: PhysicsWorld;
+  physics: Physics3D;
   instancedMesh: InstancedMesh;
   instances: PhysicsInstance[];
 }): void {
   const physicsBodies: PhysicsBody[] = [];
+  let allInstancesAreFixed = true;
 
   instances.forEach(
     ({ id, position, rotation, rigidBodyDesc, colliderDesc, restitution }) => {
@@ -29,19 +29,31 @@ export function addInstancedMesh({
         );
 
       // Create rigid body
-      const rigidBody = physicsWorld.createRigidBody(rigidBodyDesc);
+      const rigidBody = physics.physicsWorld.createRigidBody(rigidBodyDesc);
 
       // Describe collider
       if (!colliderDesc) throw new Error("No colliderDesc set on instance.");
       restitution && colliderDesc.setRestitution(restitution);
 
       // Create collider
-      const collider = physicsWorld.createCollider(colliderDesc, rigidBody);
+      const collider = physics.physicsWorld.createCollider(
+        colliderDesc,
+        rigidBody
+      );
 
       physicsBodies.push({ id, rigidBody, collider });
+
+      if (!rigidBody.isFixed()) {
+        allInstancesAreFixed = false;
+      }
     }
   );
 
   // Store rigidBody and collider in instancedMesh userData
   instancedMesh.userData.physicsBodies = physicsBodies;
+
+  // Add instancedMesh to movableObjects if not all instances are fixed
+  if (!allInstancesAreFixed) {
+    physics.movableObjects.push(instancedMesh);
+  }
 }
