ced1daef40f3ef6ecca3a4bd49e2ca1e531a0a95
Tony Dang <tony@tonydang.blog>
2023-05-09 12:24:29 -0700

    init add

---
+++ apps/frontend/src/lib/three/components/physcialCubes/onHover.ts
@@ -0,0 +1,30 @@
+import { getInstance } from "@lib/three/utils/instances/getInstance";
+import { updateInstanceColors } from "@lib/three/utils/instances/updateInstanceColors";
+import { instances } from "./instances";
+
+import type { InstancedMesh } from "three";
+import type { Patched } from "@lib/three/types/Patched";
+
+export function onHover({
+  instancedMesh,
+}: {
+  instancedMesh: InstancedMesh & Patched;
+}): void {
+  instancedMesh.onPointerEnter = ({ intersection }) => {
+    document.body.style.cursor = "pointer";
+    const instance = getInstance(intersection, instances);
+    if (!instance) return;
+
+    instance.color = 0xff00ff;
+    updateInstanceColors(instancedMesh, instances);
+  };
+
+  instancedMesh.onPointerLeave = ({ intersection }) => {
+    document.body.style.cursor = "default";
+    const instance = getInstance(intersection, instances);
+    if (!instance) return;
+
+    instance.color = 0x00ffff;
+    updateInstanceColors(instancedMesh, instances);
+  };
+}
