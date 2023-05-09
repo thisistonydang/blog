ce8d634da71ee3829d5e0fabd07eeb555fa637cb
Tony Dang <tony@tonydang.blog>
2023-05-09 13:58:55 -0700

    add 2D physics to cube

--- apps/frontend/src/lib/three/components/physicalCube/addPhysics2D.ts
+++ apps/frontend/src/lib/three/components/physicalCube/addPhysics2D.ts
@@ -1,17 +1,17 @@
-import RAPIER from "@dimforge/rapier3d-compat";
+import RAPIER from "@dimforge/rapier2d-compat";
 
-import { addMesh } from "@lib/three/systems/Physics/3D/addMesh";
-import { cuboidColliderDesc } from "@lib/three/systems/Physics/3D/cuboidColliderDesc";
+import { addMesh } from "@lib/three/systems/Physics/2D/addMesh";
+import { cuboidColliderDesc } from "@lib/three/systems/Physics/2D/cuboidColliderDesc";
 
 import type { BoxGeometry, Mesh } from "three";
 import type { Patched } from "@lib/three/types/Patched";
 
-export function addPhysics3D({
+export function addPhysics2D({
   mesh,
 }: {
   mesh: Mesh<BoxGeometry> & Patched;
 }): void {
-  mesh.addPhysics3D = (physics) => {
+  mesh.addPhysics2D = (physics) => {
     addMesh({
       physics,
       mesh,
