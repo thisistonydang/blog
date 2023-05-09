e9ce7b7d885db369a3ab56e6fc13e625ee1e3e77
Tony Dang <tony@tonydang.blog>
2023-05-09 13:55:49 -0700

    add 2D physics to floor

---
+++ apps/frontend/src/lib/three/components/floor/addPhysics2D.ts
@@ -0,0 +1,22 @@
+import RAPIER from "@dimforge/rapier2d-compat";
+
+import { addMesh } from "@lib/three/systems/Physics/2D/addMesh";
+import { cuboidColliderDesc } from "@lib/three/systems/Physics/2D/cuboidColliderDesc";
+
+import type { BoxGeometry, Mesh } from "three";
+import type { Patched } from "@lib/three/types/Patched";
+
+export function addPhysics2D({
+  mesh,
+}: {
+  mesh: Mesh<BoxGeometry> & Patched;
+}): void {
+  mesh.addPhysics2D = (physics) => {
+    addMesh({
+      physics,
+      mesh,
+      rigidBodyDesc: RAPIER.RigidBodyDesc.fixed(),
+      colliderDesc: cuboidColliderDesc(mesh),
+    });
+  };
+}
