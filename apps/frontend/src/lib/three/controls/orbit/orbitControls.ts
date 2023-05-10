8ea06950dca65f6954cd19efa69d9aec5f58d9b6
Tony Dang <tony@tonydang.blog>
2023-05-10 06:28:55 -0700

    add props for tweaking

--- apps/frontend/src/lib/three/controls/orbit/orbitControls.ts
+++ apps/frontend/src/lib/three/controls/orbit/orbitControls.ts
@@ -1,19 +1,60 @@
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { updateGui } from "./updateGui";
 
 import type { Patched } from "../../types/Patched";
 import type { World } from "../../World";
 
-interface Controls {
+export interface Controls {
+  enabled: boolean;
   enableDamping: boolean;
+  enablePan: boolean;
+  enableRotate: boolean;
+  enableZoom: boolean;
+  minAzimuthAngle: number;
+  maxAzimuthAngle: number;
+  minPolarAngle: number;
+  maxPolarAngle: number;
+  maxDistance: number;
+  minDistance: number;
+  targetX: number;
+  targetY: number;
+  targetZ: number;
 }
 
-export function orbitControls({
-  camera,
-  renderer,
-  loop,
-}: World): OrbitControls & Patched {
+export function orbitControls(
+  { camera, renderer, loop }: World,
+  {
+    enabled = true,
+    enableDamping = true,
+    enablePan = true,
+    enableRotate = true,
+    enableZoom = true,
+    minAzimuthAngle = Infinity,
+    maxAzimuthAngle = Infinity,
+    minPolarAngle = 0,
+    maxPolarAngle = Math.PI,
+    minDistance = 0,
+    maxDistance = Infinity,
+    targetX = 0,
+    targetY = 0,
+    targetZ = 0,
+  }
+): OrbitControls {
   const c: Controls = {
-    enableDamping: true,
+    enabled,
+    enableDamping,
+    enablePan,
+    enableRotate,
+    enableZoom,
+    minAzimuthAngle,
+    maxAzimuthAngle,
+    minPolarAngle,
+    maxPolarAngle,
+    minDistance,
+    maxDistance,
+    targetX,
+    targetY,
+    targetZ,
   };
 
   // Create controls.
@@ -21,22 +62,31 @@ export function orbitControls({
     camera,
     renderer.domElement
   );
+  controls.enabled = c.enabled;
   controls.enableDamping = c.enableDamping;
-
-  // Update controls each tick to allow damping.
-  controls.tickOnRenderRequest = () => controls.update();
-  controls.tickOnWorldStart = () => controls.update();
+  controls.enablePan = c.enablePan;
+  controls.enableRotate = c.enableRotate;
+  controls.enableZoom = c.enableZoom;
+  controls.minAzimuthAngle = c.minAzimuthAngle;
+  controls.maxAzimuthAngle = c.maxAzimuthAngle;
+  controls.minPolarAngle = c.minPolarAngle;
+  controls.maxPolarAngle = c.maxPolarAngle;
+  controls.minDistance = c.minDistance;
+  controls.maxDistance = c.maxDistance;
+  controls.target.x = c.targetX;
+  controls.target.y = c.targetY;
+  controls.target.z = c.targetZ;
 
   // When rendering on demand, request a render on change events if a render has
   // not already been requested.
   controls.addEventListener("change", loop.requestRender);
 
-  controls.updateGui = ({ createFolder }) => {
-    const folder = createFolder("orbit controls");
-    folder.add(c, "enableDamping").onChange((enableDamping: boolean) => {
-      controls.enableDamping = enableDamping;
-    });
-  };
+  // Update controls each tick to allow damping.
+  controls.tickOnRenderRequest = () => controls.update();
+  controls.tickOnWorldStart = () => controls.update();
+
+  // Add tweaks
+  updateGui({ c, controls });
 
   return controls;
 }
