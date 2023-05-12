34852b60580c91f4bd1619ce13a2c3b5c1551d1b
Tony Dang <tony@tonydang.blog>
2023-05-12 09:12:57 -0700

    rm color change from physical cube onHover

--- apps/frontend/src/lib/three/components/physicalCube/onHover.ts
+++ apps/frontend/src/lib/three/components/physicalCube/onHover.ts
@@ -1,23 +1,16 @@
-import type { Mesh, MeshMatcapMaterial } from "three";
+import type { Mesh } from "three";
 import type { Patched } from "@lib/three/types/Patched";
-import type { Controls } from "./physicalCube";
 
 export function onHover({
-  c,
-  material,
   mesh,
 }: {
-  c: Controls;
-  material: MeshMatcapMaterial;
   mesh: Mesh & Patched;
 }): void {
   mesh.onPointerEnter = () => {
-    material.color.set(0xff0000);
     document.body.style.cursor = "pointer";
   };
 
   mesh.onPointerLeave = () => {
     document.body.style.cursor = "default";
-    material.color.set(c.color);
   };
 }
