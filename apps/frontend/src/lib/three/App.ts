6ba0c5002f851331c0a11b129419c89073f8286a
Tony Dang <tony@tonydang.blog>
2023-05-16 11:52:41 -0700

    update starter App

--- apps/frontend/src/lib/three/App.ts
+++ apps/frontend/src/lib/three/App.ts
@@ -1,26 +1,32 @@
-import { perspectiveCamera } from "./components-core/perspectiveCamera";
+import RAPIER from "@dimforge/rapier2d-compat";
+
+import { perspectiveCamera } from "@lib/three/components-core/perspectiveCamera";
+import { orbitControls } from "@lib/three/controls/orbit/orbitControls";
+import { createAxesHelper } from "@lib/three/helpers/axes-helper";
+// import { createGridHelper } from "@lib/three/helpers/grid-helper";
+
+import { Physics2D } from "@lib/three/systems/Physics/Physics2D";
+import { Pointer } from "@lib/three/systems/Pointer/Pointer";
+import { Gui } from "@lib/three/systems/Gui";
+import { Statistics } from "@lib/three/systems/Statistics";
+import { World } from "@lib/three/World";
 
 import { camera } from "./components/camera/camera";
 import { floor } from "./components/floor/floor";
-import { physicalCube } from "./components/physicalCube/physicalCube";
 import { physicalCubes } from "./components/physcialCubes/physicalCubes";
+import { physicalCube } from "./components/physicalCube/physicalCube";
 
-import { orbitControls } from "./controls/orbit/orbitControls";
-
-import { createAxesHelper } from "./helpers/axes-helper";
-// import { createGridHelper } from "./helpers/grid-helper";
-
-import { World } from "./World";
-
-import type { Rapier2D } from "./types/Rapier2D";
-import type { Rapier3D } from "./types/Rapier3D";
+await RAPIER.init();
 
 export class App extends World {
-  constructor(container: HTMLDivElement, RAPIER: Rapier2D | Rapier3D) {
+  constructor(container: HTMLDivElement) {
     super({
       camera: perspectiveCamera({}),
       container,
-      RAPIER,
+      pointer: Pointer,
+      physics: Physics2D,
+      gui: Gui,
+      statistics: Statistics,
     });
 
     this.addObjects([
