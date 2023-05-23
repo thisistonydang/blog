441866ea2d7996cff76d4b98a069484a88bc035e
Tony Dang <tony@tonydang.blog>
2023-05-23 15:02:57 -0700

    apps/frontend/src/lib/components/IconButton.svelte: init add

---
+++ apps/frontend/src/lib/components/IconButton.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Ping from "./Ping.svelte";
+  import Prose from "./Prose.svelte";
+
+  export let ariaLabel: string | undefined = undefined;
+  export let ariaLabelToggled: string | undefined = undefined;
+  export let isToggled = false;
+  export let disabled = false;
+  export let hasPing = false;
+  export let isPill = false;
+  export let fixedWidth: number | undefined = undefined;
+  export let onClick: () => void;
+
+  let mounted = false;
+  onMount(() => (mounted = true));
+</script>
+
+<button
+  class="
+    bg-text pointer-events-auto relative
+    h-10 rounded-full drop-shadow
+    disabled:cursor-not-allowed disabled:opacity-75
+  "
+  class:min-w-[40px]={!fixedWidth}
+  class:cursor-not-allowed={!mounted}
+  style:width={fixedWidth}
+  aria-label={isToggled ? ariaLabelToggled : ariaLabel}
+  {disabled}
+  on:click={onClick}
+>
+  <Prose>
+    <span
+      class="text-bg flex justify-center"
+      class:px-3={isPill}
+      class:font-serif={!isPill}
+      class:text-2xl={!isPill}
+    >
+      {#if isToggled}
+        <slot name="toggled-text" />
+      {:else}
+        <slot />
+      {/if}
+    </span>
+  </Prose>
+
+  {#if hasPing}
+    <div class="absolute bottom-8 left-8">
+      <Ping />
+    </div>
+  {/if}
+</button>
