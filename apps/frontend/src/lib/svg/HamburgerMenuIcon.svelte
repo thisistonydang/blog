7f904b585a0956650a2d65d431987c62bcd2699e
Tony Dang <tony@tonydang.blog>
2023-05-23 14:38:39 -0700

    HamburgerMenuIcon.svelte & XIcon.svelte: add default prop values

--- apps/frontend/src/lib/svg/HamburgerMenuIcon.svelte
+++ apps/frontend/src/lib/svg/HamburgerMenuIcon.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import SvgIcon from "./SvgIcon.svelte";
 
-  export let width: number | undefined;
-  export let height: number | undefined;
+  export let width: number | undefined = undefined;
+  export let height: number | undefined = undefined;
 </script>
 
 <SvgIcon {width} {height}>
