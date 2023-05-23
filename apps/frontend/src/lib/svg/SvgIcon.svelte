97a7c563fc4397ec32c9e23c67f4e3e9d6fb0a55
Tony Dang <tony@tonydang.blog>
2023-05-23 14:26:19 -0700

    SvgIcon.svelte: init add

---
+++ apps/frontend/src/lib/svg/SvgIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let width = 25;
+  export let height = 25;
+  export let viewBoxWidth = 512;
+  export let viewBoxHeight = 512;
+</script>
+
+<svg
+  {width}
+  {height}
+  viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}
+  fill="currentColor"
+>
+  <slot />
+</svg>
