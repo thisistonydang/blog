7bac659bcafd575004fdc95c59a812b72cbd364e
Tony Dang <tony@tonydang.blog>
2023-05-24 16:32:23 -0700

    mv react specific hooks into separate directory

--- apps/frontend/src/content/projects/particles/_components/App.tsx
+++ apps/frontend/src/content/projects/particles/_components/App.tsx
@@ -4,7 +4,7 @@ import { Canvas } from "@react-three/fiber";
 
 import LevaControls from "@lib/components/LevaControls";
 import Loading from "@lib/components/Loading";
-import { useMounted } from "@lib/hooks/useMounted";
+import { useMounted } from "@lib/react/hooks/useMounted";
 
 import Experience from "./Experience";
 import Interface from "./Interface";
