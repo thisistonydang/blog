f228d2ff5c9581f18ecab65fc3f5b98c68754ec7
Tony Dang <tony@tonydang.blog>
2023-05-24 16:45:49 -0700

    rename hasTouchScreen.ts -> useHasTouchScreen.ts

--- apps/frontend/src/lib/hooks/useHasTouchScreen.ts
+++ apps/frontend/src/lib/hooks/useHasTouchScreen.ts
@@ -8,7 +8,7 @@ interface NavigatorExtended extends Navigator {
  *
  * From MDN: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#mobile_device_detection
  */
-export function hasTouchScreen(): boolean {
+export function useHasTouchScreen(): boolean {
   const navigatorExtended: NavigatorExtended = navigator;
   let hasTouchScreen = false;
 
