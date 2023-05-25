c421e50976259f1391869c64b96f6b6f96cb8ae1
Tony Dang <tony@tonydang.blog>
2023-05-25 16:01:52 -0700

    possible-game/_components/App.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import { gameState, isFullscreen } from "../_stores/appState";
+  import { App } from "../_world/App";
+
+  import Interface from "./Interface.svelte";
+  import RestartModal from "./RestartModal.svelte";
+
+  let container: HTMLDivElement;
+  let app: App;
+
+  onMount(() => {
+    app = new App(container);
+    app.requestRender();
+  });
+
+  $: if ($isFullscreen) container.requestFullscreen();
+</script>
+
+<div
+  bind:this={container}
+  class="h-screen w-screen animate-[fade-in_10s]"
+  on:fullscreenchange={() => {
+    $isFullscreen = Boolean(document.fullscreenElement);
+  }}
+/>
+
+{#if $gameState === "not_started"}
+  <Interface />
+{:else if $gameState === "stopped"}
+  <RestartModal title="Try again?" {app} />
+{:else if $gameState === "ended"}
+  <RestartModal title="You won!" {app} />
+{/if}
