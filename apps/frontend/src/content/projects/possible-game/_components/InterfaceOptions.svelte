16cfe44b3efed7e41a75aff18c3999237dd8a6da
Tony Dang <tony@tonydang.blog>
2023-05-25 16:33:03 -0700

    possible-game/_components/InterfaceOptions.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/InterfaceOptions.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import IconButton from "@lib/components/IconButton.svelte";
+  import InterfaceList from "@lib/components/InterfaceList.svelte";
+  import { interfaceState, isFullscreen, isMuted } from "../_stores/appState";
+</script>
+
+<InterfaceList isVertical shiftUp>
+  <li class="flex flex-wrap gap-2">
+    <IconButton isPill onClick={() => ($isFullscreen = true)}>
+      Enter Fullscreen
+    </IconButton>
+
+    <IconButton isPill fixedWidth={100} onClick={() => ($isMuted = !$isMuted)}>
+      Sound {$isMuted ? "Off" : "On"}
+    </IconButton>
+  </li>
+
+  <li class="flex flex-wrap gap-2">
+    <IconButton isPill onClick={() => ($interfaceState = "credits")}>
+      Credits
+    </IconButton>
+
+    <IconButton isPill onClick={() => ($interfaceState = "how_to_play")}>
+      How To Play
+    </IconButton>
+  </li>
+</InterfaceList>
