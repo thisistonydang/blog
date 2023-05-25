aadac7e48c029ada9a5ea86f775355f3be6853ad
Tony Dang <tony@tonydang.blog>
2023-05-25 16:18:54 -0700

    possible-game/_components/Interface.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/Interface.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import { interfaceState } from "../_stores/appState";
+  import Credits from "./Credits.svelte";
+  import HowToPlay from "./HowToPlay.svelte";
+  import InterfaceOptions from "./InterfaceOptions.svelte";
+  import InterfaceTitle from "./InterfaceTitle.svelte";
+
+  onMount(() => ($interfaceState = "opened"));
+</script>
+
+<nav>
+  {#if ["closed", "opened"].includes($interfaceState)}
+    <InterfaceTitle />
+  {/if}
+
+  {#if $interfaceState === "opened"}
+    <InterfaceOptions />
+  {:else if $interfaceState === "credits"}
+    <Credits />
+  {:else if $interfaceState === "how_to_play"}
+    <HowToPlay />
+  {/if}
+</nav>
