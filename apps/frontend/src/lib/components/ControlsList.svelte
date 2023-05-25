736094dbea37735838dbe57c30869ee66fd1ef7b
Tony Dang <tony@tonydang.blog>
2023-05-25 11:13:26 -0700

    components/ControlsList.svelte: init add

---
+++ apps/frontend/src/lib/components/ControlsList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { fly } from "svelte/transition";
+
+  export let isVertical = false;
+  export let shiftUp = false;
+  export let shiftRight = false;
+</script>
+
+<ul
+  in:fly={{ duration: 250, x: !isVertical ? -50 : 0, y: isVertical ? 50 : 0 }}
+  class="pointer-events-none fixed flex gap-2"
+  class:flex-col-reverse={isVertical}
+  class:bottom-24={shiftUp}
+  class:bottom-8={!shiftUp}
+  class:left-24={shiftRight}
+  class:left-8={!shiftRight}
+>
+  <slot />
+</ul>
