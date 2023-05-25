9c9e551b48d50179db29736124829fba3dee0649
Tony Dang <tony@tonydang.blog>
2023-05-25 15:54:33 -0700

    possible-game/_components/RestartModal.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/RestartModal.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Dialog from "@lib/components/Dialog.svelte";
+  import { isFullscreen } from "../_stores/appState";
+  import type { App } from "../_world/App";
+
+  export let title: string;
+  export let app: App;
+
+  function restart(e: Event) {
+    e.preventDefault();
+    app.restart();
+  }
+
+  function exitFullscreen(e: Event) {
+    e.preventDefault();
+    document.exitFullscreen();
+  }
+</script>
+
+<Dialog
+  isModal
+  isProse
+  hasPadding
+  buttonWidth={175}
+  confirmText="RESTART"
+  cancelText={$isFullscreen ? "EXIT FULLSCREEN" : undefined}
+  onConfirm={restart}
+  onCancel={exitFullscreen}
+  onClose={restart}
+>
+  <h1 class="mb-8 text-center">{title}</h1>
+</Dialog>
