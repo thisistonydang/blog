6704b4642bda27a6a8b3a58c06b168f777d26915
Tony Dang <tony@tonydang.blog>
2023-05-25 16:39:35 -0700

    possible-game/_components/InterfaceModal.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/InterfaceModal.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Dialog from "@lib/components/Dialog.svelte";
+  import { interfaceState } from "../_stores/appState";
+
+  function close(e: Event) {
+    e.preventDefault();
+    $interfaceState = "opened";
+  }
+</script>
+
+<Dialog
+  isModal
+  isProse
+  hasPadding
+  buttonWidth={100}
+  confirmText="CLOSE"
+  onConfirm={close}
+  onClose={close}
+>
+  <slot />
+</Dialog>
