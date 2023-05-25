7a88cb043f76a9f7601fc2dcd816ea502babc35d
Tony Dang <tony@tonydang.blog>
2023-05-25 11:05:46 -0700

    particles/_components/Interface.tsx: hide interface when showing info

--- apps/frontend/src/content/projects/particles/_components/Interface.tsx
+++ apps/frontend/src/content/projects/particles/_components/Interface.tsx
@@ -10,28 +10,7 @@ export default function Interface({ children }: { children: React.ReactNode }) {
 
   return (
     <aside>
-      <div
-        className="
-          fixed bottom-8 left-8 flex
-          animate-[fade-in_0.25s] animate-[fly-up_0.25s]
-          items-center gap-4
-        "
-      >
-        <IconButton
-          ariaLabel="Show Info."
-          hasPing={hasPing}
-          onClick={() => {
-            setHasPing(false);
-            setShowInfo(true);
-          }}
-        >
-          i
-        </IconButton>
-        <Prose>
-          <h1>Particles</h1>
-        </Prose>
-      </div>
-      {showInfo && (
+      {showInfo ? (
         <Dialog
           isModal
           isProse
@@ -44,6 +23,28 @@ export default function Interface({ children }: { children: React.ReactNode }) {
         >
           <div className="mb-5">{children}</div>
         </Dialog>
+      ) : (
+        <div
+          className="
+          fixed bottom-8 left-8 flex
+          animate-[fade-in_0.25s] animate-[fly-up_0.25s]
+          items-center gap-4
+        "
+        >
+          <IconButton
+            ariaLabel="Show Info."
+            hasPing={hasPing}
+            onClick={() => {
+              setHasPing(false);
+              setShowInfo(true);
+            }}
+          >
+            i
+          </IconButton>
+          <Prose>
+            <h1>Particles</h1>
+          </Prose>
+        </div>
       )}
     </aside>
   );
