14d7f19584caf81c8dc94248e1f44f67045745a8
Tony Dang <tony@tonydang.blog>
2023-05-26 14:01:15 -0700

    possible-game/_components/Credits.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/Credits.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import InterfaceDialog from "./InterfaceDialog.svelte";
+</script>
+
+<InterfaceDialog title="Credits">
+  <p>
+    This game was inspired by
+    <a href="https://impossible.game/1/">The Impossible Game</a>
+    by <a href="https://www.flukegames.com/">Fluke Games</a>.
+  </p>
+
+  <p><b>Music:</b></p>
+  <ul>
+    <li>
+      <a
+        href="https://loyaltyfreakmusic.com/music/turbo-rallye-clicker-4000-sountrack/"
+      >
+        Turbo Rallye Clicker 4000 Soundtrack "Level 2" by Monplaisir
+      </a>
+    </li>
+  </ul>
+
+  <p><b>Sounds:</b></p>
+  <ul>
+    <li>
+      <a href="https://mixkit.co/free-sound-effects/arcade/">
+        "Arcade retro game over" by Mixkit
+      </a>
+    </li>
+    <li>
+      <a href="https://mixkit.co/free-sound-effects/arcade/">
+        "Arcade score interface" by Mixkit
+      </a>
+    </li>
+  </ul>
+
+  <p>
+    Created with <a href="https://threejs.org/">Three.js</a>,
+    <a href="https://rapier.rs/">Rapier</a>, and
+    <a href="https://svelte.dev/">Svelte</a>.
+  </p>
+
+  <small>
+    <b>Code:</b>
+    <a
+      href="https://github.com/tonydangblog/blog/tree/main/apps/frontend/src/content/projects/possible-game"
+    >
+      Github
+    </a>
+  </small>
+</InterfaceDialog>
