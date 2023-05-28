8a21290dcb8d69c22311adeb8ed036769d4f76ba
Tony Dang <tony@tonydang.blog>
2023-05-28 13:31:58 -0700

    possible-game/_world/components/player.ts: init add

---
+++ apps/frontend/src/content/projects/possible-game/_world/components/player.ts
@@ -0,0 +1,141 @@
+import RAPIER from "@dimforge/rapier2d-compat";
+import { get } from "svelte/store";
+import { MathUtils, Mesh, MeshBasicMaterial } from "three";
+
+import { theme } from "@layouts/page/_stores/theme";
+
+import { boxGeometry } from "@lib/three/geometries/box";
+import { addMesh } from "@lib/three/systems/Physics/2D/addMesh";
+import { cuboidColliderDesc } from "@lib/three/systems/Physics/2D/cuboidColliderDesc";
+
+import { gameState } from "../../_stores/appState";
+
+import type { BoxGeometry } from "three";
+import type { Physics2D } from "@lib/three/systems/Physics/Physics2D";
+import type { Patched } from "@lib/three/types/Patched";
+import type { App } from "../App";
+
+export function player(app: App): Mesh {
+  const playerHeight = 1;
+
+  // State
+  let playerState: "grounded" | "aerial" | "dead" = "grounded";
+  let rotationY = 0;
+  let restarted = false;
+
+  // Create mesh
+  const material = new MeshBasicMaterial({ wireframe: true });
+  const player: Mesh<BoxGeometry> & Patched = new Mesh(boxGeometry, material);
+  // start: 0, pre-stairs: 115, pre-drop: 213
+  player.position.set(0, playerHeight / 2, 0);
+  player.name = "player";
+
+  // Add physics
+  player.addPhysics2D = (physics) => {
+    addMesh({
+      physics,
+      mesh: player,
+      rigidBodyDesc: RAPIER.RigidBodyDesc.dynamic(),
+      gravityScale: 1.5,
+      lockRotations: true,
+      colliderDesc: cuboidColliderDesc(player),
+      activeEvents: RAPIER.ActiveEvents.COLLISION_EVENTS,
+    });
+  };
+
+  player.onCollisionEnter = (_, { id }) => {
+    // Update player state when collision occurs
+    if (id.includes("platform")) {
+      playerState = "grounded";
+    } else if (id.includes("trap")) {
+      playerState = "dead";
+      app.stopGame();
+    }
+
+    // Update cameraHeight state if player is on second-to-last or last platform
+    if (id === "second-to-last-platform") {
+      app.cameraHeight = "follow_player_y";
+    } else if (id === "last-platform") {
+      app.cameraHeight = "end";
+    }
+  };
+
+  player.onCollisionExit = () => {
+    // Prevent onCollisionExit from firing on restarts. This prevents a bug
+    // where the playerState is incorrectly aerial despite being in contact with
+    // the starting platfom.
+    if (restarted) {
+      restarted = false;
+      return;
+    }
+
+    playerState = "aerial";
+  };
+
+  player.tickOnWorldStart = () => {
+    // If player is grounded, slide the player at a constant linear velocity.
+    if (playerState === "grounded") {
+      const physicsBody = (app.physics as Physics2D).meshMap.get(player);
+      physicsBody?.rigidBody.setLinvel({ x: 6, y: 0 }, true);
+    }
+
+    // Spin player if player rotation does not equal current rotationY value
+    player.rotation.y = MathUtils.lerp(player.rotation.y, rotationY, 0.075);
+
+    // End game if player is past finishEnd checkpoint and game hasn't ended
+    if (
+      player.position.x > app.checkpoints.finishEnd &&
+      get(gameState) !== "ended"
+    ) {
+      app.endGame();
+    }
+
+    // Stop render loop to prevent unnessary renders after player is offscreen
+    if (player.position.x > app.checkpoints.offscreen) {
+      app.stop();
+    }
+  };
+
+  player.userData.jump = () => {
+    // Don't jump if player is not grounded nor if player has passed the
+    // finishStart checkpoint.
+    if (
+      playerState !== "grounded" ||
+      player.position.x > app.checkpoints.finishStart
+    ) {
+      return;
+    }
+
+    // Jump!
+    const physicsBody = (app.physics as Physics2D).meshMap.get(player);
+    physicsBody?.rigidBody.applyImpulse({ x: 0, y: 7 }, true);
+
+    // Spin!
+    rotationY = rotationY + Math.PI;
+  };
+
+  // Reset player on restart
+  player.userData.reset = () => {
+    // Reset player position
+    player.position.set(0, playerHeight / 2, 0);
+    const physicsBody = (app.physics as Physics2D).meshMap.get(player);
+    physicsBody?.rigidBody.setTranslation({ x: 0, y: playerHeight / 2 }, true);
+
+    // Reset player state
+    playerState = "grounded";
+
+    // Reset player rotation
+    rotationY = 0;
+    player.rotation.y = 0;
+
+    // Flag as restarted
+    restarted = true;
+  };
+
+  // Sync player color with theme
+  theme.subscribe((theme) => {
+    material.color.set(theme === "dark" ? 0x00ffff : 0x006161);
+  });
+
+  return player;
+}
