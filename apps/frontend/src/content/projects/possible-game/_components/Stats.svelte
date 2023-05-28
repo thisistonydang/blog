abc75ecae7cb02436cd97f00c1182e40ce95cfb0
Tony Dang <tony@tonydang.blog>
2023-05-28 09:06:00 -0700

    possible-game/_components/Stats.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/Stats.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import InterfaceDialog from "./InterfaceDialog.svelte";
+  import StatsList from "./StatsList.svelte";
+</script>
+
+<InterfaceDialog title="Stats">
+  <StatsList />
+  <p class="xxs:text-base max-w-xs text-center text-sm">
+    Please note, these counts will reset if you clear your browser history.
+  </p>
+</InterfaceDialog>
