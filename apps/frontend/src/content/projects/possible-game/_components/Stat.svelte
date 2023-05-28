8ee5b4b7614805aad9dbe68c0bb52af7a393c6cf
Tony Dang <tony@tonydang.blog>
2023-05-28 08:45:22 -0700

    possible-game/_components/Stat.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/Stat.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let name: string;
+  export let count: number;
+</script>
+
+<div class="flex justify-between gap-3">
+  <dt class="font-bold">{name}</dt>
+  <dd>{count}</dd>
+</div>
