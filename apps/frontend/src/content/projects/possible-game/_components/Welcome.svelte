7577c7274e63798a85e26269ad501369b97335f7
Tony Dang <tony@tonydang.blog>
2023-05-28 10:05:26 -0700

    possible-game/_components/Welcome.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/Welcome.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import HowToPlayInstructions from "./HowToPlayInstructions.svelte";
+  import InterfaceDialog from "./InterfaceDialog.svelte";
+</script>
+
+<InterfaceDialog title="Possible Game">
+  <div class="max-w-md">
+    <p class="xxs:text-base text-sm">
+      Welcome to the Possible Game, a 2D platformer inspired by
+      <a href="https://impossible.game/1/">The Impossible Game</a>.
+    </p>
+
+    <h2 class="text-center">How To Play</h2>
+    <HowToPlayInstructions />
+  </div>
+</InterfaceDialog>
