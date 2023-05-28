b64909e9666f7d1d5822729e6c40c5919452c029
Tony Dang <tony@tonydang.blog>
2023-05-28 08:13:16 -0700

    possible-game/_stores/appState.ts: 1. add "stats" and "welcome" interface states and 2. add local storage state

--- apps/frontend/src/content/projects/possible-game/_stores/appState.ts
+++ apps/frontend/src/content/projects/possible-game/_stores/appState.ts
@@ -5,10 +5,17 @@ export const gameState = writable<
 >("not_started");
 
 export const interfaceState = writable<
-  "closed" | "opened" | "credits" | "how_to_play"
+  "closed" | "opened" | "credits" | "how_to_play" | "stats" | "welcome"
 >("closed");
 
 export const isFullscreen = writable(false);
 
 // Start isMuted as undefined so sound doesn't play on start up
 export const isMuted = writable<boolean>(undefined);
+
+// Local storage
+export const attempts = writable<number>(0);
+export const finishes = writable<number>(0);
+export const currentStreak = writable<number>(0);
+export const longestStreak = writable<number>(0);
+export const visited = writable<boolean>(false);
