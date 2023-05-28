7bfee8163c53596a65ec59020aaf5a42ee8f53d4
Tony Dang <tony@tonydang.blog>
2023-05-28 08:54:41 -0700

    possible-game/_components/StatsList.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/StatsList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import {
+    attempts,
+    currentStreak,
+    finishes,
+    longestStreak,
+  } from "../_stores/appState";
+
+  import Stat from "./Stat.svelte";
+</script>
+
+<dl class="mx-auto mb-8 max-w-[160px]">
+  <Stat name="Attempts" count={$attempts} />
+  <Stat name="Finishes" count={$finishes} />
+  <Stat name="Current Streak" count={$currentStreak} />
+  <Stat name="Longest Streak" count={$longestStreak} />
+</dl>
