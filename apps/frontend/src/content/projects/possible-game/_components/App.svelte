45f87a05750dd38bf2223cb6182a57b2251160da
Tony Dang <tony@tonydang.blog>
2023-05-28 10:38:51 -0700

    possible-game/_components/App.svelte: 1. add local storage syncing and 2. mv mouse/keypress handling from threejs app to svelte

--- apps/frontend/src/content/projects/possible-game/_components/App.svelte
+++ apps/frontend/src/content/projects/possible-game/_components/App.svelte
@@ -1,27 +1,78 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
-  import { gameState, interfaceState, isFullscreen } from "../_stores/appState";
+  import { getParsedValue } from "@lib/local-storage/getParsedValue";
+
+  import {
+    LOCAL_STORAGE_KEY,
+    LOCAL_STORAGE_VERSION,
+    updateLocalStorage,
+  } from "../_lib/local-storage/updateLocalStorage";
+  import {
+    attempts,
+    currentStreak,
+    finishes,
+    gameState,
+    interfaceState,
+    isFullscreen,
+    longestStreak,
+    visited,
+  } from "../_stores/appState";
   import { App } from "../_world/App";
 
   import Interface from "./Interface.svelte";
   import RestartDialog from "./RestartDialog.svelte";
 
   let container: HTMLDivElement;
+  let canvas: HTMLCanvasElement;
   let app: App;
-
-  onMount(() => {
-    app = new App(container);
-    app.requestRender();
-  });
 
   $: if ($isFullscreen) container.requestFullscreen();
 
-  function closeInterfaceDialog() {
-    if (["credits", "how_to_play"].includes($interfaceState)) {
+  // Set visited state to true if user has played a game
+  $: if (["stopped", "ended"].includes($gameState) && !$visited) {
+    $visited = true;
+    updateLocalStorage(["visited", true]);
+  }
+
+  function handleMouseDown() {
+    if (!["closed", "opened"].includes($interfaceState)) {
+      // Close the currently opened interface dialog
       $interfaceState = "opened";
+    } else if ($gameState === "not_started") {
+      app.startGame();
+    } else if ($gameState === "playing") {
+      app.jump();
     }
   }
+
+  function handleKeyDown(e: KeyboardEvent) {
+    if (e.code === "Space" || e.key === " ") {
+      handleMouseDown();
+    }
+  }
+
+  onMount(() => {
+    // Sync game stats with local storage
+    function isValid(value: unknown): boolean {
+      return typeof value === "number" && Number.isInteger(value) && value > 0;
+    }
+    const data = getParsedValue(LOCAL_STORAGE_KEY, LOCAL_STORAGE_VERSION);
+    if (isValid(data.attempts)) $attempts = data.attempts;
+    if (isValid(data.finishes)) $finishes = data.finishes;
+    if (isValid(data.currentStreak)) $currentStreak = data.currentStreak;
+    if (isValid(data.longestStreak)) $longestStreak = data.longestStreak;
+
+    // Create and render game app
+    app = new App(container);
+    app.requestRender();
+
+    // Add event listeners to canvas
+    canvas = app.renderer.domElement;
+    canvas.tabIndex = 0; // Allow canvas to be focusable
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("keydown", handleKeyDown);
+  });
 </script>
 
 <div
@@ -30,8 +81,6 @@
   on:fullscreenchange={() => {
     $isFullscreen = Boolean(document.fullscreenElement);
   }}
-  on:click={closeInterfaceDialog}
-  on:keypress={closeInterfaceDialog}
 />
 
 {#if $gameState === "not_started"}
