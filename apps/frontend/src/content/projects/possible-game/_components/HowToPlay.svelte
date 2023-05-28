cf7bd8d834873ce23143aebfe39680e614870231
Tony Dang <tony@tonydang.blog>
2023-05-28 10:02:49 -0700

    extract how to play instructions into reusable component

--- apps/frontend/src/content/projects/possible-game/_components/HowToPlay.svelte
+++ apps/frontend/src/content/projects/possible-game/_components/HowToPlay.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
+  import HowToPlayInstructions from "./HowToPlayInstructions.svelte";
   import InterfaceDialog from "./InterfaceDialog.svelte";
 </script>
 
 <InterfaceDialog title="How To Play">
-  <ol>
-    <li>Tap/click the screen or press the space bar to jump.</li>
-    <li>Avoid the obstacles and try to reach the end of the level.</li>
-  </ol>
-  <p class="text-center text-2xl font-bold">Good luck!</p>
+  <HowToPlayInstructions />
 </InterfaceDialog>
