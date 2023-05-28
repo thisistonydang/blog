b2d8511c16e330e05125f6c84fb4bda7bdf2a7a6
Tony Dang <tony@tonydang.blog>
2023-05-28 09:50:27 -0700

    add stats and welcome to interface

--- apps/frontend/src/content/projects/possible-game/_components/Interface.svelte
+++ apps/frontend/src/content/projects/possible-game/_components/Interface.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
-  import { interfaceState } from "../_stores/appState";
+  import { getParsedValue } from "@lib/local-storage/getParsedValue";
+
+  import {
+    LOCAL_STORAGE_KEY,
+    LOCAL_STORAGE_VERSION,
+  } from "../_lib/local-storage/updateLocalStorage";
+  import { interfaceState, visited } from "../_stores/appState";
+
   import Credits from "./Credits.svelte";
   import HowToPlay from "./HowToPlay.svelte";
   import InterfaceOptions from "./InterfaceOptions.svelte";
   import InterfaceTitle from "./InterfaceTitle.svelte";
+  import Stats from "./Stats.svelte";
+  import Welcome from "./Welcome.svelte";
 
-  onMount(() => ($interfaceState = "opened"));
+  onMount(() => {
+    // Show welcome information if user has not visited before, else show the
+    // interface options.
+    const data = getParsedValue(LOCAL_STORAGE_KEY, LOCAL_STORAGE_VERSION);
+    if (typeof data.visited === "boolean") {
+      $visited = data.visited;
+    }
+
+    $interfaceState = $visited ? "opened" : "welcome";
+  });
 </script>
 
 <nav>
@@ -21,5 +39,9 @@
     <Credits />
   {:else if $interfaceState === "how_to_play"}
     <HowToPlay />
+  {:else if $interfaceState === "stats"}
+    <Stats />
+  {:else if $interfaceState === "welcome"}
+    <Welcome />
   {/if}
 </nav>
