7e16576ce22a16f92e7d3759e75de6fb5289e68b
Tony Dang <tony@tonydang.blog>
2023-05-29 16:52:27 -0700

    possible-game/_components/App.svelte: extract Canvas and LocalStorage logic into separate components

--- apps/frontend/src/content/projects/possible-game/_components/App.svelte
+++ apps/frontend/src/content/projects/possible-game/_components/App.svelte
@@ -1,94 +1,34 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-
-  import { getParsedValue } from "@lib/local-storage/getParsedValue";
-
   import {
-    LOCAL_STORAGE_KEY,
-    LOCAL_STORAGE_VERSION,
-    updateLocalStorage,
-  } from "../_lib/local-storage/updateLocalStorage";
-  import {
-    attempts,
-    currentStreak,
-    finishes,
+    gameOverAudioCanPlayThrough,
     gameState,
-    interfaceState,
-    isFullscreen,
-    isMuted,
-    longestStreak,
-    visited,
+    levelAudioCanPlayThrough,
+    scoreAudioCanPlayThrough,
   } from "../_stores/appState";
-  import { App } from "../_world/App";
 
+  import Audio from "./Audio.svelte";
+  import Canvas from "./Canvas.svelte";
   import Interface from "./Interface.svelte";
+  import LocalStorage from "./LocalStorage.svelte";
   import RestartDialog from "./RestartDialog.svelte";
 
-  let container: HTMLDivElement;
-  let canvas: HTMLCanvasElement;
-  let app: App;
-
-  $: if ($isFullscreen) container.requestFullscreen();
-
-  // Set visited state to true if user has played a game
-  $: if (["stopped", "ended"].includes($gameState) && !$visited) {
-    $visited = true;
-    updateLocalStorage(["visited", true]);
-  }
-
-  function handleMouseDown() {
-    if (!["closed", "opened"].includes($interfaceState)) {
-      // Close the currently opened interface dialog
-      $interfaceState = "opened";
-    } else if ($gameState === "not_started") {
-      app.startGame();
-    } else if ($gameState === "playing") {
-      app.jump();
-    }
-  }
-
-  function handleKeyDown(e: KeyboardEvent) {
-    if (e.code === "Space" || e.key === " ") {
-      handleMouseDown();
-    }
-  }
-
-  onMount(() => {
-    // Sync game stats with local storage
-    function isValid(value: unknown): boolean {
-      return typeof value === "number" && Number.isInteger(value) && value > 0;
-    }
-    const data = getParsedValue(LOCAL_STORAGE_KEY, LOCAL_STORAGE_VERSION);
-    if (isValid(data.attempts)) $attempts = data.attempts;
-    if (isValid(data.finishes)) $finishes = data.finishes;
-    if (isValid(data.currentStreak)) $currentStreak = data.currentStreak;
-    if (isValid(data.longestStreak)) $longestStreak = data.longestStreak;
-    if (typeof data.isMuted === "boolean") $isMuted = data.isMuted;
-
-    // Create and render game app
-    app = new App(container);
-    app.requestRender();
-
-    // Add event listeners to canvas
-    canvas = app.renderer.domElement;
-    canvas.tabIndex = 0; // Allow canvas to be focusable
-    canvas.addEventListener("mousedown", handleMouseDown);
-    canvas.addEventListener("keydown", handleKeyDown);
-  });
+  $: isReady =
+    $scoreAudioCanPlayThrough &&
+    $gameOverAudioCanPlayThrough &&
+    $levelAudioCanPlayThrough;
 </script>
 
-<div
-  bind:this={container}
-  class="h-screen w-screen animate-[fade-in_10s]"
-  on:fullscreenchange={() => {
-    $isFullscreen = Boolean(document.fullscreenElement);
-  }}
-/>
+<Audio />
+<LocalStorage />
+
+{#if isReady}
+  <Canvas />
+{/if}
 
 {#if $gameState === "not_started"}
   <Interface />
 {:else if $gameState === "stopped"}
-  <RestartDialog title="You died :(" {app} />
+  <RestartDialog title="You died :(" />
 {:else if $gameState === "ended"}
-  <RestartDialog title="You won!" {app} />
+  <RestartDialog title="You won!" />
 {/if}
