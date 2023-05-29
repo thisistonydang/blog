255469ad42f868953cba2457508aad4aa0b8f1b3
Tony Dang <tony@tonydang.blog>
2023-05-29 16:19:56 -0700

    possible-game/_components/ScoreAudio.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/ScoreAudio.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import {
+    gameState,
+    isMuted,
+    scoreAudioCanPlayThrough,
+  } from "../_stores/appState";
+
+  let audio: HTMLAudioElement;
+  let mounted = false;
+
+  onMount(() => {
+    audio.load();
+    mounted = true;
+  });
+
+  function play() {
+    if (mounted && $scoreAudioCanPlayThrough && !$isMuted) {
+      audio.pause();
+      audio.currentTime = 0;
+      audio.play();
+    }
+  }
+
+  function pause() {
+    if (mounted && $scoreAudioCanPlayThrough) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  }
+
+  $: if ($gameState === "ended") play();
+  $: if (!$isMuted) play();
+  $: if ($isMuted) pause();
+</script>
+
+<audio
+  bind:this={audio}
+  src="/audio/possible-game/score.mp3"
+  preload="auto"
+  on:canplaythrough={() => ($scoreAudioCanPlayThrough = true)}
+/>
