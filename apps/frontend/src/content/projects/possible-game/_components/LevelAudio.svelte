9a94121108f11dcae64a519dfc2ee866a86ca542
Tony Dang <tony@tonydang.blog>
2023-05-29 16:15:37 -0700

    possible-game/_components/LevelAudio.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/LevelAudio.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import {
+    gameState,
+    isMuted,
+    levelAudioCanPlayThrough,
+  } from "../_stores/appState";
+
+  let audio: HTMLAudioElement;
+  let mounted = false;
+
+  onMount(() => {
+    audio.load();
+    mounted = true;
+  });
+
+  function play() {
+    if (mounted && $levelAudioCanPlayThrough && !$isMuted) {
+      audio.pause();
+      audio.currentTime = 0;
+      audio.play();
+    }
+  }
+
+  function pause() {
+    if (mounted && $levelAudioCanPlayThrough) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  }
+
+  $: if ($gameState === "playing") play();
+  $: if (["not_started", "stopped"].includes($gameState)) pause();
+  $: if ($isMuted) pause();
+</script>
+
+<audio
+  bind:this={audio}
+  src="/audio/possible-game/level-2.mp3"
+  preload="auto"
+  on:canplaythrough={() => ($levelAudioCanPlayThrough = true)}
+/>
