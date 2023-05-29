0590a508470ca68caec62132015b7ce6ee56e4a9
Tony Dang <tony@tonydang.blog>
2023-05-29 16:48:34 -0700

    possible-game/_components/Canvas.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/Canvas.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+
+  import {
+    app,
+    gameState,
+    interfaceState,
+    isFullscreen,
+  } from "../_stores/appState";
+
+  import { App } from "../_world/App";
+
+  let container: HTMLDivElement;
+
+  function handleMouseDown() {
+    if (!["closed", "opened"].includes($interfaceState)) {
+      // Close the currently opened interface dialog
+      $interfaceState = "opened";
+    } else if ($gameState === "not_started") {
+      $app.startGame();
+    } else if ($gameState === "playing") {
+      $app.jump();
+    }
+  }
+
+  function handleKeyDown(e: KeyboardEvent) {
+    if (e.code === "Space" || e.key === " ") {
+      handleMouseDown();
+    }
+  }
+
+  onMount(() => {
+    // Create and render game app
+    $app = new App(container);
+    $app.requestRender();
+
+    // Add event listeners to canvas
+    const canvas = $app.renderer.domElement;
+    canvas.tabIndex = 0; // Allow canvas to be focusable
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("keydown", handleKeyDown);
+  });
+</script>
+
+<div
+  bind:this={container}
+  in:fade={{ duration: 5000 }}
+  class="h-screen w-screen"
+  on:fullscreenchange={() => {
+    $isFullscreen = Boolean(document.fullscreenElement);
+  }}
+/>
