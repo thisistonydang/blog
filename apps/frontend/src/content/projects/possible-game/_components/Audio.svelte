87d8344bf1d3f6c3194307d6997db1843f5b1307
Tony Dang <tony@tonydang.blog>
2023-05-29 15:29:17 -0700

    possible-game/_components/Audio.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/Audio.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import GameOverAudio from "./GameOverAudio.svelte";
+  import LevelAudio from "./LevelAudio.svelte";
+  import ScoreAudio from "./ScoreAudio.svelte";
+</script>
+
+<ScoreAudio />
+<GameOverAudio />
+<LevelAudio />
