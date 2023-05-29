54cfda65cc0211701814e1e7c2d0262884d01b40
Tony Dang <tony@tonydang.blog>
2023-05-29 16:17:30 -0700

    possible-game/_components/GameOverAudio.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/GameOverAudio.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import {
+    gameOverAudioCanPlayThrough,
+    gameState,
+    isMuted,
+  } from "../_stores/appState";
+
+  let audio: HTMLAudioElement;
+  let mounted = false;
+
+  onMount(() => {
+    audio.load();
+    mounted = true;
+  });
+
+  function play() {
+    if (mounted && $gameOverAudioCanPlayThrough && !$isMuted) {
+      audio.pause();
+      audio.currentTime = 0;
+      audio.play();
+    }
+  }
+
+  function pause() {
+    if (mounted && $gameOverAudioCanPlayThrough) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  }
+
+  $: if ($gameState === "stopped") play();
+  $: if ($isMuted) pause();
+</script>
+
+<audio
+  bind:this={audio}
+  src="/audio/possible-game/game-over.mp3"
+  preload="auto"
+  on:canplaythrough={() => ($gameOverAudioCanPlayThrough = true)}
+/>
