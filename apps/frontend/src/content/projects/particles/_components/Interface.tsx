ec3e81b8bc997ef9dc5aafa85e75bb37c680465c
Tony Dang <tony@tonydang.blog>
2023-05-30 14:18:24 -0700

    particles/_components/Interface.tsx: update with new Dialog api

--- apps/frontend/src/content/projects/particles/_components/Interface.tsx
+++ apps/frontend/src/content/projects/particles/_components/Interface.tsx
@@ -10,42 +10,47 @@ export default function Interface({ children }: { children: React.ReactNode }) {
 
   return (
     <aside>
-      {showInfo ? (
+      {showInfo && (
         <Dialog
           isModal
           isProse
           hasPadding
-          buttonText="CLOSE"
-          onButtonClick={(e) => {
+          buttonWidth={100}
+          confirmText="CLOSE"
+          onConfirm={(e) => {
+            e.preventDefault();
+            setShowInfo(false);
+          }}
+          onClose={(e) => {
             e.preventDefault();
             setShowInfo(false);
           }}
         >
           <div className="mb-5">{children}</div>
         </Dialog>
-      ) : (
-        <div
-          className="
+      )}
+
+      <div
+        className="
           fixed bottom-8 left-8 flex
           animate-[fade-in_0.25s] animate-[fly-up_0.25s]
           items-center gap-4
         "
+      >
+        <IconButton
+          ariaLabel="Show Info."
+          hasPing={hasPing}
+          onClick={() => {
+            setHasPing(false);
+            setShowInfo(!showInfo);
+          }}
         >
-          <IconButton
-            ariaLabel="Show Info."
-            hasPing={hasPing}
-            onClick={() => {
-              setHasPing(false);
-              setShowInfo(true);
-            }}
-          >
-            i
-          </IconButton>
-          <Prose>
-            <h1>Particles</h1>
-          </Prose>
-        </div>
-      )}
+          i
+        </IconButton>
+        <Prose>
+          <h1>Particles</h1>
+        </Prose>
+      </div>
     </aside>
   );
 }
