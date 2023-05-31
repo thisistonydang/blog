4054456bb63f3a16b6b5e9441888f69d7743f8b5
Tony Dang <tony@tonydang.blog>
2023-05-31 14:52:40 -0700

    possible-game/_components/PlayerImage.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/PlayerImage.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { playerImage, playerImageLoaded } from "../_stores/appState";
+
+  export let validPlayerNames: string[];
+
+  let tryLoadingImage = false;
+  let playerName: string | undefined;
+
+  onMount(() => {
+    const params = new URLSearchParams(window.location.search);
+    playerName = params.get("player")?.toLowerCase();
+
+    if (playerName && validPlayerNames.includes(playerName)) {
+      tryLoadingImage = true;
+      return;
+    }
+
+    $playerImageLoaded = true;
+  });
+</script>
+
+{#if tryLoadingImage}
+  <img
+    bind:this={$playerImage}
+    src={`/img/possible-game/${playerName}.jpg`}
+    alt=""
+    class="hidden"
+    on:load={() => ($playerImageLoaded = true)}
+    on:error={() => {
+      $playerImage = undefined;
+      $playerImageLoaded = true;
+    }}
+  />
+{/if}
