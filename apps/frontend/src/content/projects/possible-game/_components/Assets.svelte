bfcc191095aa976cdd86227edaef9be9cbb3104f
Tony Dang <tony@tonydang.blog>
2023-05-31 12:51:04 -0700

    rename Audio.svelte -> Assets.svelte

--- apps/frontend/src/content/projects/possible-game/_components/Assets.svelte
+++ apps/frontend/src/content/projects/possible-game/_components/Assets.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import GameOverAudio from "./GameOverAudio.svelte";
   import LevelAudio from "./LevelAudio.svelte";
+  import PlayerImage from "./PlayerImage.svelte";
   import ScoreAudio from "./ScoreAudio.svelte";
 </script>
 
+<!-- Audio -->
 <ScoreAudio />
 <GameOverAudio />
 <LevelAudio />
+
+<!-- Textures -->
+<PlayerImage />
