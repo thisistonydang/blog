e37ef32d6c311230bbd1f95630e3c6a74de6d759
Tony Dang <tony@tonydang.blog>
2023-06-05 13:08:49 -0700

    possible-game/_components/TrapImage.svelte: init add

---
+++ apps/frontend/src/content/projects/possible-game/_components/TrapImage.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { trapImage, trapImageLoaded } from "../_stores/appState";
+
+  const defaultTrapName = "box";
+  let trapName: string | undefined;
+
+  onMount(() => {
+    const params = new URLSearchParams(window.location.search);
+    trapName = params.get("trap")?.toLowerCase() || defaultTrapName;
+  });
+</script>
+
+<img
+  bind:this={$trapImage}
+  src={`/img/possible-game/${trapName}.png`}
+  alt={trapName}
+  class="hidden"
+  on:load={() => ($trapImageLoaded = true)}
+  on:error={() => (trapName = defaultTrapName)}
+/>
