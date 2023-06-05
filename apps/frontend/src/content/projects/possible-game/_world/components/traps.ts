49bfe889e6868244df69ec868d1be393b17d61d3
Tony Dang <tony@tonydang.blog>
2023-06-05 13:23:21 -0700

    possible-game/_world/components/traps.ts: use image texture

--- apps/frontend/src/content/projects/possible-game/_world/components/traps.ts
+++ apps/frontend/src/content/projects/possible-game/_world/components/traps.ts
@@ -1,5 +1,11 @@
 import RAPIER from "@dimforge/rapier2d-compat";
-import { InstancedMesh, MeshBasicMaterial } from "three";
+import { get } from "svelte/store";
+import {
+  InstancedMesh,
+  MeshBasicMaterial,
+  SRGBColorSpace,
+  Texture,
+} from "three";
 
 import { theme } from "@layouts/page/_stores/theme";
 
@@ -10,14 +16,30 @@ import { setInstanceIds } from "@lib/three/utils/instances/setInstanceIds";
 import { updateInstanceColors } from "@lib/three/utils/instances/updateInstanceColors";
 import { updateInstanceMatrices } from "@lib/three/utils/instances/updateInstanceMatrices";
 
+import { trapImage } from "../../_stores/appState";
+
 import type { BoxGeometry } from "three";
 import type { Instance } from "@lib/three/types/Instance";
 import type { Patched } from "@lib/three/types/Patched";
 import type { PhysicsInstance } from "@lib/three/types/Rapier2D";
 
 export function traps({ instances }: { instances: Instance[] }): InstancedMesh {
+  const image = get(trapImage);
+  let map: Texture | null = null;
+
+  // Load trap image as texture if available
+  if (image) {
+    map = new Texture(image);
+    map.colorSpace = SRGBColorSpace;
+    map.needsUpdate = true;
+  }
+
   // Create instancedMesh
-  const material = new MeshBasicMaterial({ wireframe: true });
+  const material = new MeshBasicMaterial({
+    map,
+    transparent: true,
+    wireframe: !image,
+  });
   const instancedMesh: InstancedMesh<BoxGeometry> & Patched = new InstancedMesh(
     boxGeometry,
     material,
