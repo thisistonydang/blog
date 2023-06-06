d675e58c1e27a5382ff74669a39ba89b80a57415
Tony Dang <tony@tonydang.blog>
2023-06-05 17:16:53 -0700

    add logic for handling fullscreen request on ios

--- apps/frontend/src/lib/fullscreen/requestFullscreen.ts
+++ apps/frontend/src/lib/fullscreen/requestFullscreen.ts
@@ -1,7 +1,22 @@
-export function requestFullscreen(element: Element): void {
-  if (element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if ("webkitRequestFullscreen" in element) {
-    (element.webkitRequestFullscreen as () => Promise<void>)();
+/**
+ * Attempt to request fullscreen.
+ *
+ * Return false if error occurs while requesting fullscreen, else return true.
+ */
+export function requestFullscreen(element: Element): boolean {
+  try {
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if ("webkitRequestFullscreen" in element) {
+      (element.webkitRequestFullscreen as () => Promise<void>)();
+    } else if ("webkitEnterFullscreen" in element) {
+      (element.webkitEnterFullscreen as () => Promise<void>)();
+    } else {
+      return false; // API for requesting fullscreen does not exist.
+    }
+  } catch (error: unknown) {
+    return false; // Error occurred while requesting fullscreen.
   }
+
+  return true; // Fullscreen requested successfully.
 }
