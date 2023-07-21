fbb8dd8e6c36cfc8868dc0e48fc3d87910a296f0
Tony Dang <tony@tonydang.blog>
2023-07-21 16:15:17 -0700

    componentize entry image -> EntryImage.svelte

---
+++ apps/frontend/src/lib/components/EntryImage.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { CollectionEntry } from "astro:content";
+  type Entry = CollectionEntry<"posts"> | CollectionEntry<"projects">;
+
+  export let entry: Entry;
+  export let assetsVersion: number;
+</script>
+
+<div class="aspect-[1200/630]">
+  <!-- Light mode image -->
+  <img
+    class="rounded-t dark:hidden"
+    src="/og/light/dist/960/{entry.slug}.jpg?v={assetsVersion}"
+    alt={entry.data.alt}
+    loading="lazy"
+  />
+
+  <!-- Dark mode image -->
+  <img
+    class="hidden rounded-t dark:inline"
+    src="/og/dark/dist/960/{entry.slug}.jpg?v={assetsVersion}"
+    alt={entry.data.alt}
+    loading="lazy"
+  />
+</div>
