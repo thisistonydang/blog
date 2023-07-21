d677ffd7e473e5662af9ba779decfa40aafdea96
Tony Dang <tony@tonydang.blog>
2023-07-21 11:15:22 -0700

    create Swatches.svelte

---
+++ apps/frontend/src/lib/d3/Swatches.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let color;
+  export let swatchWidth = 15;
+  export let swatchHeight = 15;
+  export let marginLeft = 0;
+</script>
+
+<div
+  style="
+    display: flex; 
+    align-items: center; 
+    min-height: 33px; 
+    margin-left: {marginLeft}px; 
+    font: 10px sans-serif;
+  "
+>
+  {#each color.domain() as category}
+    <span
+      class="border"
+      style="
+        display: inline-flex;
+        align-items: center; 
+        margin-right: 1em; 
+        --color: {color(category)};
+        --width: {swatchWidth}px;
+        --height: {swatchHeight}px;
+      "
+    >
+      {category}
+    </span>
+  {/each}
+</div>
+
+<style>
+  span::before {
+    content: "";
+    width: var(--width);
+    height: var(--height);
+    margin-right: 0.5em;
+    background: var(--color);
+  }
+</style>
