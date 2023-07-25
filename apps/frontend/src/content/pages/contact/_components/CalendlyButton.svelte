427afeaba3aa96c1ae80e3f8c3220c6ba39ac4b6
Tony Dang <tony@tonydang.blog>
2023-07-25 14:31:41 -0700

    init add /CalendlyButton.svelte

---
+++ apps/frontend/src/content/pages/contact/_components/CalendlyButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Button from "@lib/components/Button.svelte";
+
+  function showCalendlyWidget() {
+    // @ts-expect-error Global Calendly object from injected head script.
+    Calendly.showPopupWidget("https://calendly.com/tonydang/lets-chat");
+  }
+</script>
+
+<svelte:head>
+  <script
+    src="https://assets.calendly.com/assets/external/widget.js"
+    defer
+  ></script>
+  <link
+    href="https://assets.calendly.com/assets/external/widget.css"
+    rel="stylesheet"
+  />
+</svelte:head>
+
+<Button width={120} onClick={showCalendlyWidget}>BOOK A CALL</Button>
