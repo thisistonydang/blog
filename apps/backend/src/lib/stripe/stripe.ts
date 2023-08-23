e4fa9879f4ffe3e99c5c7a70a78d532ca65f81d1
Tony Dang <tony@tonydang.blog>
2023-08-23 09:23:38 -0700

    update stripe API version

--- apps/backend/src/lib/stripe/stripe.ts
+++ apps/backend/src/lib/stripe/stripe.ts
@@ -3,7 +3,7 @@ import type { Env } from "@lib/types/env";
 
 export function stripe(env: Env): Stripe {
   return new Stripe(env.STRIPE_API_KEY, {
-    apiVersion: "2022-08-01",
+    apiVersion: "2022-11-15",
     httpClient: Stripe.createFetchHttpClient(),
   });
 }
