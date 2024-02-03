b34b30d4c3ae858b345bb9e0bf71d3ffc126ddb8
Tony Dang <tony@tonydang.blog>
2024-02-02 21:10:25 -0800

    refactor

--- apps/backend/src/routes/microservices/yjs/merge/index.ts
+++ apps/backend/src/routes/microservices/yjs/merge/index.ts
@@ -1,11 +1,14 @@
 import { fromUint8Array, toUint8Array } from "js-base64";
 import * as Y from "yjs";
 
-import { verify_jwt } from "@lib/jwt/verify-jwt";
+import { getJwtPayloadFromRequest } from "@lib/jwt/getJwtPayloadFromRequest";
 import type { Env } from "@lib/types/env";
 
 export default async function merge(
   request: Request,
   env: Env,
 ): Promise<Response> {
+  const payload = await getJwtPayloadFromRequest(request, env);
+  if (!payload) return new Response("Invalid JWT.", { status: 400 });
+
 }
