4544ea7e5e0b90066f32319a6aa34e1e616c629d
Tony Dang <tony@tonydang.blog>
2024-02-02 20:58:29 -0800

    refactor

--- apps/backend/src/routes/microservices/mailer/send/index.ts
+++ apps/backend/src/routes/microservices/mailer/send/index.ts
@@ -1,4 +1,4 @@
-import { verify_jwt } from "@lib/jwt/verify-jwt";
+import { getJwtPayloadFromRequest } from "@lib/jwt/getJwtPayloadFromRequest";
 import { send_email } from "@lib/mailchannels/send_email";
 import type { Env } from "@lib/types/env";
 
@@ -6,37 +6,32 @@ export default async function send(
   request: Request,
   env: Env,
 ): Promise<Response> {
-  let payload;
-  const jwt = new URL(request.url).searchParams.get("jwt");
-  if (jwt) payload = await verify_jwt(env, jwt);
+  const payload = await getJwtPayloadFromRequest(request, env);
+  if (!payload) return new Response("Invalid JWT.", { status: 400 });
 
-  if (payload) {
-    const { email, name, subject, html } = payload;
+  const { email, name, subject, html } = payload;
 
-    // Check payload claims are valid.
-    if (
-      typeof email !== "string" ||
-      typeof name !== "string" ||
-      typeof subject !== "string" ||
-      typeof html !== "string"
-    ) {
-      return new Response("Invalid payload.", { status: 400 });
-    }
-
-    // Send email via MailChannels.
-    const mailChannelsRes = await send_email(env, email, name, subject, html);
-
-    // Send me an email notification.
-    await send_email(
-      env,
-      "tony@tonydang.blog",
-      "Tony",
-      `"${subject}" email sent to ${name} <${email}>`,
-      `Email sent:<br><br>${html}`,
-    );
-
-    return mailChannelsRes;
+  // Check payload claims are valid.
+  if (
+    typeof email !== "string" ||
+    typeof name !== "string" ||
+    typeof subject !== "string" ||
+    typeof html !== "string"
+  ) {
+    return new Response("Invalid payload.", { status: 400 });
   }
 
-  return new Response("Invalid JWT.", { status: 400 });
+  // Send email via MailChannels.
+  const mailChannelsRes = await send_email(env, email, name, subject, html);
+
+  // Send me an email notification.
+  await send_email(
+    env,
+    "tony@tonydang.blog",
+    "Tony",
+    `"${subject}" email sent to ${name} <${email}>`,
+    `Email sent:<br><br>${html}`,
+  );
+
+  return mailChannelsRes;
 }
