ea5876b36e4c009e35f5471d9ef259e3ae6b2c77
Tony Dang <tony@tonydang.blog>
2024-02-02 17:31:26 -0800

    add merge route fn

--- apps/backend/src/routes/microservices/yjs/merge/index.ts
+++ apps/backend/src/routes/microservices/yjs/merge/index.ts
@@ -4,3 +4,8 @@ import * as Y from "yjs";
 import { verify_jwt } from "@lib/jwt/verify-jwt";
 import type { Env } from "@lib/types/env";
 
+export default async function merge(
+  request: Request,
+  env: Env,
+): Promise<Response> {
+}
