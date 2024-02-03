96940f705ed7d545ca657742c6669e55a66997fc
Tony Dang <tony@tonydang.blog>
2024-02-02 21:12:18 -0800

    refactor

--- apps/backend/src/routes/microservices/mailer/send/index.ts
+++ apps/backend/src/routes/microservices/mailer/send/index.ts
@@ -9,6 +9,7 @@ export default async function send(
   const payload = await getJwtPayloadFromRequest(request, env);
   if (!payload) return new Response("Invalid JWT.", { status: 400 });
 
+  // Extract claims from payload.
   const { email, name, subject, html } = payload;
 
   // Check payload claims are valid.
