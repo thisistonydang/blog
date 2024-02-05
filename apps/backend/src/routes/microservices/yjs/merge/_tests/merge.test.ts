4ebcfb004ccab704836f9e483dede67abe9448f6
Tony Dang <tony@tonydang.blog>
2024-02-05 08:33:42 -0800

    add test yDoc updates

--- apps/backend/src/routes/microservices/yjs/merge/_tests/merge.test.ts
+++ apps/backend/src/routes/microservices/yjs/merge/_tests/merge.test.ts
@@ -8,4 +8,12 @@ import { env } from "@lib/testing/env";
 import api_route from "../index";
 
 describe("/microservices/yjs/merge", () => {
+  const doc1 = new Y.Doc();
+  const doc1Binary = Y.encodeStateAsUpdate(doc1);
+  const doc1Base64 = fromUint8Array(doc1Binary);
+
+  const doc2 = new Y.Doc();
+  const doc2Binary = Y.encodeStateAsUpdate(doc2);
+  const doc2Base64 = fromUint8Array(doc2Binary);
+
 });
