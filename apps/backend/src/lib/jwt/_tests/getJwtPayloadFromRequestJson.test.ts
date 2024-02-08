6642415a445449de0a7f34b627b01fb19361afdb
Tony Dang <tony@tonydang.blog>
2024-02-07 19:38:31 -0800

    set up test cases

--- apps/backend/src/lib/jwt/_tests/getJwtPayloadFromRequestJson.test.ts
+++ apps/backend/src/lib/jwt/_tests/getJwtPayloadFromRequestJson.test.ts
@@ -8,4 +8,16 @@ import { getJwtPayloadFromRequestJson } from "../getJwtPayloadFromRequestJson";
 describe("getJwtPayloadFromRequestJson", async () => {
   const jwt = await sign_jwt(env, { foo: "bar" });
 
+  it.each([
+    // Has valid JWT.
+    [true, jwt, false, "bar"],
+
+    // Has JWT but invalid.
+    [true, "invalid_jwt", true, undefined],
+
+    // No JWT.
+    [false, jwt, true, undefined],
+  ])(
+    "hasJwt: %s, jwt: %s, isPayloadNull: %s, expectedFoo: %s",
+  );
 });
