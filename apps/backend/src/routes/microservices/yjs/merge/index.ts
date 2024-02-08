252698523a371ca23537d1556a29533d49dfab2c
Tony Dang <tony@tonydang.blog>
2024-02-07 19:21:06 -0800

    get jwt as json rather than query param

--- apps/backend/src/routes/microservices/yjs/merge/index.ts
+++ apps/backend/src/routes/microservices/yjs/merge/index.ts
@@ -1,14 +1,14 @@
 import { fromUint8Array, toUint8Array } from "js-base64";
 import * as Y from "yjs";
 
-import { getJwtPayloadFromRequest } from "@lib/jwt/getJwtPayloadFromRequest";
+import { getJwtPayloadFromRequestJson } from "@lib/jwt/getJwtPayloadFromRequestJson";
 import type { Env } from "@lib/types/env";
 
 export default async function merge(
   request: Request,
   env: Env,
 ): Promise<Response> {
-  const payload = await getJwtPayloadFromRequest(request, env);
+  const payload = await getJwtPayloadFromRequestJson(request, env);
   if (!payload) return new Response("Invalid JWT.", { status: 400 });
 
   // Extract claims from payload.
