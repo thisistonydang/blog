c284fb556a46f5b45b5b8144b56990248df171d7
Tony Dang <tony@tonydang.blog>
2024-05-06 10:17:47 -0700

    add nav links

--- apps/frontend/src/layouts/page/_stores/nav_pages.ts
+++ apps/frontend/src/layouts/page/_stores/nav_pages.ts
@@ -1,6 +1,12 @@
 export const nav_pages = [
   { name: "posts", path: "/posts/", prefetch: true },
   { name: "projects", path: "/projects/", prefetch: true },
+  {
+    name: "talks",
+    path: "https://www.youtube.com/playlist?list=PL8lFmBcH3vX-JNIgxW3THUy7REthSRFEI",
+    prefetch: false,
+  },
+  { name: "now", path: "/now/", prefetch: true },
   // { name: "about", path: "/about", prefetch: true },
   // { name: "now", path: "/now", prefetch: true },
   // { name: "contact", path: "/contact", prefetch: false },
