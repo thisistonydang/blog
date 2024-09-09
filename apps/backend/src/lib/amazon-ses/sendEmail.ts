3adcfa23b9dd15549adb9de0b72532123a505fd4
Tony Dang <tony@tonydang.blog>
2024-09-09 14:34:39 -0700

    add amazon-ses dir

---
+++ apps/backend/src/lib/amazon-ses/sendEmail.ts
@@ -0,0 +1,39 @@
+import type { Env } from "@lib/types/env";
+
+/**
+ * Send email via MailChannels.
+ *
+ * @param env - Cloudflare worker environmental variables.
+ * @param email - Email address of recipient.
+ * @param name - Name of recipient.
+ * @param subject - Subject line of email.
+ * @param html - HTML body of email.
+ */
+export async function send_email(
+  env: Env,
+  email: string,
+  name: string,
+  subject: string,
+  html: string,
+): Promise<Response> {
+  // Note: MailChannels returns 202 status code in production.
+  if (env.MODE !== "production") return new Response(null, { status: 202 });
+
+  return await fetch("https://api.mailchannels.net/tx/v1/send", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      from: { email: "tony@tonydang.blog", name: "Tony Dang" },
+      personalizations: [
+        {
+          to: name ? [{ email, name }] : [{ email }],
+          dkim_domain: "tonydang.blog",
+          dkim_selector: "mc",
+          dkim_private_key: env.MC_DKIM_PRIVATE_KEY,
+        },
+      ],
+      subject,
+      content: [{ type: "text/html", value: html }],
+    }),
+  });
+}
