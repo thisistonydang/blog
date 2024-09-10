743c8f07df8059ffbacfc79d5ac3dae329d0a391
Tony Dang <tony@tonydang.blog>
2024-09-10 10:55:39 -0700

    update tests

--- apps/backend/src/routes/microservices/mailer/send/_tests/send.test.ts
+++ apps/backend/src/routes/microservices/mailer/send/_tests/send.test.ts
@@ -9,27 +9,40 @@ describe("/microservices/mailer/send", () => {
   it.each([
     // Valid payload.
     [
-      "test@example.com",
-      "Recipient Name",
+      "Sender <sender@example.com>",
+      "Recipient <recipient@example.com>",
+      "ReplyTo <replyto@example.com>",
       "Subject",
-      "Body HTML",
+      "Text Body",
+      "<h1>HTML Body</h1>",
       true,
       200,
       "Success: Email sent.",
     ],
 
     // Invalid payload.
-    [0, 0, 0, 0, true, 400, "Invalid payload."],
+    [0, 0, 0, 0, 0, 0, true, 400, "Invalid payload."],
 
     // Invalid JWT.
-    [0, 0, 0, 0, false, 400, "Invalid JWT."],
+    [0, 0, 0, 0, 0, 0, false, 400, "Invalid JWT."],
   ])(
-    "email: %s, name: %s, subject: %s, html: %s, isValidJwt: %s, expectedStatus: %s, expectedBody: %s",
+    `sender: %s, 
+     recipient: %s, 
+     replyTo: %s, 
+     subject: %s, 
+     textBody: %s, 
+     htmlBody: %s, 
+     isValidJwt: %s, 
+     expectedStatus: %s, 
+     expectedBody: %s
+    `,
     async (
-      email,
-      name,
+      sender,
+      recipient,
+      replyTo,
       subject,
-      html,
+      textBody,
+      htmlBody,
       isValidJwt,
       expectedStatus,
       expectedBody,
@@ -37,14 +50,7 @@ describe("/microservices/mailer/send", () => {
       // GIVEN Payload and whether JWT is valid.
 
       // WHEN Request is made to api route with jwt.
-      const jwt = await sign_jwt(env, { email, name, subject, html });
-      const request = new Request(
-        "https://tonydang.blog/microservices/mailer/send",
-        {
-          method: "POST",
-          body: JSON.stringify({ jwt: isValidJwt ? jwt : "invalid_jwt" }),
-          headers: {
-            "Content-Type": "application/json",
+      const jwt = await sign_jwt(env, { sender, recipient, replyTo, subject, textBody, htmlBody });
           },
         },
       );
