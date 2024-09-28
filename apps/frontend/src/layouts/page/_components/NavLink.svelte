81a676b5a134db576cf44e78663a81b77b4f3d43
Tony Dang <tony@tonydang.blog>
2024-09-27 17:35:39 -0700

    remove hover animation on touch devices

--- apps/frontend/src/layouts/page/_components/NavLink.svelte
+++ apps/frontend/src/layouts/page/_components/NavLink.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { useHasTouchScreen } from "@lib/hooks/useHasTouchScreen";
+
   export let current_path: string;
   export let page: { name: string; path: string; prefetch: boolean };
+
+  let hasTouchScreen = false;
+  onMount(() => (hasTouchScreen = useHasTouchScreen()));
 </script>
 
 <a
@@ -9,8 +15,8 @@
     pointer-events-auto opacity-90 after:block
     after:h-0.5
     after:w-0 after:transition-[width] after:duration-200
-    after:hover:w-full
   "
+  class:after:hover:w-full={!hasTouchScreen}
   class:after:w-full={current_path === page.path}
   href={page.path}
   rel={page.prefetch ? "prefetch" : null}
